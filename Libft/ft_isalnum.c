8d59ee46e89ca9516f77c51ea8c1372eccb199c8
BishopVK <danielgraindorge@gmail.com>
2024-01-13 23:39:16 +0100

    Corregida la función y añadida función main comentada

--- Libft/ft_isalnum.c
+++ Libft/ft_isalnum.c
@@ -6,14 +6,26 @@
 /*   By: danjimen <danjimen@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 19:28:22 by danjimen          #+#    #+#             */
-/*   Updated: 2024/01/10 21:00:43 by bishopvk         ###   ########.fr       */
+/*   Updated: 2024/01/13 23:38:23 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 int	ft_isalnum(int c)
 {
-	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= 0 && c <= 9))
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')
+		|| (c >= '0' && c <= '9'))
 		return (1);
 	else
 		return (0);
 }
+
+/*int	main(void)
+{
+	char	c;
+
+	c = '5';
+	printf("%d\n", ft_isalnum(c));
+	return (0);
+}*/
