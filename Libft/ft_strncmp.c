f5861b4f67a86a6efe7fed086ae04d1dae0f9811
BishopVK <danielgraindorge@gmail.com>
2024-01-14 00:28:11 +0100

    Corregido el cast de s1[i] y s2[i] para que funcione correctamente

--- Libft/ft_strncmp.c
+++ Libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 11:02:48 by danjimen          #+#    #+#             */
-/*   Updated: 2024/01/12 11:11:33 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/01/14 00:23:16 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,17 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	u_s1;
+	unsigned char	u_s2;
 
 	i = 0;
 	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		u_s1 = (unsigned char)s1[i];
+		u_s2 = (unsigned char)s2[i];
+		if (u_s1 != u_s2)
+			return (u_s1 - u_s2);
 		i++;
 	}
 	return (0);
