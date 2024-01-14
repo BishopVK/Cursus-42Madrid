98597f0d9e366ef1ad701e806d6637630dfd1941
BishopVK <danielgraindorge@gmail.com>
2024-01-14 19:06:20 +0100

    Creada función ft_strdup()

---
+++ Libft/ft_strdup.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bishopvk <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/14 18:59:36 by bishopvk          #+#    #+#             */
+/*   Updated: 2024/01/14 19:04:33 by bishopvk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	char	*dest;
+	char	*principio;
+
+	if (s == NULL)
+		return (NULL);
+	dest = (char *)malloc(ft_strlen(s) + 1);
+	if (dest == NULL)
+		exit (EXIT_FAILURE);
+	principio = dest;
+	while (*s != '\0')
+		*dest++ = *s++;
+	*dest = '\0';
+	return (principio);
+}
+
+/*int	main(void)
+{
+	char	*original = "Hello, World!";
+	char	*duplicate = ft_strdup(original);
+
+	if (duplicate != NULL)
+	{
+		printf("Original: %s\n", original);
+		printf("Duplicate: %s\n", duplicate);
+		free(duplicate);
+	}
+	else
+		fprintf(stderr, "Error: No se pudo duplicar la cadena\n");
+	return (0);
+}*/
