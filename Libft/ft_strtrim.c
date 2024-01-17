f83116c77aa0cfbe1d34df288bdfb5d01d651ad2
BishopVK <danielgraindorge@gmail.com>
2024-01-17 18:51:25 +0100

    Añadida y testeada función ft_strtrim()

--- Libft/ft_strtrim.c
+++ Libft/ft_strtrim.c
@@ -6,22 +6,45 @@
 /*   By: danjimen <danjimen@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 13:44:53 by danjimen          #+#    #+#             */
-/*   Updated: 2024/01/16 14:40:59 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/01/17 09:55:17 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*result;
+	int		start;
+	int		end;
+	int		i;
 
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	end = ft_strlen(s1) - 1;
+	while (end > start && ft_strchr(set, s1[end]))
+		end--;
+	result = (char *)malloc((end - start + 2) * sizeof(char));
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (start <= end)
+		result[i++] = s1[start++];
+	result[i] = '\0';
+	return (result);
 }
 
-int	main(void)
+/*int	main(void)
 {
 	char const 	*s1 = "12Hola12Hola12";
 	char const 	*set = "12";
-	char 		*result = ft_strtrim(s1, s2);
+	char 		*result = ft_strtrim(s1, set);
 
 	printf("Franse resultante: %s\n", result);
+	free (result);
 	return (0);
-}
+}*/
