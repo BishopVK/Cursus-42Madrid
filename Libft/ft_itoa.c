1336f9ec31b2428bf06324cf15b87d3afbddf1aa
BishopVK <danielgraindorge@gmail.com>
2024-01-21 11:46:22 +0100

    Terminada y testada función ft_itoa()

--- Libft/ft_itoa.c
+++ Libft/ft_itoa.c
@@ -5,30 +5,94 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: danjimen <danjimen@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/18 13:28:39 by danjimen          #+#    #+#             */
-/*   Updated: 2024/01/18 15:25:33 by danjimen         ###   ########.fr       */
+/*   Created: 2024/01/19 17:28:37 by danjimen          #+#    #+#             */
+/*   Updated: 2024/01/21 11:44:05 by bishopvk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_itoa(int n)
+static int  ft_num_len(int n)
 {
-	int	negative;
+    int len;
 
-	negative = 0;
-	//1. Comprobar si es positivo o negativo
-	if (n < 0)
-		negative = 1;
-
-	//2. Convertir el número a cadenas (comenzando desde el final)
-
-	//3. Agregar el caracter Nulo
-
-	//4. Manejar casos especiales (0, INT_MIN, INT_MAX, nº más grandes que INT, nº que comiencen por ceros...)
+    len = 0;
+    if (n < 0)
+        n = -n;
+    while (n != 0)
+    {
+        n = n / 10;
+        len++;
+    }
+    return (len);
 }
 
-int	main(void)
+static char *ft_num_to_string(int n, int len, int negative)
+{
+    int     i;
+    char    *str;
+
+    i = 0;
+    str = (char *)malloc(len * sizeof(char));
+    if (!str)
+        return (NULL);
+    len = len - 1;
+    str[len] = '\0';
+    while (len-- > 1)
+    {
+        str[len] = (char)((n % 10) + 48);
+        n = n / 10;
+    }
+    if (negative == 1)
+        str[len] = '-';
+    else
+        str[len] = (char)((n % 10) + 48);
+    return (str);
+}
+
+static char *ft_special_nbrs(int n)
+{
+    char    *str;
+
+    if (n == 0)
+    {
+        str = (char *)malloc(2 * sizeof(char));
+        ft_strlcpy(str, "0", 2);
+        return (str);
+    }
+    if (n == -2147483648)
+    {
+        str = (char *)malloc(12 * sizeof(char));
+        ft_strlcpy(str, "-2147483648", 12);
+        return (str);
+    }
+    return (NULL);
+}
+
+char    *ft_itoa(int n)
+{
+    char    *str;
+    int     negative;
+    int     len;
+
+    negative = 0;
+    if (n == 0 || n == -2147483648)
+    {
+        str = ft_special_nbrs(n);
+        return (str);
+    }
+    if (n < 0)
+    {
+        negative = 1;
+        n = -n;
+    }
+    len = (ft_num_len(n) + negative + 1);
+
+    str = ft_num_to_string(n, len, negative);
+    return (str);
+}
+
+/* int  main(void)
 {
     // Test 1: Número positivo
     int num1 = 12345;
@@ -55,40 +119,28 @@ int	main(void)
     free(str4);
 
     // Test 5: Número mínimo para un int en sistemas de 32 bits
-    int num5 = -2147483648;
+    int num5 = -2147483648; // Mínimo valor para un int en sistemas de 32 bits
     char* str5 = ft_itoa(num5);
     printf("Test 5: %d convertido a cadena: %s\n", num5, str5);
     free(str5);
 
-	// Test 6: Número positivo con muchos dígitos
-    int num6 = 12345678901234567890;
+    // Test 6: Número con varios ceros al principio
+    int num6 = 0005000;
     char* str6 = ft_itoa(num6);
     printf("Test 6: %d convertido a cadena: %s\n", num6, str6);
     free(str6);
 
-    // Test 7: Número negativo con muchos dígitos
-    int num7 = -99987654321;
+    // Test 7: Número con varios ceros al final
+    int num7 = 120000;
     char* str7 = ft_itoa(num7);
     printf("Test 7: %d convertido a cadena: %s\n", num7, str7);
     free(str7);
 
-    // Test 8: Número con varios ceros al principio
-    int num8 = 0005000;
+    // Test 8: Número con varios ceros intermedios
+    int num8 = 10203;
     char* str8 = ft_itoa(num8);
     printf("Test 8: %d convertido a cadena: %s\n", num8, str8);
     free(str8);
-
-    // Test 9: Número con varios ceros al final
-    int num9 = 120000;
-    char* str9 = ft_itoa(num9);
-    printf("Test 9: %d convertido a cadena: %s\n", num9, str9);
-    free(str9);
-
-    // Test 10: Número con varios ceros intermedios
-    int num10 = 10203;
-    char* str10 = ft_itoa(num10);
-    printf("Test 10: %d convertido a cadena: %s\n", num10, str10);
-    free(str10);
-
+    
     return 0;
-}
+} */
