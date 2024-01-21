216cd2a9697c6230768d4d507c8e7ec1cf925ebb
BishopVK <danielgraindorge@gmail.com>
2024-01-21 15:09:57 +0100

    Creada pero no testeada función ft_strmapi()

---
+++ Libft/ft_strmapi.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/21 13:43:09 by danjimen          #+#    #+#             */
+/*   Updated: 2024/01/21 13:43:09 by danjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static char	my_function(unsigned int n, char c)
+{
+	(void)n;
+	if (c >= 'a' && c <= 'z')
+		return (char)(c - 'a' + 'A');
+	return (c);
+}
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	if (s == NULL || f == NULL)
+		return (NULL);
+	char			*result;
+	unsigned int	i;
+
+	result = (char *)malloc((ft_strlen(s) + 1) * sizeof(char));
+	if (result == NULL)
+		return (NULL);
+	i = 0;
+	while (i < ft_strlen(s))
+	{
+		result[i] = f(i, s[i]);
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
+}
+
+int	main(void)
+{
+	char const	*s = "Hola, mundo!!";
+	char	*result;
+
+	result = ft_strmapi(s, my_function);
+
+	if (result != NULL)
+	{
+		printf("Resultado: %s\n", result);
+		free(result);
+	}
+	else
+		printf("Fallo en la reserva de memoria\n");
+	return (0);
+}
+
