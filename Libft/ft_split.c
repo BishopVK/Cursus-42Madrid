8f7d798506922e527455f1299399888d636d5ba3
BishopVK <danielgraindorge@gmail.com>
2024-01-21 11:41:06 +0100

    Función ft_split() terminada

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -6,74 +6,107 @@
 /*   By: danjimen <danjimen@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 11:06:55 by danjimen          #+#    #+#             */
-/*   Updated: 2024/01/17 15:01:49 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/01/18 12:44:12 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <string.h>
 
-char    **ft_split(char const *s, char c)
+static int	ft_count_elements(const char *s, char c)
 {
-    char    **split;
-    int        i;
-    int        count;
-    int        j;
-    
-    i = 0;
-    count = 0;
-    while (s[i] != '\0')
-        if (s[i++] == c)
-            count++;
-    split = (char **)malloc((count + 2) * sizeof(char *));
-    if (!split)
-    {
-        free(split);
-        return (NULL);
-    }
-    i = 0;
-    j = 0;
-    while (i <= count)
-    {
-        split[i] = (char *)malloc(((ft_strchr(s, c) - s) - j + 1) * sizeof(char));
-        while (i < ft_strchr(s, c) - s)
-            ft_strlcpy(split[i], s, (ft_strchr(s, c) - s + 1));
-        i++;
-        split[i]
-    }
-    split[count] = '\0';
-    return (split);
+	int	count;
+	int	i;
+
+	count = 0;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		while (s[i] == c)
+			i++;
+		if (s[i] != c && s[i] != '\0')
+			count++;
+		while (s[i] != c && s[i] != '\0')
+			i++;
+	}
+	return (count);
 }
 
-int	main(void)
+static char	**ft_copy_replace(const char *s, char c, char **split)
 {
-	char const	*s = "Hola,que,tal";
+	int	i;
+	int	start;
+	int	count;
+
+	i = 0;
+	start = 0;
+	count = 0;
+	while (s[i])
+	{
+		while (s[i] == c)
+			i++;
+		if (s[i] == '\0')
+			break ;
+		start = i;
+		while (s[i] != c && s[i] != '\0')
+			i++;
+		split[count] = (char *)malloc((i - start + 1) * sizeof(char));
+		if (!split[count])
+			return (NULL);
+		ft_strlcpy(split[count], s + start, i - start + 1);
+		count++;
+	}
+	split[count] = NULL;
+	return (split);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**split;
+	int		count;
+
+	count = ft_count_elements(s, c);
+	split = (char **)malloc((count + 1) * sizeof(char *));
+	if (!split)
+	{
+		free(split);
+		return (NULL);
+	}
+	ft_copy_replace(s, c, split);
+	split[count] = NULL;
+	return (split);
+}
+
+/*int	main(void)
+{
+	char const	*s = "Hola,,,,,,,,,que,,,,tal";
 	char		c = ',';
 	char		**split;
 	int			i;
-    int			numStrings = 0;
+	int			numStrings = 0;
 
+	i = 0;
 	split = ft_split(s, c);
 
 	// Contar cuántos elementos no nulos hay en frases
-    while (split[numStrings] != NULL) {
-        numStrings++;
-    }
+	while (split[numStrings] != NULL) {
+		numStrings++;
+	}
 
-    // Imprimir el número de frases almacenadas
-    printf("Número de frases almacenadas: %d\n", numStrings);
+	// Imprimir el número de frases almacenadas
+	printf("Número de frases almacenadas: %d\n", numStrings);
 
 	// Imprimir las frases almacenadas
-    for (int i = 0; i < 3; i++) {
-        printf("split[%d]: %s\n", i, split[i]);
-    }
+	for (int i = 0; i < numStrings; i++) {
+		printf("split[%d]: %s\n", i, split[i]);
+	}
 
-    // Liberar la memoria asignada
-    for (int i = 0; i < 3; i++) {
-        free(split[i]);
-    }
-    
+	// Liberar la memoria asignada
+	for (int i = 0; i < numStrings; i++) {
+		free(split[i]);
+	}
+
 	free(split);
 
 	return(0);
-}
+}*/
