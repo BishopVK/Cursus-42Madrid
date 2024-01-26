41854b75e2dac4eaec3ba67741f0a31a07f0767f
BishopVK <danielgraindorge@gmail.com>
2024-01-26 18:26:50 +0100

    Creada la función pero aun con errores

---
+++ Libft/ft_lstmap_bonus.c
@@ -0,0 +1,141 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/26 09:00:41 by danjimen          #+#    #+#             */
+/*   Updated: 2024/01/26 15:18:23 by danjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static void	ft_del_node_content(void *content)
+{
+	if (content != NULL)
+		free(content);
+}
+
+static void	*ft_strlowcase(void *content)
+{
+	if (content != NULL)
+	{
+		while (*((char *)content))
+		{
+			if (*((char *)content) >= 'A' && *((char *)content) <= 'Z')
+				*((char *)content) = *((char *)content) + 32;
+			content = (char *)content + 1;
+		}
+	}
+	return (content);
+}
+
+/* t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*my_list;
+	t_list	*new_node;
+
+	my_list = NULL;
+
+	if (lst != NULL && f != NULL && del != NULL)
+	{
+		while (lst != NULL)
+		{
+			printf("Content: %s\n", (char *)(lst->content));
+			new_node = ft_lstnew(f(lst->content));
+			ft_lstadd_back(&my_list, new_node);
+			my_list->content = f(lst->content);
+			printf("Content: %s\n", (char *)(new_node->content));
+			ft_lstclear(&lst, del);
+			lst = lst->next;
+		}
+	}
+	else
+		my_list = NULL;
+
+	return (my_list);
+} */
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*my_list;
+	t_list	*new_node;
+
+	my_list = NULL;
+	if (lst != NULL && f != NULL && del != NULL)
+	{
+		while (lst != NULL)
+		{
+			new_node = ft_lstnew(f(lst->content));
+			if (new_node == NULL)
+			{
+				ft_lstclear(&my_list, del);
+				return (NULL);
+			}
+			ft_lstadd_back(&my_list, new_node);
+			lst = lst->next;
+		}
+		ft_lstclear(&my_list, del);
+	}
+	else
+		my_list = NULL;
+	return (my_list);
+}
+//cc -Wall -Wextra -Werror -g3 -fsanitize=address ft_lstmap_bonus.c ft_lstadd_back_bonus.c ft_lstclear_bonus.c ft_lstnew_bonus.c ft_strdup.c ft_strlen.c
+int	main(void)
+{
+	// Crear algunos nodos de la lista para el ejemplo
+	t_list	*node1;
+	t_list	*node2;
+	t_list	*node3;
+	t_list	*my_list;
+	t_list	*my_new_list;
+
+	// Crear manualmente los nodos
+	node3 = (t_list *)malloc(sizeof(t_list));
+	node3->content = ft_strdup("NODO 3");
+	node3->next = NULL;
+
+	node2 = (t_list *)malloc(sizeof(t_list));
+	node2->content = ft_strdup("NODO 2");
+	node2->next = node3;
+
+	node1 = (t_list *)malloc(sizeof(t_list));
+	node1->content = ft_strdup("NODO 1");
+	node1->next = node2;
+
+	// Imprimir los contenidos de los nodos
+	printf("Antes de ft_lstmap:\n");
+	my_list = node1;
+	while (my_list != NULL)
+	{
+		printf("Nodo: %p. Content: %s\n", my_list, (char *)(my_list->content));
+		my_list = my_list->next;
+	}
+
+	// Pasa el puntero al primer nodo a ft_lstmap
+	my_new_list = ft_lstmap(node1, ft_strlowcase, ft_del_node_content);
+
+	// Imprimir los contenidos de los nodos de la nueva lista
+	printf("Después de ft_lstmap:\n");
+	while (my_new_list != NULL)
+	{
+		printf("Nodo: %p. Content: %s\n", my_new_list, (char *)(my_new_list->content));
+		my_new_list = my_new_list->next;
+	}
+
+	//¿?
+	// Después de imprimir los contenidos de los nodos, liberar la nueva lista
+	/* free(node1->content);
+	free(node1);
+	free(node2->content);
+	free(node2);
+	free(node3->content);
+	free(node3); */
+	ft_lstclear(&my_list, ft_del_node_content);
+	ft_lstclear(&my_new_list, ft_del_node_content);
+
+	return (0);
+}
