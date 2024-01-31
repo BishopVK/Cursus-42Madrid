efe1eb72f38cee6735f9c7633a28ef4130686b35
BishopVK <danielgraindorge@gmail.com>
2024-01-31 20:19:32 +0100

    Creada función para imprimir unsigned int

---
+++ lvl1/printf/ft_putunbr_printf.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putunbr_printf.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/31 20:10:46 by danjimen          #+#    #+#             */
+/*   Updated: 2024/01/31 20:10:46 by danjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libftprintf.h"
+
+void	ft_putunbr_printf(unsigned int n, size_t *counter)
+{
+	if (n >= 10)
+		ft_putunbr_printf((n / 10), counter);
+	ft_putchar_printf(((n % 10) + '0'), counter);
+}
