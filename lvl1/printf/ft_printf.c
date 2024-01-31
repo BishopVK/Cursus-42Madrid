7c15babf13f13c88fff16bd12e0a793785b57e60
BishopVK <danielgraindorge@gmail.com>
2024-01-31 20:16:22 +0100

    Añadida conversión a string y a unsigned int

--- lvl1/printf/ft_printf.c
+++ lvl1/printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 13:17:43 by danjimen          #+#    #+#             */
-/*   Updated: 2024/01/31 15:07:29 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/01/31 20:15:14 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,18 @@ static void	ft_detect_format(va_list args, char *format, size_t *counter)
 		ft_putnbr_printf(va_arg(args, int), counter);
 	else if (*format == '%')
 		ft_putchar_printf('%', counter);
+	else if (*format == 's')
+		ft_putstr_printf(va_arg(args, char *), counter);
+	else if (*format == 'u')
+		ft_putunbr_printf(va_arg(args, unsigned int), counter);
+	/* else if (*format == 'p')
+		ft_tohex_printf(va_arg(args, int), format, counter);
+	else if (*format == 'x')
+		ft_tohex_printf(va_arg(args, int), format, counter);
+	else if (*format == 'X')
+		ft_tohex_printf(va_arg(args, int), format, counter);
+	else
+		ft_putchar_printf(*format, counter); */
 }
 
 int	ft_printf(char const *format, ...)
@@ -47,18 +59,18 @@ int	ft_printf(char const *format, ...)
 	va_end(args);
 	return (counter);
 }
-
+//cc -Wall -Wextra -Werror *.c && ./a.out && rm a.out
 int	main(void)
 {
 	int	size;
-	// Prueba de caracteres
+
 	char	c = 'D';
-	int		n = -123;
 	printf("CARACTER (c):\n");
 	printf("Printf: %c\n", c);
 	size = ft_printf("ft_printf: %c\n", c);
 	printf("Size: %d\n", size);
 
+	int		n = -123;
 	printf("\nNÚMERO ENTERO (i):\n");
 	printf("Printf: %i\n", n);
 	size = ft_printf("ft_printf: %i\n", n);
@@ -69,14 +81,44 @@ int	main(void)
 	size = ft_printf("ft_printf: %d\n", n);
 	printf("Size: %d\n", size);
 
+	unsigned int	ui = 42;
+	printf("\nUNSIGNED INT (u):\n");
+	printf("Printf: %u\n", ui);
+	size = ft_printf("ft_printf: %u\n", ui);
+	printf("Size: %d\n", size);
+
 	printf("\nPORCENTAJE (%%):\n");
 	printf("Printf: %%\n");
 	size = ft_printf("ft_printf: %%\n");
 	printf("Size: %d\n", size);
 
-	/* printf("");
-	size = ft_printf("");
-	printf("Size: %d", size); */
+	char	*s = "Hola Mundo!";
+	printf("\nSTRING (s):\n");
+	printf("Printf: %s\n", s);
+	size = ft_printf("ft_printf: %s\n", s);
+	printf("Size: %d\n", size);
+
+	char *p = "456";
+	printf("\nPUNTEROS (p):\n");
+	printf("Printf: %p\n", p);
+	//size = ft_printf("ft_printf: %%\n");
+	//printf("Size: %d\n", size);
+
+	int	x = 123;
+	printf("\nHEXADECIMAL EN MINÚSCULAS (x):\n");
+	printf("Printf: %x\n", x);
+	//size = ft_printf("ft_printf: %%\n");
+	//printf("Size: %d\n", size);
+
+	int	xx = 123;
+	printf("\nHEXADECIMAL EN MAYÚSCULAS (X):\n");
+	printf("Printf: %X\n", xx);
+	//size = ft_printf("ft_printf: %%\n");
+	//printf("Size: %d\n", size);
+
+	//printf("");
+	//size = ft_printf("");
+	//printf("Size: %d", size);
 
 	return (0);
 }
