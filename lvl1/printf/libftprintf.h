6c188e0ba93d9722b87d51ea8fd25ff4590d024e
BishopVK <danielgraindorge@gmail.com>
2024-01-31 20:18:10 +0100

    Añadidas funciones ft_putstr_printf y ft_putunbr_printf

--- lvl1/printf/libftprintf.h
+++ lvl1/printf/libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 13:50:32 by danjimen          #+#    #+#             */
-/*   Updated: 2024/01/31 14:22:30 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/01/31 20:14:04 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,7 @@
 int		ft_printf(char const *format, ...);
 void	ft_putchar_printf(char c, size_t *size);
 void	ft_putnbr_printf(int n, size_t *counter);
+void	ft_putstr_printf(char *str, size_t *counter);
+void	ft_putunbr_printf(unsigned int n, size_t *counter);
 
 #endif
