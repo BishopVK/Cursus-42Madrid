4033e8dd4d9d1da442bcc06454a593d5c9474e33
BishopVK <danielgraindorge@gmail.com>
2024-02-01 21:06:43 +0100

    Renambrada la cabecera

--- lvl1/printf/ft_printf.h
+++ lvl1/printf/ft_printf.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/30 13:50:32 by danjimen          #+#    #+#             */
-/*   Updated: 2024/01/31 23:05:53 by danjimen         ###   ########.fr       */
+/*   Created: 2024/02/01 09:09:47 by danjimen          #+#    #+#             */
+/*   Updated: 2024/02/01 09:10:46 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFTPRINTF_H
-# define LIBFTPRINTF_H
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
 # include <stdarg.h>
 # include <stdio.h>
@@ -22,6 +22,6 @@ void	ft_putchar_printf(char c, size_t *size);
 void	ft_putnbr_printf(int n, size_t *counter);
 void	ft_putstr_printf(char *str, size_t *counter);
 void	ft_putunbr_printf(unsigned int n, size_t *counter);
-void	ft_tohex_printf(unsigned int n, char *format, size_t *counter);
+void	ft_tohex_printf(unsigned long n, char *format, size_t *counter);
 
 #endif
