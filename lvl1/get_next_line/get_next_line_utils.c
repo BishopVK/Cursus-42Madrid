cdaa93859f3f9fb549672c0ecc7495c4faa0557a
BishopVK <danielgraindorge@gmail.com>
2024-02-12 21:20:23 +0100

    Añadida función ft_strjoin.c

--- lvl1/get_next_line/get_next_line_utils.c
+++ lvl1/get_next_line/get_next_line_utils.c
@@ -92,3 +92,24 @@ char	*ft_strdup(const char *s)
 	*dest = '\0';
 	return (principio);
 }
+
+//Reserva (con malloc(3)) y devuelve una nueva string,
+//formada por la concatenación de ’s1’ y ’s2’.
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*result;
+	int		i;
+
+	i = 0;
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+	result = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (result == NULL)
+		return (NULL);
+	while (*s1)
+		result[i++] = *s1++;
+	while (*s2)
+		result[i++] = *s2++;
+	result[i] = '\0';
+	return (result);
+}
