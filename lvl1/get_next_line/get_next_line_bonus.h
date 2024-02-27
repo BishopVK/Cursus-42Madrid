7912283dc74d29655a526c3e384aac0c3c8f7b4b
Daniel Jiménez Graindorge <danielgraindorge@gmail.com>
2024-02-27 10:24:47 +0100

    Create Bonus

---
+++ lvl1/get_next_line/get_next_line_bonus.h
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/27 08:13:32 by danjimen          #+#    #+#             */
+/*   Updated: 2024/02/27 10:21:44 by danjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+//read, close
+# include <unistd.h>
+
+//open
+# include <fcntl.h>
+
+//printf
+# include <stdio.h>
+
+//malloc, free
+# include <stdlib.h>
+
+//NULL
+# include <stddef.h>
+
+//strchr, strlen, strdup, strlcpy
+//# include <string.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 100
+# endif
+
+# ifndef MAX_FD
+#  define MAX_FD 1024
+# endif
+
+# define COLOR_CYAN "\x1b[36m"
+# define COLOR_RESET "\x1b[0m"
+
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *str);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+
+#endif
