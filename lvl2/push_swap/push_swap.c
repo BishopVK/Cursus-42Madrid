f698cf5b886295e72c042120c10174eaaa63248d
BishopVK <danielgraindorge@gmail.com>
2024-03-11 21:24:16 +0100

    Crado main para incluir tanto contenido con comillas como sin ellas

--- lvl2/push_swap/push_swap.c
+++ lvl2/push_swap/push_swap.c
@@ -10,7 +10,60 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(void)
-{
+#include "push_swap.h"
+
+int main(int argc, char *argv[]) {
+	int all_quoted;
+	int i;
+	// Verificar si se proporcionaron al menos dos argumentos
+	if (argc < 2) {
+		printf("Error: Debe proporcionar al menos un argumento.\n");
+		return (1); // Devolver un código de error
+	}
+
+	// Verificar si todos los argumentos están entre comillas o ninguno lo está
+	all_quoted = 1;
+	i = 1;
+	while (i < argc)
+	{
+		if (argv[i][0] != '"' || argv[i][strlen(argv[i]) - 1] != '"')
+		{
+			all_quoted = 0;
+			break;
+		}
+		i++;
+	}
+
+	if (all_quoted)
+	{
+		// Si todos los argumentos están entre comillas, imprimir y procesar los valores
+		i = 1;
+		while (i < argc)
+		{
+			char *argument = argv[i];
+			// Eliminar las comillas
+			memmove(argument, argument + 1, strlen(argument));
+			argument[strlen(argument) - 1] = '\0';
+
+			// Ahora, imprimir y procesar los valores separados por espacio
+			char *token = strtok(argument, " ");
+			while (token != NULL)
+			{
+				printf("%s\n", token);
+				token = strtok(NULL, " ");
+			}
+			i++;
+		}
+	}
+	else
+	{
+		// Si no todos los argumentos están entre comillas, imprimir y procesar todos los argumentos
+		i = 1;
+		while (i < argc)
+		{
+			printf("%s\n", argv[i]);
+			i++;
+		}
+	}
 	return (0);
 }
