c8dcdc944f6f60397b635b30fe06d4676281cbb7
BishopVK <danielgraindorge@gmail.com>
2024-03-11 21:23:37 +0100

    Añadidas primeras cabeceras para pruebas del main

---
+++ lvl2/push_swap/push_swap.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/11 19:17:24 by danjimen          #+#    #+#             */
+/*   Updated: 2024/03/11 19:17:24 by danjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+//printf
+# include <stdio.h>
+
+//argc, argv
+# include <stdlib.h>
+
+//NULL
+# include <stddef.h>
+
+//strlen, memmove, strtok
+# include <string.h>
+
+#endif
