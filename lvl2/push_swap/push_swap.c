edca50ca34d0e5b463049439f97a2895cb5b1668
Daniel Jiménez Graindorge <danielgraindorge@gmail.com>
2024-03-14 15:35:11 +0100

    Add files via upload
    Parseado la entradas de diferentes tipos de argumentos

--- lvl2/push_swap/push_swap.c
+++ lvl2/push_swap/push_swap.c
@@ -13,7 +13,58 @@
 #include "push_swap.h"
 
 // cc push_swap.c push_swap.a libft/libft.a printf/libftprintf.a
+//                  n min max
+// ./a.out `jot -r 500 -100 100`
+/* int	compare(const void *a, const void *b)
+{
+	return (*(int *)a - *(int *)b);
+}
+
 int	main(int argc, char *argv[])
+{
+	int	numbers[MAX_NUMBERS];
+	int	i;
+	int	num;
+
+	if (argc < 2)
+	{
+		ft_printf("Uso: %s <num1> <num2> ... <numN>\n", argv[0]);
+		return (1);
+	}
+
+	if (argc - 1 > MAX_NUMBERS)
+	{
+		ft_printf("Se excedió el límite de %d números enteros.\n", MAX_NUMBERS);
+		return (1);
+	}
+	i = 1;
+	while (i < argc)
+	{
+		num = atoi(argv[i]);
+		if (num == 0 && argv[i][0] != '0')
+		{
+			ft_printf("Error: '%s' no es un número entero válido.\n", argv[i]);
+			return (1);
+		}
+		numbers[i - 1] = num;
+		i++;
+	}
+
+	//qsort(numbers, argc - 1, sizeof(int), compare); // Ordenamos los números
+
+	ft_printf("Números ordenados: ");
+	i = 0;
+	while (i < argc - 1)
+	{
+		ft_printf("%d ", numbers[i]);
+		i++;
+	}
+	ft_printf("\n");
+
+	return (0);
+} */
+
+/* int	main(int argc, char *argv[])
 {
 	int		i;
 	int		all_quoted;
@@ -78,9 +129,66 @@ int	main(int argc, char *argv[])
 		i = 1;
 		while (i < argc)
 		{
-			ft_printf("%s\n", argv[i]);
+			ft_printf("%s ", argv[i]);
 			i++;
 		}
 	}
 	return (0);
+} */
+
+int parse_input(char *argv[])
+{
+	int		i;
+	int		j;
+	char	**split;
+	int		num_strings;
+
+
+}
+
+int	main(int argc, char *argv[])
+{
+	int		i;
+	int		j;
+	char	**split;
+	int		num_strings;
+
+	if (argc < 2)
+	{
+		ft_printf("Uso: %s <num1> <num2> ... <numN>\n", argv[0]);
+		return (1);
+	}
+	if (argc >= 2)
+	{
+		i = 1;
+		while (i < argc)
+		{
+			parse_input(*argv);
+			split = ft_split(argv[i], ' '); // Realizar split a cada argumento
+			num_strings = 0;
+			while (split[num_strings] != NULL) // Contar cuántos elementos no nulos hay en frases
+			{
+				num_strings++;
+			}
+			j = 0;
+			while (j < num_strings) // Imprimir las frases almacenadas
+			{
+				/* while (split[j] != '\0')
+				{
+					ft_strchr("0123456789+-", *split[j]);
+					split[j]++;
+				} */
+				printf("split[%d]: %s\n", j, split[j]);
+				j++;
+			}
+			j = 0;
+			while (j < num_strings) // Liberar la memoria asignada a cada split
+			{
+				free(split[j]);
+				j++;
+			}
+			free(split); // Liberar el array bidimensional
+			i++;
+		}
+	}
 }
