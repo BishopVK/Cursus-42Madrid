777db82163f030cd71e82671e590cb001b837ae6
BishopVK <danielgraindorge@gmail.com>
2024-03-15 13:37:31 +0100

    Add parse: not allowed chars

--- lvl2/push_swap/push_swap.c
+++ lvl2/push_swap/push_swap.c
@@ -136,22 +136,91 @@ int	main(int argc, char *argv[])
 	return (0);
 } */
 
-int parse_input(char *argv[])
+static int	allowed_chars(char *argv, char *allowed)
 {
-	int		i;
+	int		j;
+	int		k;
+	int		not_allowed_char;
+
+	j = 0;
+	not_allowed_char = 0;
+	while (argv[j] != '\0')
+	{
+		k = 0;
+		while (allowed[k] != '\0')
+		{
+			if (argv[j] != allowed[k])
+				not_allowed_char++;
+			else
+			{
+				not_allowed_char = 0;
+				break ;
+			}
+			k++;
+		}
+		if (not_allowed_char != 0)
+			return (not_allowed_char);
+		j++;
+	}
+	return (not_allowed_char);
+}
+
+static void	split_argvs(char *argv)
+{
 	int		j;
 	char	**split;
 	int		num_strings;
 
+	split = ft_split(argv, ' '); // Realizar split a cada argumento
+	num_strings = 0;
+	while (split[num_strings] != NULL) // Contar cuántos elementos no nulos hay en frases
+		num_strings++;
+	j = 0;
+	while (j < num_strings) // Imprimir las frases almacenadas
+	{
+		/* while (split[j] != '\0')
+		{
+			ft_strchr("0123456789+-", *split[j]);
+			split[j]++;
+		} */
+		printf("split[%d]: %s\n", j, split[j]);
+		j++;
+	}
+	j = 0;
+	while (j < num_strings) // Liberar la memoria asignada a cada split
+	{
+		free(split[j]);
+		j++;
+	}
+	free(split); // Liberar el array bidimensional
+}
 
+static int	parse_argvs(int argc, char **argv)
+{
+	int	i;
+	int	not_allowed_char;
+	char *allowed;
+
+	allowed = ft_strdup("0123456789 +-");
+	i = 1;
+	while (i < argc)
+	{
+		not_allowed_char = allowed_chars(argv[i++], allowed);
+		if (not_allowed_char != 0)
+		{
+			printf("Has introducido caracteres no permitidos\n");
+			return (1);
+		}
+	}
+	i = 1;
+	while (i < argc)
+		split_argvs(argv[i++]);
+	return (0);
 }
 
 int	main(int argc, char *argv[])
 {
-	int		i;
-	int		j;
-	char	**split;
-	int		num_strings;
+	int	not_allowed_char;
 
 	if (argc < 2)
 	{
@@ -160,35 +229,9 @@ int	main(int argc, char *argv[])
 	}
 	if (argc >= 2)
 	{
-		i = 1;
-		while (i < argc)
-		{
-			parse_input(*argv);
-			split = ft_split(argv[i], ' '); // Realizar split a cada argumento
-			num_strings = 0;
-			while (split[num_strings] != NULL) // Contar cuántos elementos no nulos hay en frases
-			{
-				num_strings++;
-			}
-			j = 0;
-			while (j < num_strings) // Imprimir las frases almacenadas
-			{
-				/* while (split[j] != '\0')
-				{
-					ft_strchr("0123456789+-", *split[j]);
-					split[j]++;
-				} */
-				printf("split[%d]: %s\n", j, split[j]);
-				j++;
-			}
-			j = 0;
-			while (j < num_strings) // Liberar la memoria asignada a cada split
-			{
-				free(split[j]);
-				j++;
-			}
-			free(split); // Liberar el array bidimensional
-			i++;
-		}
+		not_allowed_char = parse_argvs(argc, argv);
+		if (not_allowed_char != 0)
+			return (1);
 	}
+	return (0);
 }
