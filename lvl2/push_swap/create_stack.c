5452cbceab68d7b3e82ea95fe71d2647a75c8e15
BishopVK <danielgraindorge@gmail.com>
2024-03-19 11:32:32 +0100

    Created file to generete stacks

---
+++ lvl2/push_swap/create_stack.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   create_stack.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/18 15:30:23 by danjimen          #+#    #+#             */
+/*   Updated: 2024/03/19 11:22:25 by danjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+/* t_stack_node	*stack_new_node(void *content, void *index)
+{
+	t_stack_node	*new_node;
+
+	new_node = (t_stack_node *)malloc(sizeof(t_stack_node));
+	if (!new_node)
+		return (NULL);
+	new_node->nb = content;
+	new_node->index = index;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	return (new_node);
+}
+
+void	initialize_stack(struct t_stack	*stack)
+{
+	stack->head = NULL;
+	stack->tail = NULL;
+}
+
+t_stack_node	*create_stack(void)
+{
+	t_stack_node	*stack_a;
+	t_stack_node	*stack_b;
+
+	initialize_stack(&stack_a);
+} */
+
