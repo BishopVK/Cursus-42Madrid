cc7e363fa2ee98c5e525f473c687965cee9af983
BishopVK <danielgraindorge@gmail.com>
2024-03-21 16:01:30 +0100

    Added no_repeat_numbers() and stack_clear() functions

--- lvl2/push_swap/create_stack.c
+++ lvl2/push_swap/create_stack.c
@@ -6,11 +6,65 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 15:30:23 by danjimen          #+#    #+#             */
-/*   Updated: 2024/03/20 19:56:18 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/03/21 13:26:12 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+/* void	ft_del_node_content(void *content)
+{
+	if (content != NULL)
+		free(content);
+} */
+
+void	stack_clear(t_stack_node *node)
+{
+	t_stack_node	*current;
+	t_stack_node	*next;
+	//int				*ptr;
+
+	current = node;
+	if (node != NULL)
+	{
+		while (current != NULL)
+		{
+			//ptr = &(current->nb);
+			next = current->next;
+			//ft_del_node_content((void *)ptr);
+			free(current);
+			current = next;
+		}
+		node = NULL;
+	}
+}
+
+void	no_repeat_numbers(t_stack_node *new_node)
+{
+	t_stack_node	*local_node;
+	t_stack_node	*tmp_node;
+
+	local_node = new_node;
+	if (local_node == NULL)
+		ft_printf("Stack vacío\n");
+	else
+	{
+		while (local_node)
+		{
+			tmp_node = local_node->next;
+			while (tmp_node)
+			{
+				if (local_node->nb == tmp_node->nb)
+				{
+					ft_printf("valor duplicado -> %d.\n", local_node->nb);
+					exit(-1);
+				}
+				tmp_node = tmp_node->next;
+			}
+			local_node = local_node->next;
+		}
+	}
+}
 
 void	display(t_stack_node *new_node)
 {
