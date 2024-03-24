7f6a7b51cade6654052b0abc7c096661f3ba021f
BishopVK <danielgraindorge@gmail.com>
2024-03-24 19:01:29 +0100

    Fixed swap() function to receive data by reference instead of value

--- lvl2/push_swap/swap.c
+++ lvl2/push_swap/swap.c
@@ -12,7 +12,43 @@
 
 #include "push_swap.h"
 
-void	swap(t_stack_node *node)
+void	swap(t_stack_node **node)
+{
+	t_stack_node	*first_node;
+	t_stack_node	*second_node;
+
+	if (*node == NULL || (*node)->next == NULL)
+		return ; // No hay suficientes nodos para intercambiar
+	first_node = *node;
+	second_node = (*node)->next;
+	// Cambiar los punteros para intercambiar los nodos
+	first_node->next = second_node->next;
+	second_node->next = first_node;
+	// Actualizar el puntero de la cabeza si los nodos intercambiados son los primeros
+	*node = second_node;
+}
+
+void	sa(t_stack_node **stack_node_a)
+{
+	swap(stack_node_a);
+	ft_printf("sa\n");
+}
+
+void	sb(t_stack_node **stack_node_b)
+{
+	swap(stack_node_b);
+	ft_printf("sb\n");
+}
+
+void	ss(t_stack_node **stack_node_a, t_stack_node **stack_node_b)
+{
+	swap(stack_node_a);
+	swap(stack_node_b);
+	ft_printf("ss\n");
+}
+
+// Swap por referencia y no por valor
+/* void	swap(t_stack_node *node)
 {
 	t_stack_node	*first_node;
 	t_stack_node	*second_node;
@@ -28,23 +64,4 @@ void	swap(t_stack_node *node)
 	tmp = first_node->nb;
 	first_node->nb = second_node->nb;
 	second_node->nb = tmp;
-}
-
-void	sa(t_stack_node *stack_node_a)
-{
-	swap(stack_node_a);
-	ft_printf("sa\n");
-}
-
-void	sb(t_stack_node *stack_node_b)
-{
-	swap(stack_node_b);
-	ft_printf("sb\n");
-}
-
-void	ss(t_stack_node *stack_node_a, t_stack_node *stack_node_b)
-{
-	swap(stack_node_a);
-	swap(stack_node_b);
-	ft_printf("ss\n");
-}
+} */
