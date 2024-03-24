291e24f9013028ea90da112867d578e0176a30be
BishopVK <danielgraindorge@gmail.com>
2024-03-24 23:19:13 +0100

    Without changes

--- lvl2/push_swap/ft_custom_atoi.c
+++ lvl2/push_swap/ft_custom_atoi.c
@@ -21,12 +21,14 @@ int ft_custom_atoi(const char *nptr)
 	sign = 1;
 	result = 0;
 	i = 0;
-	while (nptr[i] == '+' || nptr[i] == '-') {
+	while (nptr[i] == '+' || nptr[i] == '-')
+	{
 		if (nptr[i] == '-')
 			sign *= -1;
 		i++;
 	}
-	while (ft_isdigit(nptr[i])) {
+	while (ft_isdigit(nptr[i]))
+	{
 		result = result * 10 + (nptr[i] - '0');
 		i++;
 	}
