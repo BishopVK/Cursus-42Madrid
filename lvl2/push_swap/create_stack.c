e9cb14a85e979e023bc58178ce3ad0e32b0992a4
BishopVK <danielgraindorge@gmail.com>
2024-03-24 18:37:25 +0100

    Moved no_repeat_numbers() function to parse.c from create_stack.c

--- lvl2/push_swap/create_stack.c
+++ lvl2/push_swap/create_stack.c
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 15:30:23 by danjimen          #+#    #+#             */
-/*   Updated: 2024/03/24 18:21:06 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/03/24 18:35:29 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,33 +27,6 @@ void	stack_clear(t_stack_node **node)
 			current = next;
 		}
 		*node = NULL;
-	}
-}
-
-void	no_repeat_numbers(t_stack_node *new_node)
-{
-	t_stack_node	*local_node;
-	t_stack_node	*tmp_node;
-
-	local_node = new_node;
-	if (local_node == NULL)
-		ft_printf("Stack vacío\n");
-	else
-	{
-		while (local_node)
-		{
-			tmp_node = local_node->next;
-			while (tmp_node)
-			{
-				if (local_node->nb == tmp_node->nb)
-				{
-					ft_printf("valor duplicado -> %d.\n", local_node->nb);
-					exit(-1);
-				}
-				tmp_node = tmp_node->next;
-			}
-			local_node = local_node->next;
-		}
 	}
 }
 
