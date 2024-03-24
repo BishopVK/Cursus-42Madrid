81984720ab11ac4e41657192e4f87c232e8fe97c
BishopVK <danielgraindorge@gmail.com>
2024-03-24 23:13:22 +0100

    Created file to order numbers

---
+++ lvl2/push_swap/sort_numbers.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_numbers.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/24 21:46:50 by danjimen          #+#    #+#             */
+/*   Updated: 2024/03/24 21:46:50 by danjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+/* void	its_ordered(t_stack_node *stack_node_a)
+{
+	
+}
+
+void	sort_numbers(t_stack_node *stack_node_a, t_stack_node *stack_node_b)
+{
+	ordered_numbers(stack_node_a);
+} */
