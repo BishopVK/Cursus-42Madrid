7acb21c64791548f670b9faec4ec9af184d1ddec
BishopVK <danielgraindorge@gmail.com>
2024-04-03 12:09:44 +0200

    Added display_pair() and removed display_by_order()

--- lvl2/push_swap/display.c
+++ lvl2/push_swap/display.c
@@ -12,7 +12,7 @@
 
 #include "push_swap.h"
 
-void	display_by_order(t_stack_node *new_node)
+/* void	display_by_order(t_stack_node *new_node)
 {
 	t_stack_node	*local_node;
 	int				i;
@@ -37,6 +37,24 @@ void	display_by_order(t_stack_node *new_node)
 			}
 			local_node = new_node;
 			i++;
+		}
+	}
+} */
+
+void	display_pair(t_stack_node *new_node)
+{
+	t_stack_node	*local_node;
+
+	local_node = new_node;
+	if (local_node == NULL)
+		ft_printf("Stack vacío\n");
+	else
+	{
+		while (local_node)
+		{
+			ft_printf("valor = %d\n", local_node->nb);
+			ft_printf("pair = %d\n\n", local_node->pair_order);
+			local_node = local_node->next;
 		}
 	}
 }
