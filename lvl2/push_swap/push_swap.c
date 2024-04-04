386e63ad3440732c7d631fa97cf217c64cd9e629
BishopVK <danielgraindorge@gmail.com>
2024-04-04 12:42:44 +0200

    Comments and message if no arguments were entered removed

--- lvl2/push_swap/push_swap.c
+++ lvl2/push_swap/push_swap.c
@@ -12,14 +12,6 @@
 
 #include "push_swap.h"
 
-// cc push_swap.c push_swap.a libft/libft.a printf/libftprintf.a
-//                  n min max
-// ./push_swap `jot -r 500 -100 100`
-// ./push_swap `seq 1 500 | shuf -n 500`
-// ARG="4 67 3 87 23"; ./push_swap $ARG | ./checker/checker_linux $ARG
-// ARG=`jot -r 500 -1000 1000`; ./push_swap $ARG | ./checker/checker_linux $ARG
-// ARG=`seq -10000 10000 | shuf -n 500`; ./push_swap $ARG | ./checker/checker_linux $ARG
-// ((VER LEAKS)) valgrind --leak-check=full ./push_swap <lista_de_numeros>
 int	main(int argc, char *argv[])
 {
 	int				not_allowed_char;
@@ -30,7 +22,6 @@ int	main(int argc, char *argv[])
 	stack_node_b = NULL;
 	if (argc < 2)
 	{
-		ft_printf("Use: %s <num1> <num2> ... <numN>\n", argv[0]);
 		return (1);
 	}
 	if (argc >= 2)
