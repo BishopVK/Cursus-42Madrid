33aabc50dac2e3196f63d286030bd0d6e2993001
BishopVK <danielgraindorge@gmail.com>
2024-04-06 17:44:05 +0200

    Moved ft_put*.c functions to ft_printf_utils.c. Renamed ft_detect_format() by ft_conversions() and moved to ft_converions.c. Created ft_dprintf.c

--- lvl2/push_swap/printf/ft_printf.h
+++ lvl2/push_swap/printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 09:09:47 by danjimen          #+#    #+#             */
-/*   Updated: 2024/04/06 16:41:45 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/04/06 17:33:42 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,28 @@
 //write
 # include <unistd.h>
 
+/****************************************/
+/*				ft_printf				*/
+/****************************************/
 int	ft_printf(char const *format, ...);
-int	ft_putchar_printf(char c, size_t *counter);
-int	ft_putnbr_printf(int n, size_t *counter);
-int	ft_putstr_printf(char *str, size_t *counter);
-int	ft_putunbr_printf(unsigned int n, size_t *counter);
-int	ft_tohex_printf(unsigned long n, char *format, size_t *counter);
+
+/****************************************/
+/*				ft_dprintf				*/
+/****************************************/
+int	ft_dprintf(int fd, char const *format, ...);
+
+/****************************************/
+/*			ft_conversions.c			*/
+/****************************************/
+int	ft_conversions(va_list args, char *format, size_t *ctr, int fd);
+
+/****************************************/
+/*			ft_printf_utils.c			*/
+/****************************************/
+int	ft_putchar_printf(char c, size_t *ctr, int fd);
+int	ft_putnbr_printf(int n, size_t *ctr, int fd);
+int	ft_putstr_printf(char *str, size_t *ctr, int fd);
+int	ft_putunbr_printf(unsigned int n, size_t *ctr, int fd);
+int	ft_tohex_printf(unsigned long n, char *format, size_t *ctr, int fd);
 
 #endif
