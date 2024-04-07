2fd80d81a5de03c9f3cec54d6f76cadd99eb51f5
BishopVK <danielgraindorge@gmail.com>
2024-04-07 10:14:19 +0200

    Moved ft_put*.c functions to ft_printf_utils.c. Renamed ft_detect_format() by ft_conversions() and moved to ft_converions.c. Created ft_dprintf.c

--- lvl1/printf/ft_printf.h
+++ lvl1/printf/ft_printf.h
@@ -6,23 +6,44 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 09:09:47 by danjimen          #+#    #+#             */
-/*   Updated: 2024/02/02 09:35:10 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/04/06 17:33:42 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
+//va_list, va_start, va_arg, va_end
 # include <stdarg.h>
+
+//printf
 # include <stdio.h>
+
+//write
 # include <unistd.h>
 
-int		ft_printf(char const *format, ...);
-void	ft_putchar_printf(char c, size_t *counter, int *write_error);
-void	ft_putnbr_printf(int n, size_t *counter, int *write_error);
-void	ft_putstr_printf(char *str, size_t *counter, int *write_error);
-void	ft_putunbr_printf(unsigned int n, size_t *counter, int *write_error);
-void	ft_tohex_printf(unsigned long n, char *format, size_t *counter,
-			int *write_error);
+/****************************************/
+/*				ft_printf				*/
+/****************************************/
+int	ft_printf(char const *format, ...);
+
+/****************************************/
+/*				ft_dprintf				*/
+/****************************************/
+int	ft_dprintf(int fd, char const *format, ...);
+
+/****************************************/
+/*			ft_conversions.c			*/
+/****************************************/
+int	ft_conversions(va_list args, char *format, size_t *ctr, int fd);
+
+/****************************************/
+/*			ft_printf_utils.c			*/
+/****************************************/
+int	ft_putchar_printf(char c, size_t *ctr, int fd);
+int	ft_putnbr_printf(int n, size_t *ctr, int fd);
+int	ft_putstr_printf(char *str, size_t *ctr, int fd);
+int	ft_putunbr_printf(unsigned int n, size_t *ctr, int fd);
+int	ft_tohex_printf(unsigned long n, char *format, size_t *ctr, int fd);
 
 #endif
