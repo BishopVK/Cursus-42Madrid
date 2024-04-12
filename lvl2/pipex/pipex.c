017c68f59181843297a6f84b5fcfbfe16cefb2c9
Daniel Jimenez Graindorge <danjimen@c2r13s6.42madrid.com>
2024-04-12 14:54:05 +0200

    Added new testing changes

--- lvl2/pipex/pipex.c
+++ lvl2/pipex/pipex.c
@@ -12,7 +12,7 @@
 
 #include "pipex.h"
 
-int	main(int argc, char **argv, char **env)
+/* int	main(int argc, char **argv, char **env)
 {
 	int		fd_infile;
 	pid_t	pid;
@@ -53,60 +53,76 @@ int	main(int argc, char **argv, char **env)
 
 	close(fd_infile);
 	return (0);
-}
-
-/* int main(int argc, char **argv) {
-    if (argc != 5) {
-        fprintf(stderr, "Uso: %s archivo1 comando1 comando2 archivo2\n", argv[0]);
-        return 1;
-    }
-
-    // Abrir archivo1
-    int fd_infile = open(argv[1], O_RDONLY);
-    if (fd_infile == -1) {
-        perror("Error al abrir archivo1");
-        return 1;
-    }
-
-    // Crear pipe
-    int pipe_fd[2];
-    if (pipe(pipe_fd) == -1) {
-        perror("Error al crear el pipe");
-        return 1;
-    }
-
-    // Crear primer proceso hijo
-    pid_t pid1 = fork();
-    if (pid1 == -1) {
-        perror("Error al crear el primer proceso hijo");
-        return 1;
-    } else if (pid1 == 0) {
-        // Código del primer hijo
-    } else {
-        // Código del padre
-        // Crear segundo proceso hijo
-        pid_t pid2 = fork();
-        if (pid2 == -1) {
-            perror("Error al crear el segundo proceso hijo");
-            return 1;
-        } else if (pid2 == 0) {
-            // Código del segundo hijo
-        } else {
-            // Código del padre
-        }
-    }
-
-    // Cerrar descriptores de archivo
-    close(fd_infile);
-    close(pipe_fd[0]);
-    close(pipe_fd[1]);
-
-    // Esperar a que ambos procesos hijos terminen
-    wait(NULL);
-    wait(NULL);
-
-    return 0;
 } */
+
+int	main(int argc, char **argv)
+{
+	int fd_infile;
+	int pipe_fd[2];
+	
+	if (argc != 5)
+	{
+		fprintf(stderr, "Uso: %s archivo1 comando1 comando2 archivo2\n", argv[0]);
+		return 1;
+	}
+
+	// Abrir archivo1
+	fd_infile = open(argv[1], O_RDONLY);
+	if (fd_infile == -1)
+	{
+		perror("Error al abrir archivo1");
+		return 1;
+	}
+
+	// Crear pipe
+	if (pipe(pipe_fd) == -1)
+	{
+		perror("Error al crear el pipe");
+		return 1;
+	}
+
+	// Crear primer proceso hijo
+	pid_t pid1 = fork();
+	if (pid1 == -1)
+	{
+		perror("Error al crear el primer proceso hijo");
+		return 1;
+	}
+	else if (pid1 == 0)
+	{
+		// Código del primer hijo
+	}
+	else
+	{
+		// Código del padre
+		// Crear segundo proceso hijo
+		pid_t pid2 = fork();
+		if (pid2 == -1)
+		{
+			perror("Error al crear el segundo proceso hijo");
+			return 1;
+		}
+		else if (pid2 == 0)
+		{
+			// Código del segundo hijo
+		}
+		else
+		{
+			// Código del padre
+		}
+	}
+
+	// Cerrar descriptores de archivo
+	close(fd_infile);
+	close(pipe_fd[0]);
+	close(pipe_fd[1]);
+
+	// Esperar a que ambos procesos hijos terminen
+	wait(NULL);
+	wait(NULL);
+
+	return 0;
+}
 
 /* int	main(int argc, char **argv, char **env)
 {
