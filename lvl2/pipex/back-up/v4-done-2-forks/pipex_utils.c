1da678a38b9bed3a37cb61d3681e080c03a13dac
BishopVK <danielgraindorge@gmail.com>
2024-04-21 22:47:10 +0200

    Created v4 back-up whit finish work whit 2 forks. Remaining: Norminette

---
+++ lvl2/pipex/back-up/v4-done-2-forks/pipex_utils.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/19 10:50:59 by danjimen          #+#    #+#             */
+/*   Updated: 2024/04/21 21:09:04 by danjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+void	free_split(char **split)
+{
+	int	i;
+
+	if (split == NULL)
+		return ; // No hay nada que liberar
+	i = 0;
+	while (split[i])
+	{
+		free(split[i]);
+		split[i] = NULL;
+		i++;
+	}
+	free(split);
+}
+
+char	*find_command_in_path(const char *command, char **path_list)
+{
+	int		i;
+	char	*full_path;
+
+	i = 0;
+	full_path = NULL;
+	while (path_list[i] != NULL)
+	{
+		full_path = malloc(ft_strlen(path_list[i]) + ft_strlen(command) + 2); // +1 para el '/' y +1 para el '\0'
+		if (full_path == NULL)
+		{
+			perror("malloc failed");
+			exit(1);
+		}
+		ft_strcpy(full_path, path_list[i]);
+		full_path[ft_strlen(path_list[i])] = '/';
+		ft_strcpy(full_path + ft_strlen(path_list[i]) + 1, command);
+		if (access(full_path, X_OK) == 0) // Comprueba si el archivo es ejecutable
+		{
+			//ft_printf("The process \"%s\" is executable\n", command);
+			return (full_path);
+		}
+		free(full_path);
+		i++;
+	}
+	return (NULL); // No se encontró el comando en ninguna de las rutas del PATH
+}
+
+char	**get_path(char **env)
+{
+	int		i;
+	char	*path;
+	char	**split_path;
+
+	i = 0;
+	while (env[i] != NULL)
+	{
+		path = ft_strnstr(env[i], "PATH=", ft_strlen(env[i]));
+		if (path != NULL)
+		{
+			path += 5;
+			break ;
+		}
+		i++;
+	}
+	// Separar path por ':'
+	split_path = ft_split(path, ':');
+	return (split_path);
+}
+
+int	open_fd(char *file_name, int stdin_stdout)
+{
+	int	fd;
+
+	if (stdin_stdout == 0)
+		fd = open(file_name, O_RDONLY, 0777);
+	if (stdin_stdout == 1)
+		fd = open(file_name, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (fd < 0)
+	{
+		perror("open");
+		exit (0);
+	}
+	return (fd);
+}
