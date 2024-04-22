510f8f12ecb5fd6e53413936da554b9bc06c865b
Daniel Jimenez Graindorge <danjimen@c2r13s6.42madrid.com>
2024-04-22 13:01:01 +0200

    Norminette fixed

--- lvl2/pipex/pipex.h
+++ lvl2/pipex/pipex.h
@@ -14,34 +14,34 @@
 # define PIPEX_H
 
 //libft
-#include "libft/libft.h"
+# include "libft/libft.h"
 
 //ft_printf, ft_dprintf
-#include "printf/ft_printf.h"
+# include "printf/ft_printf.h"
 
 //open, wait, waitpid
-#include <sys/types.h>
+# include <sys/types.h>
 
 //close, read, access, dup, dup2, execve, fork, pipe, unlink
-#include <unistd.h>
+# include <unistd.h>
 
 //malloc, free, exit
-#include <stdlib.h>
+# include <stdlib.h>
 
 //perror
-#include <stdio.h>
+# include <stdio.h>
 
 //strerror
-#include <string.h>
+# include <string.h>
 
 //wait, waitpid
-#include <sys/wait.h>
+# include <sys/wait.h>
 
 //index pipe extremo escritura
-#define READ_END 0
+# define READ_END 0
 
 //index pipe extremo lectura
-#define WRITE_END 1
+# define WRITE_END 1
 
 /************************************/
 /*				pipex.c				*/
@@ -49,6 +49,7 @@
 void	execute(char **split_argv, char *full_path, char **env);
 void	child1(char **argv, int *p_fd, char **env);
 void	child2(char **argv, int *p_fd, char **env);
+void	second_fork(char **argv, char **env, int *p_fd, pid_t pid1);
 int		main(int argc, char **argv, char **env);
 
 /************************************/
