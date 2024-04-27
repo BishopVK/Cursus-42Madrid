241a10819e7d1bcb9ec22d6481a9258a08eb39ed
BishopVK <danielgraindorge@gmail.com>
2024-04-27 23:27:55 +0200

    Added complet implementation of here:doc function

--- lvl2/pipex/here_doc_bonus.c
+++ lvl2/pipex/here_doc_bonus.c
@@ -12,33 +12,40 @@
 
 #include "pipex_bonus.h"
 
-int here_doc(int argc, char *argv[])
+int	here_doc(char *limit)
 {
-	if (argc != 2) {
-		printf("Uso: %s here_doc\n", argv[0]);
-		return 1;
-	}
+	char	buffer[1024];
+	ssize_t	bytes_read;
+	int		fd;
+	char	*eof;
 
-	if (strcmp(argv[1], "here_doc") != 0) {
-		printf("El primer argumento debe ser \"here_doc\"\n");
-		return 1;
-	}
-
-	char buffer[1024];
-	ssize_t bytes_read;
-	while ((bytes_read = read(STDIN_FILENO, buffer, sizeof(buffer))) > 0) {
+	eof = (char *)ft_calloc((ft_strlen(limit) + 2), sizeof(char));
+	if (eof == NULL)
+		exit (1);
+	ft_strcpy(eof, limit);
+	eof[ft_strlen(limit)] = '\n';
+	fd = open_fd("tmp/tmp.txt", 11);
+	while (1)
+	{
+		bytes_read = read(STDIN_FILENO, buffer, sizeof(buffer));
+		
 		if (bytes_read == -1) {
 			perror("Error al leer de stdin");
-			return 1;
+			return (1);
 		}
-		if (write(STDOUT_FILENO, buffer, bytes_read) != bytes_read) {
-			perror("Error al escribir en stdout");
-			return 1;
+		if (bytes_read == 0)
+			break ;
+		if (ft_memcmp(buffer, eof, bytes_read) == 0)
+		{
+			// Se encontró EOF, termina el bucle
+			free(eof);
+			break ;
 		}
-		if (strcmp(buffer, "EOF\n") == 0) {
-			break;
+		if (write(fd, buffer, bytes_read) != bytes_read)
+		{
+			perror("Error al escribir en el fd");
+			return (1);
 		}
 	}
-
-	return 0;
+	return (fd);
 }
