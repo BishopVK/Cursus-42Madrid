53b0b8d1e10d100431f8707417b0ee03d58d7602
BishopVK <danielgraindorge@gmail.com>
2024-04-29 21:26:07 +0200

    Added more tests

--- lvl2/pipex/testing/increment.c
+++ lvl2/pipex/testing/increment.c
@@ -12,6 +12,20 @@
 
 #include <stdio.h>
 
+void	my_second_function(int *i)
+{
+	printf("i6 = %d\n", *i);
+	(*i)++;
+	printf("i7 = %d\n", *i);
+	(*i)++;
+	printf("i8 = %d\n", *i);
+	(*i)++;
+	printf("i9 = %d\n", *i);
+	(*i)++;
+	printf("i10 = %d\n", *i);
+	(*i)++;
+}
+
 void	my_function(int *i)
 {
 	printf("i1 = %d\n", *i);
@@ -24,6 +38,7 @@ void	my_function(int *i)
 	(*i)++;
 	printf("i5 = %d\n", *i);
 	(*i)++;
+	my_second_function(i);
 }
 
 int	main(void)
