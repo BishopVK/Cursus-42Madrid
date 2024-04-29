aa3f668a0a78d3187a0824dc09aaa72578ff5bee
BishopVK <danielgraindorge@gmail.com>
2024-04-29 18:53:54 +0200

    Moved execute() to pipex_utils_bonus.c. Modified prototype of some functions. Nos first_child() work whit or whitout here_doc

--- lvl2/pipex/pipex_bonus.c
+++ lvl2/pipex/pipex_bonus.c
@@ -12,36 +12,42 @@
 
 #include "pipex_bonus.h"
 
-void	execute(char **split_argv, char *full_path, char **env)
-{
-	if (full_path != NULL && access(full_path, X_OK) == 0)
-	{
-		execve(full_path, split_argv, env);
-		perror("execve failed");
-		exit (127);
-	}
-	else
-	{
-		if (full_path == NULL)
-		{
-			ft_dprintf(2, "The full path of the command was not found\n");
-			exit (127);
-		}
-		else
-		{
-			ft_dprintf(2, "The command is not accessible\n");
-			exit (127);
-		}
-	}
-}
-
-void	child1(char **argv, int *p_fd, char **env)
+void	fitst_child(char **argv, int *p_fd, char **env, int *i)
 {
 	char	**split_path;
 	char	**split_argv;
 	char	*full_path;
 	int		fd;
 
+	if (ft_strcmp(argv[(*i)++], "here_doc") == 0)
+		fd = open_fd("tmp/tmp.txt", 0);
+	else
+		fd = open_fd(argv[(--(*i))], 0);
+	dup2(fd, STDIN_FILENO);
+	close(fd);
+	unlink("tmp/tmp.txt");
+	close(p_fd[0]);
+	dup2(p_fd[1], STDOUT_FILENO);
+	close(p_fd[1]);
+	if (ft_strncmp(argv[++(*i)], "awk", 3) == 0)
+		split_argv = ft_split_awk(argv[*i], ' ');
+	else
+		split_argv = ft_split(argv[*i], ' ');
+	split_path = get_path(env);
+	full_path = find_command_in_path(split_argv[0], split_path);
+	free_split(split_path);
+	execute(split_argv, full_path, env);
+	free_split(split_argv);
+}
+
+/* void	middle_child(char **argv, int *p_fd, char **env)
+{
+	char	**split_path;
+	char	**split_argv;
+	char	*full_path;
+	int		fd;
+
+	if (ft_strcmp(argv[i], "here_doc") == 0)
 	fd = open_fd(argv[1], 0);
 	dup2(fd, STDIN_FILENO);
 	close(fd);
@@ -57,9 +63,9 @@ void	child1(char **argv, int *p_fd, char **env)
 	free_split(split_path);
 	execute(split_argv, full_path, env);
 	free_split(split_argv);
-}
+} */
 
-void	child2(char **argv, int *p_fd, char **env)
+void	last_child(char **argv, int *p_fd, char **env)
 {
 	char	**split_path;
 	char	**split_argv;
@@ -96,13 +102,15 @@ int	second_fork(char **argv, char **env, int *p_fd, pid_t pid1)
 	if (ft_strcmp(argv[1], "here_doc") == 0)
 		here_doc_child2(argv, p_fd, env);
 	else
-		child2(argv, p_fd, env);
+		last_child(argv, p_fd, env);
 	else if (pid2 > 0)
 	{
 		close(p_fd[0]);
 		close(p_fd[1]);
-		waitpid(pid1, &status, 0);
-		waitpid(pid2, &status, 0);
+		(void)pid1;
+		wait(&status);
+		// waitpid(pid1, &status, 0);
+		// waitpid(pid2, &status, 0);
 		status = WEXITSTATUS(status);
 	}
 	return (status);
@@ -112,24 +120,23 @@ int	main(int argc, char **argv, char **env)
 {
 	int		p_fd[2];
 	pid_t	pid1;
+	int	i;
 
+	i = 1;
 	if (argc < 5)
 	{
 		ft_dprintf(2, "Correct use: ./pipex infile \"cmd1\" \"cmd2\" outfile\n"
 			"Or: ./pipex here_doc LIMITADOR \"cmd\" \"cmd1\" outfile\n");
 		return (-1);
 	}
-	here_doc(argv);
+	here_doc(argv, &i);
 	if (pipe(p_fd) == -1)
 		exit(-1);
 	pid1 = fork();
 	if (pid1 == -1)
 		exit(-1);
 	if (pid1 == 0)
-	if (ft_strcmp(argv[1], "here_doc") == 0)
-		here_doc_child1(argv, p_fd, env);
-	else
-		child1(argv, p_fd, env);
+	fitst_child(argv, p_fd, env, &i);
 	else if (pid1 > 0)
 		return (second_fork(argv, env, p_fd, pid1));
 	return (0);
