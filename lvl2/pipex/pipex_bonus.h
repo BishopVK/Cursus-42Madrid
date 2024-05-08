e170597774b09bfcf6ff5becc6c8e81116ebf572
BishopVK <danielgraindorge@gmail.com>
2024-05-08 19:44:02 +0200

    Added define_and_validate_args() function

--- lvl2/pipex/pipex_bonus.h
+++ lvl2/pipex/pipex_bonus.h
@@ -81,6 +81,7 @@ char	**ft_split_awk(char const *s, char c);
 /************************************/
 /*			pipex_bonus_extra.c		*/
 /************************************/
+int		define_and_validate_args(int argc, char **argv, char **env, t_child_args *args);
 void	free_pipefd(char **pipefd, int num_cmds);
 int		**alloc_pipefd(int num_cmds);
 
