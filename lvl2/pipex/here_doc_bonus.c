6b588e82dd0c848b493b4c42bcb5a371e7789e2d
Daniel Jimenez Graindorge <danjimen@c1r11s6.42madrid.com>
2024-05-08 11:51:54 +0200

    Deleted here_doc_child1() and here_doc_child2() functions

--- lvl2/pipex/here_doc_bonus.c
+++ lvl2/pipex/here_doc_bonus.c
@@ -12,55 +12,6 @@
 
 #include "pipex_bonus.h"
 
-/* void	here_doc_child1(char **argv, int *p_fd, char **env)
-{
-	char	**split_path;
-	char	**split_argv;
-	char	*full_path;
-	int		fd;
-
-	fd = open_fd("tmp/tmp.txt", 0);
-	dup2(fd, STDIN_FILENO);
-	close(fd);
-	unlink("tmp/tmp.txt");
-	close(p_fd[0]);
-	dup2(p_fd[1], STDOUT_FILENO);
-	close(p_fd[1]);
-	if (ft_strncmp(argv[3], "awk", 3) == 0)
-		split_argv = ft_split_awk(argv[3], ' ');
-	else
-		split_argv = ft_split(argv[3], ' ');
-	split_path = get_path(env);
-	full_path = find_command_in_path(split_argv[0], split_path);
-	free_split(split_path);
-	execute(split_argv, full_path, env);
-	free_split(split_argv);
-} */
-
-/* void	here_doc_child2(char **argv, int *p_fd, char **env)
-{
-	char	**split_path;
-	char	**split_argv;
-	char	*full_path;
-	int		fd;
-
-	fd = open_fd(argv[5], 11);
-	dup2(fd, STDOUT_FILENO);
-	close(fd);
-	close(p_fd[1]);
-	dup2(p_fd[0], STDIN_FILENO);
-	close(p_fd[0]);
-	if (ft_strncmp(argv[4], "awk", 3) == 0)
-		split_argv = ft_split_awk(argv[4], ' ');
-	else
-		split_argv = ft_split(argv[4], ' ');
-	split_path = get_path(env);
-	full_path = find_command_in_path(split_argv[0], split_path);
-	free_split(split_path);
-	execute(split_argv, full_path, env);
-	free_split(split_argv);
-} */
-
 static void	create_fd(char *eof, int fd)
 {
 	char	buffer[BUFFER_SIZE];
@@ -69,8 +20,8 @@ static void	create_fd(char *eof, int fd)
 	while (1)
 	{
 		bytes_read = read(STDIN_FILENO, buffer, sizeof(buffer));
-		
-		if (bytes_read == -1) {
+		if (bytes_read == -1)
+		{
 			perror("Error al leer de stdin");
 			exit (1);
 		}
@@ -78,8 +29,7 @@ static void	create_fd(char *eof, int fd)
 			break ;
 		if (ft_memcmp(buffer, eof, bytes_read) == 0)
 		{
-			// Se encontró EOF, termina el bucle
-			free(eof);
+			free(eof); // Se encontró EOF, termina el bucle
 			break ;
 		}
 		if (write(fd, buffer, bytes_read) != bytes_read)
