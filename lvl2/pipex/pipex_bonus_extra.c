7fdd04398db0be3a7056f014de7ce69ebe53f739
BishopVK <danielgraindorge@gmail.com>
2024-05-08 19:46:48 +0200

    Moved to define_and_validate_args() function the valitaation and definiton of args from main() function

--- lvl2/pipex/pipex_bonus_extra.c
+++ lvl2/pipex/pipex_bonus_extra.c
@@ -6,11 +6,32 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 13:08:57 by danjimen          #+#    #+#             */
-/*   Updated: 2024/05/08 13:08:59 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/05/08 19:42:06 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex_bonus.h"
+
+int	define_and_validate_args(int argc, char **argv, char **env, t_child_args *args)
+{
+	int	num_cmds;
+
+	args->argv = argv;
+	args->env = env;
+	args->i = 0;
+	if (argc < 5)
+	{
+		ft_dprintf(2, "Correct use: ./pipex infile \"cmd1\" \"cmd2\" outfile\n"
+			"Or: ./pipex here_doc LIMITADOR \"cmd\" \"cmd1\" outfile\n");
+		exit (-1);
+	}
+	here_doc(argv);
+	if (ft_strcmp(args->argv[1], "here_doc") == 0)
+		num_cmds = argc - 4;
+	else
+		num_cmds = argc - 3;
+	return (num_cmds);
+}
 
 void	free_pipefd(char **pipefd, int num_cmds)
 {
