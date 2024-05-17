30ae397ee25bd6ba3d9ef9628f50b51022058303
Daniel Jimenez Graindorge <danjimen@c1r12s3.42madrid.com>
2024-05-17 11:31:45 +0200

    Added get_next_line

---
+++ lvl2/so_long/get_next_line/get_next_line.h
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/07 11:29:37 by danjimen          #+#    #+#             */
+/*   Updated: 2024/02/07 11:29:37 by danjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+//read, close
+# include <unistd.h>
+
+//open
+# include <fcntl.h>
+
+//printf
+# include <stdio.h>
+
+//malloc, free
+# include <stdlib.h>
+
+//NULL
+# include <stddef.h>
+
+//strchr, strlen, strdup, strlcpy
+//# include <string.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 100
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *str);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+
+#endif
