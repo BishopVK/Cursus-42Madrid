2eb67870e9a98506049e8e6a7316eb8155a3d196
Daniel Jimenez Graindorge <danjimen@c1r12s3.42madrid.com>
2024-05-21 08:00:49 +0200

    Norminette fixed

--- lvl2/so_long/so_long.c
+++ lvl2/so_long/so_long.c
@@ -24,8 +24,8 @@ int	main(int argc, char *argv[])
 	return (0);
 }
 
-
-//cc -I /usr/local/include main.c -L /usr/local/lib/ -lmlx -framework OpenGL -framework AppKit
+// cc -I /usr/local/include main.c -L /usr/local/lib/ -lmlx
+// -framework OpenGL -framework AppKit
 /* int	main(void)
 {
 	void	*mlx_ptr;
