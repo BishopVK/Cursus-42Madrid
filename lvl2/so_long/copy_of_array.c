2ae8a0de76656753a7bcaf707bd330d13b0dad44
Daniel Jimenez Graindorge <danjimen@c1r12s3.42madrid.com>
2024-05-23 09:08:35 +0200

    Norminette fixed

--- lvl2/so_long/copy_of_array.c
+++ lvl2/so_long/copy_of_array.c
@@ -6,24 +6,37 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 18:53:08 by danjimen          #+#    #+#             */
-/*   Updated: 2024/05/22 23:27:30 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/05/23 09:08:14 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-char	**create_array_copy(char **originalMap, int width, int height) {
-	char **copy = (char **)malloc(height * sizeof(char *));
-	for (int i = 0; i < height; i++) {
+char	**create_array_copy(char **originalMap, int width, int height)
+{
+	int	i;
+	char **copy;
+
+	i = 0;
+	copy = (char **)malloc(height * sizeof(char *));
+	while (i < height)
+	{
 		copy[i] = (char *)malloc(width * sizeof(char));
 		ft_strcpy(copy[i], originalMap[i]);
+		i++;
 	}
-	return copy;
+	return (copy);
 }
 
-void	freeMapCopy(char **map, int height) {
-	for (int i = 0; i < height; i++) {
+void	freeMapCopy(char **map, int height)
+{
+	int	i;
+
+	i = 0;
+	while (i < height)
+	{
 		free(map[i]);
+		i++;
 	}
 	free(map);
 }
