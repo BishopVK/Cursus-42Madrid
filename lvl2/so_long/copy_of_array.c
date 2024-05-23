5116afa3ddc079241970bfc25b2e9ec3a6f261ee
Daniel Jimenez Graindorge <danjimen@c1r12s3.42madrid.com>
2024-05-23 13:23:59 +0200

    fixed bugs of function create_array_copy and added control structure in case of malloc fails

--- lvl2/so_long/copy_of_array.c
+++ lvl2/so_long/copy_of_array.c
@@ -6,37 +6,37 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 18:53:08 by danjimen          #+#    #+#             */
-/*   Updated: 2024/05/23 09:14:57 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/05/23 12:49:10 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-char	**create_array_copy(char **originalMap, int width, int height)
+char	**create_array_copy(t_map_array *map_array)
 {
-	int	i;
-	char **copy;
+	int		i;
+	int		j;
+	char	**copy;
 
 	i = 0;
-	copy = (char **)malloc(height * sizeof(char *));
-	while (i < height)
+	copy = (char **)malloc((map_array->height + 1) * sizeof(char *));
+	while (i < map_array->height)
 	{
-		copy[i] = (char *)malloc(width * sizeof(char));
-		ft_strcpy(copy[i], originalMap[i]);
+		copy[i] = (char *)malloc(map_array->width + 1);
+		if (!copy[i])
+		{
+			j = 0;
+			while (j < i)
+			{
+				free(copy[j]);
+				j++;
+			}
+			free(copy);
+			return (NULL);
+		}
+		ft_strcpy(copy[i], map_array->map[i]);
 		i++;
 	}
+	copy[i] = NULL;
 	return (copy);
 }
-
-/* void	free_map_copy(char **map, int height)
-{
-	int	i;
-
-	i = 0;
-	while (i < height)
-	{
-		free(map[i]);
-		i++;
-	}
-	free(map);
-} */
