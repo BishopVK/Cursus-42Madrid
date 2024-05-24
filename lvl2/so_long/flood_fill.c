7d6cc8ccfe4933bdd62d666f849617b1b6335962
Daniel Jimenez Graindorge <danjimen@c1r12s3.42madrid.com>
2024-05-24 14:30:19 +0200

    Created detect_player() function

--- lvl2/so_long/flood_fill.c
+++ lvl2/so_long/flood_fill.c
@@ -6,13 +6,30 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 08:46:25 by danjimen          #+#    #+#             */
-/*   Updated: 2024/05/24 11:44:27 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/05/24 14:20:16 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	flood_fill(t_map_array	*map)
+void	detect_player(char *buffer, t_map_array *map_array, int map_lines)
+{
+	int	i;
+
+	i = 0;
+	while (buffer[i])
+	{
+		if (buffer[i] == 'P')
+		{
+			map_array->startX = i;
+			map_array->startY = map_lines;
+			break ;
+		}
+		i++;
+	}
+}
+
+/* void	flood_fill(t_map_array	*map)
 {
 	detect_player(&map);
-}
+} */
