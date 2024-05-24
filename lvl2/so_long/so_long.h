19bcdb19578a05e4538a955d794e47c8ba1d2600
Daniel Jimenez Graindorge <danjimen@c1r12s3.42madrid.com>
2024-05-24 11:51:28 +0200

    Added flood_fill() prototye

--- lvl2/so_long/so_long.h
+++ lvl2/so_long/so_long.h
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 13:33:23 by danjimen          #+#    #+#             */
-/*   Updated: 2024/05/23 09:36:50 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/05/24 09:44:31 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,5 +90,10 @@ void	read_to_create_array(char *map, t_map_array *map_array);
 /*			copy_of_array.c				*/
 /****************************************/
 char	**create_array_copy(t_map_array *map_array);
+
+/****************************************/
+/*			flood_fill.c				*/
+/****************************************/
+void	flood_fill(t_map_array	copy_array);
 
 #endif
