6cb8d4fb4624fb8c9d69db6ad8b8b867be4fd16c
BishopVK <danielgraindorge@gmail.com>
2024-05-26 16:13:33 +0200

    Created copy_chars, added call to flood_fill() and added prints to debug

--- lvl2/so_long/so_long.c
+++ lvl2/so_long/so_long.c
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 13:39:00 by danjimen          #+#    #+#             */
-/*   Updated: 2024/05/24 15:09:14 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/05/26 16:06:59 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,12 @@ int	main(int argc, char *argv[])
 {
 	t_map_chars	map_chars;
 	t_map_array	map_array;
+	t_map_chars	copy_chars;
 	t_map_array	copy_array;
 
 	ft_memset(&map_chars, 0, sizeof(t_map_chars));
 	ft_memset(&map_array, 0, sizeof(t_map_array));
+	ft_memset(&copy_chars, 0, sizeof(t_map_chars));
 	ft_memset(&copy_array, 0, sizeof(t_map_array));
 	if (argc != 2)
 	{
@@ -37,13 +39,23 @@ int	main(int argc, char *argv[])
 	ft_printf("Player = (%i,%i)\n", map_array.startX, map_array.startY);
 
 	ft_printf("-----------\n");
+	create_copy_map_chars(&map_chars, &copy_chars);
 	create_array_copy(&map_array, &copy_array);
 	ft_printf("copy:\n");
 	display_array(&copy_array);
+	ft_printf("copy_array->map[x][y] ==> %c\n", copy_array.map[3][1]);
 	ft_printf("Player_copy = (%i,%i)\n", copy_array.startX, copy_array.startY);
 
 
-	//flood_fill(&copy_array);
+	ft_printf("copy_chars.collectible = %i\n", copy_chars.collectible);
+	ft_printf("copy_chars.player = %i\n", copy_chars.player);
+	ft_printf("copy_chars.exit = %i\n", copy_chars.exit);
+	flood_fill(&copy_chars, &copy_array, copy_array.startY, copy_array.startX);
+	ft_printf("flood_fill:\n");
+	display_array(&copy_array);
+	ft_printf("copy_chars.collectible = %i\n", copy_chars.collectible);
+	ft_printf("copy_chars.player = %i\n", copy_chars.player);
+	ft_printf("copy_chars.exit = %i\n", copy_chars.exit);
 
 	free_array(&map_array);
 	free_array(&copy_array);
