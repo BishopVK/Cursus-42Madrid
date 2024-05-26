ae687e3d755ef637270878f35acefd87de913f88
BishopVK <danielgraindorge@gmail.com>
2024-05-26 16:14:58 +0200

    Fixed flood_fill() function whit debugging prints

--- lvl2/so_long/flood_fill.c
+++ lvl2/so_long/flood_fill.c
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 08:46:25 by danjimen          #+#    #+#             */
-/*   Updated: 2024/05/24 14:20:16 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/05/26 16:05:18 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,32 @@ void	detect_player(char *buffer, t_map_array *map_array, int map_lines)
 	}
 }
 
-/* void	flood_fill(t_map_array	*map)
+void	flood_fill(t_map_chars *copy_chars, t_map_array *copy_array, int x, int y)
 {
-	detect_player(&map);
-} */
+	// ft_printf("x ==> %i\n", x);
+	// ft_printf("y ==> %i\n", y);
+	// Base cases
+	//ft_printf("copy_array->map[x][y] ==> %c\n", copy_array->map[1][3]);
+	if (x < 1 || x >= copy_array->height || y < 1 || y >= copy_array->width
+			|| copy_array->map[x][y] == '1' || copy_array->map[x][y] == 'V')
+	{
+		// ft_printf("Entré!:\n");
+		// ft_printf("copy_array->map[x][y] ==> %c\n", copy_array->map[3][1]);
+		return ;
+	}
+
+	// Mark the cell as visited
+	if (copy_array->map[x][y] == 'P')
+		copy_chars->player--;
+	else if (copy_array->map[x][y] == 'E')
+		copy_chars->exit--;
+	else if (copy_array->map[x][y] == 'C')
+		copy_chars->collectible--;
+	copy_array->map[x][y] = 'V';
+
+	// Recursively apply flood fill to neighboring cells
+	flood_fill(copy_chars, copy_array, x + 1, y);
+	flood_fill(copy_chars, copy_array, x - 1, y);
+	flood_fill(copy_chars, copy_array, x, y + 1);
+	flood_fill(copy_chars, copy_array, x, y - 1);
+}
