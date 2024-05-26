a78fdb4a315696129a37fc58ccd1f1c49cbcf602
BishopVK <danielgraindorge@gmail.com>
2024-05-26 18:32:06 +0200

    Created v3 back-up whit flood-fill finish

---
+++ lvl2/so_long/back-up/v3-flood-fill-done/flood_fill.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   flood_fill.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/24 08:46:25 by danjimen          #+#    #+#             */
+/*   Updated: 2024/05/26 16:05:18 by danjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void	detect_player(char *buffer, t_map_array *map_array, int map_lines)
+{
+	int	i;
+
+	i = 0;
+	while (buffer[i])
+	{
+		if (buffer[i] == 'P')
+		{
+			map_array->startX = i;
+			map_array->startY = map_lines;
+			break ;
+		}
+		i++;
+	}
+}
+
+void	flood_fill(t_map_chars *copy_chars, t_map_array *copy_array, int x, int y)
+{
+	// ft_printf("x ==> %i\n", x);
+	// ft_printf("y ==> %i\n", y);
+	// Base cases
+	//ft_printf("copy_array->map[x][y] ==> %c\n", copy_array->map[1][3]);
+	if (x < 1 || x >= copy_array->height || y < 1 || y >= copy_array->width
+			|| copy_array->map[x][y] == '1' || copy_array->map[x][y] == 'V')
+	{
+		// ft_printf("Entré!:\n");
+		// ft_printf("copy_array->map[x][y] ==> %c\n", copy_array->map[3][1]);
+		return ;
+	}
+
+	// Mark the cell as visited
+	if (copy_array->map[x][y] == 'P')
+		copy_chars->player--;
+	else if (copy_array->map[x][y] == 'E')
+		copy_chars->exit--;
+	else if (copy_array->map[x][y] == 'C')
+		copy_chars->collectible--;
+	copy_array->map[x][y] = 'V';
+
+	// Recursively apply flood fill to neighboring cells
+	flood_fill(copy_chars, copy_array, x + 1, y);
+	flood_fill(copy_chars, copy_array, x - 1, y);
+	flood_fill(copy_chars, copy_array, x, y + 1);
+	flood_fill(copy_chars, copy_array, x, y - 1);
+}
---
+++ lvl2/so_long/back-up/v3-flood-fill-done/so_long.h
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/16 13:33:23 by danjimen          #+#    #+#             */
+/*   Updated: 2024/05/26 16:00:07 by danjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+//libft
+# include "libft/libft.h"
+
+//ft_printf, ft_dprintf
+# include "printf/ft_printf.h"
+
+//get_next_line
+# include "get_next_line/get_next_line.h"
+
+//mlx
+# include "minilibx-linux/mlx.h"
+
+//open
+# include <sys/types.h>
+
+//close, read
+# include <unistd.h>
+
+//malloc, free, exit
+# include <stdlib.h>
+
+//perror
+# include <stdio.h>
+
+//strerror
+# include <string.h>
+
+typedef struct s_map_chars
+{
+	int	empty;
+	int	wall;
+	int	collectible;
+	int	exit;
+	int	player;
+}	t_map_chars;
+
+typedef struct s_map_array
+{
+	int		width;
+	int		height;
+	int		startX;
+	int		startY;
+	char	**map;
+}	t_map_array;
+
+/****************************************/
+/*				parse.c					*/
+/****************************************/
+void	exit_map_error(char *buffer, char *message);
+int		count_buffer_len(char *buffer);
+int		read_map_lines(char *buffer, char *map);
+void	read_map(char *map, t_map_chars *map_chars, t_map_array *map_array);
+void	check_arg_extension(char *map);
+
+/****************************************/
+/*		check_map_characters.c			*/
+/****************************************/
+void	check_nbr_chars(char *buffer, t_map_chars *map_chars);
+void	count_nbr_chars(char *buffer, t_map_chars *map_chars);
+void	check_borders(char *buffer, int total_lines, int map_line);
+int		read_for_check_borders(char *buffer, char *map, int map_lines,
+			t_map_chars *map_chars);
+void	check_map_characters(char *buffer, char *allowed);
+
+/****************************************/
+/*				array.c					*/
+/****************************************/
+void	free_array(t_map_array *s_map_array);
+void	display_array(t_map_array *map_array);
+void	initialize_array(char *buffer, t_map_array *map_array, int map_lines);
+void	create_array(t_map_array *map_array);
+void	read_to_create_array(char *map, t_map_array *map_array);
+
+/****************************************/
+/*			copy_of_array.c				*/
+/****************************************/
+void	create_copy_map_chars(t_map_chars *map_chars, t_map_chars *copy_chars);
+void	initialize_copy_array(t_map_array *map_array, t_map_array *copy_array);
+void	create_array_copy(t_map_array *map_array, t_map_array *copy_array);
+
+/****************************************/
+/*			flood_fill.c				*/
+/****************************************/
+void	detect_player(char *buffer, t_map_array *map_array, int map_lines);
+void	flood_fill(t_map_chars *copy_chars, t_map_array *copy_array, int x, int y);
+
+#endif
