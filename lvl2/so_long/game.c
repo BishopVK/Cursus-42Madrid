db31f166f9f36586debae51cdbc011022bc335ce
BishopVK <danielgraindorge@gmail.com>
2024-05-31 23:53:41 +0200

    Fixed put_images() and get_images_and_paths() functions

--- lvl2/so_long/game.c
+++ lvl2/so_long/game.c
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 20:59:27 by danjimen          #+#    #+#             */
-/*   Updated: 2024/05/31 15:05:36 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/05/31 23:50:16 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,47 +28,75 @@ int	on_keypress(int keysym, t_data *data)
 	return (0);
 }
 
-void	put_images(t_data *data)
+void	get_images_and_paths(t_data *data)
 {
-	data->img = (t_img *)malloc(sizeof(t_img));
-	data->img->player_path = "./textures/xpm/P.xpm";
-	data->img->collec_path = "./textures/xpm/C.xpm";
-	data->img->wall_path = "./textures/xpm/1.xpm";
-	data->img->back_path = "./textures/xpm/0.xpm";
-	data->img->exit_path = "./textures/xpm/E.xpm";
-	data->img->enemy_path = "./textures/xpm/K.xpm";
+	data->img->player_path = "./textures/xpm/PP.xpm";
+	data->img->collec_path = "./textures/xpm/CC.xpm";
+	data->img->wall_path = "./textures/xpm/11.xpm";
+	data->img->back_path = "./textures/xpm/00.xpm";
+	data->img->exit_path = "./textures/xpm/EE.xpm";
+	data->img->enemy_path = "./textures/xpm/KK.xpm";
 	data->img->img_px = 64;
+	data->img->player = mlx_xpm_file_to_image(data->mlx_ptr,
+		data->img->player_path, &data->img->img_px, &data->img->img_px);
+	data->img->collec = mlx_xpm_file_to_image(data->mlx_ptr,
+		data->img->collec_path, &data->img->img_px, &data->img->img_px);
+	data->img->wall = mlx_xpm_file_to_image(data->mlx_ptr,
+		data->img->wall_path, &data->img->img_px, &data->img->img_px);
+	data->img->back = mlx_xpm_file_to_image(data->mlx_ptr,
+		data->img->back_path, &data->img->img_px, &data->img->img_px);
+	data->img->exit = mlx_xpm_file_to_image(data->mlx_ptr,
+		data->img->exit_path, &data->img->img_px, &data->img->img_px);
+	data->img->enemy = mlx_xpm_file_to_image(data->mlx_ptr,
+		data->img->enemy_path, &data->img->img_px, &data->img->img_px);
+}
 
-	data->img->player = mlx_xpm_file_to_image(data->mlx_ptr, data->img->player_path, &data->img->img_px, &data->img->img_px);
+void	put_images(t_data *data, t_map_array *map_array)
+{
+	int	i;
+	int	j;
+
+	data->img = (t_img *)malloc(sizeof(t_img));
+	get_images_and_paths(data);
+	i = 0;
+	while (map_array->map[i])
+	{
+		j = 0;
+		while (map_array->map[i][j])
+		{
+			if (map_array->map[i][j] == '0' && map_array->map[i + 1][j] == '1' && map_array->map[i - 1][j] == '1' && map_array->map[i][j + 1] == '1' && map_array->map[i][j - 1] == '1')
+				mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img->enemy, (j * 64), (i * 64));
+			else if (map_array->map[i][j] == 'P')
+				mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img->player, (j * 64), (i * 64));
+			else if (map_array->map[i][j] == 'C')
+				mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img->collec, (j * 64), (i * 64));
+			else if (map_array->map[i][j] == '1')
+				mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img->wall, (j * 64), (i * 64));
+			else if (map_array->map[i][j] == '0')
+				mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img->back, (j * 64), (i * 64));
+			else if (map_array->map[i][j] == 'E')
+				mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img->exit, (j * 64), (i * 64));
+			else if (map_array->map[i][j] == 'K')
+				mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img->enemy, (j * 64), (i * 64));
+			j++;
+		}
+		i++;
+	}
+
+	/*
 	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img->player, 0, 0);
-
-	data->img->collec = mlx_xpm_file_to_image(data->mlx_ptr, data->img->collec_path, &data->img->img_px, &data->img->img_px);
 	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img->collec, 64, 64);
-
-	data->img->wall = mlx_xpm_file_to_image(data->mlx_ptr, data->img->wall_path, &data->img->img_px, &data->img->img_px);
 	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img->wall, 128, 128);
-
-	data->img->back = mlx_xpm_file_to_image(data->mlx_ptr, data->img->back_path, &data->img->img_px, &data->img->img_px);
 	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img->back, 192, 192);
-
-	data->img->exit = mlx_xpm_file_to_image(data->mlx_ptr, data->img->exit_path, &data->img->img_px, &data->img->img_px);
 	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img->exit, 256, 256);
-
-	data->img->enemy = mlx_xpm_file_to_image(data->mlx_ptr, data->img->enemy_path, &data->img->img_px, &data->img->img_px);
 	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img->enemy, 320, 320);
+	*/
 }
 
 void	initialize_game(t_map_chars	*map_chars, t_map_array	*map_array)
 {
 	t_data	data;
-	// void	*img;
-	// char	*relative_path;
-	// int		img_width = 64;
-	// int		img_height = 64;
 
-	// relative_path = "./textures/xpm/0.xpm";
-
-	(void)map_array;
 	(void)map_chars;
 	data.mlx_ptr = mlx_init();
 	if (!data.mlx_ptr)
@@ -76,14 +104,14 @@ void	initialize_game(t_map_chars	*map_chars, t_map_array	*map_array)
 	//img = mlx_xpm_file_to_image(data.mlx_ptr, relative_path, &img_width, &img_height);
 	mlx_get_screen_size(data.mlx_ptr, &data.screen_width, &data.screen_height);
 	ft_printf("Screen: %dx%d\n", data.screen_width, data.screen_height);
-	data.win_ptr = mlx_new_window(data.mlx_ptr, 1920, 1080, "danjimen's game!");
+	data.win_ptr = mlx_new_window(data.mlx_ptr, 832, 320, "danjimen's game!");
 	if (!data.win_ptr)
 	{
 		free(data.mlx_ptr);
 		exit (1);
 	}
 
-	put_images(&data);
+	put_images(&data, map_array);
 	// Put the image to the window
 	//mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, img, 0, 0);
 
@@ -95,4 +123,7 @@ void	initialize_game(t_map_chars	*map_chars, t_map_array	*map_array)
 
 	// Loop over the MLX pointer
 	mlx_loop(data.mlx_ptr);
+
+	// AT END
+	free(data.img);
 }
