1ad028bec05517567add7fefb93053c714a82c72
Daniel Jimenez Graindorge <danjimen@c1r12s3.42madrid.com>
2024-05-31 13:01:48 +0200

    Added mlx_get_scree_size()

--- lvl2/so_long/game.c
+++ lvl2/so_long/game.c
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 20:59:27 by danjimen          #+#    #+#             */
-/*   Updated: 2024/05/27 22:37:15 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/05/31 12:59:28 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,26 +30,28 @@ int on_keypress(int keysym, t_data *data)
 
 void	initialize_game(t_map_chars	*map_chars, t_map_array	*map_array)
 {
-	t_data data;
- 
+	t_data	data;
+
 	(void)map_array;
 	(void)map_chars;
 	data.mlx_ptr = mlx_init();
 	if (!data.mlx_ptr)
 		exit (1);
-	data.win_ptr = mlx_new_window(data.mlx_ptr, 600, 400, "hi :)");
+	mlx_get_screen_size(data.mlx_ptr, &data.screen_width, &data.screen_height);
+	ft_printf("Screen: %dx%d\n", data.screen_width, data.screen_height);
+	data.win_ptr = mlx_new_window(data.mlx_ptr, 1920, 1080, "danjimen's game!");
 	if (!data.win_ptr)
 	{
 		free(data.mlx_ptr);
 		exit (1);
 	}
- 
+
 	// Register key release hook
 	mlx_hook(data.win_ptr, KeyRelease, KeyReleaseMask, &on_keypress, &data);
- 
+
 	// Register destroy hook
 	mlx_hook(data.win_ptr, DestroyNotify, StructureNotifyMask, &on_destroy, &data);
- 
+
 	// Loop over the MLX pointer
 	mlx_loop(data.mlx_ptr);
 }
