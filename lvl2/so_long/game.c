d50cb1358c12135344c61dbea35e381d151c7690
BishopVK <danielgraindorge@gmail.com>
2024-06-06 21:12:51 +0200

    Created new_file_img() function. Initialized data_img struct

--- lvl2/so_long/game.c
+++ lvl2/so_long/game.c
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 20:59:27 by danjimen          #+#    #+#             */
-/*   Updated: 2024/06/06 09:44:41 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/06/06 21:10:25 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,7 +70,23 @@ int	on_keypress(int keysym, t_data *data)
 	return (0);
 }
 
-void	get_images_and_paths(t_data *data)
+void	*new_file_img(char *path, t_data *data, t_map_array *map_array)
+{
+	void	*img;
+
+	img = mlx_xpm_file_to_image(data->mlx_ptr,
+		path, &data->img->img_px, &data->img->img_px);
+	if (img == 0)
+	{
+		free_array(map_array);
+		free_mlx_resources(data);
+		ft_dprintf(2, "Error\n> Creating img whit %s\n", path);
+		exit (EXIT_FAILURE);
+	}
+	return (img);
+}
+
+void	get_images_and_paths(t_data *data, t_map_array *map_array)
 {
 	data->img->player_path = "./sprites/xpm/game/player/player_idle_1.xpm";
 	data->img->collec_path = "./sprites/xpm/game/collectible/M_Idle.xpm";
@@ -79,7 +95,15 @@ void	get_images_and_paths(t_data *data)
 	data->img->exit_path = "./sprites/xpm/game/exit/stairs_4.xpm";
 	data->img->enemy_path = "./sprites/xpm/game/sheep/sheep_idle_1.xpm";
 	data->img->img_px = 64;
-	data->img->player = mlx_xpm_file_to_image(data->mlx_ptr,
+
+	data->img->player = new_file_img(data->img->player_path, data, map_array);
+	data->img->collec = new_file_img(data->img->collec_path, data, map_array);
+	data->img->wall = new_file_img(data->img->wall_path, data, map_array);
+	data->img->back = new_file_img(data->img->back_path, data, map_array);
+	data->img->exit = new_file_img(data->img->exit_path, data, map_array);
+	data->img->enemy = new_file_img(data->img->enemy_path, data, map_array);
+
+	/* data->img->player = mlx_xpm_file_to_image(data->mlx_ptr,
 		data->img->player_path, &data->img->img_px, &data->img->img_px);
 	data->img->collec = mlx_xpm_file_to_image(data->mlx_ptr,
 		data->img->collec_path, &data->img->img_px, &data->img->img_px);
@@ -90,7 +114,7 @@ void	get_images_and_paths(t_data *data)
 	data->img->exit = mlx_xpm_file_to_image(data->mlx_ptr,
 		data->img->exit_path, &data->img->img_px, &data->img->img_px);
 	data->img->enemy = mlx_xpm_file_to_image(data->mlx_ptr,
-		data->img->enemy_path, &data->img->img_px, &data->img->img_px);
+		data->img->enemy_path, &data->img->img_px, &data->img->img_px); */
 }
 
 void	put_images(t_data *data, t_map_array *map_array)
@@ -99,7 +123,8 @@ void	put_images(t_data *data, t_map_array *map_array)
 	int	j;
 
 	data->img = (t_img *)malloc(sizeof(t_img));
-	get_images_and_paths(data);
+	ft_memset(data->img, 0, sizeof(t_img));
+	get_images_and_paths(data, map_array);
 	i = 0;
 	while (map_array->map[i])
 	{
@@ -133,7 +158,7 @@ void	verify_map_size(t_map_array *map_array, t_data *data)
 	{
 		free_array(map_array);
 		free_mlx_resources(data);
-		ft_dprintf(2, "The map is higger than screen resolution\n");
+		ft_dprintf(2, "Error\n> The map is higger than screen resolution\n");
 		exit (EXIT_FAILURE);
 	}
 }
@@ -152,7 +177,6 @@ void	initialize_game(t_map_chars *map_chars, t_map_array *map_array)
 	mlx_get_screen_size(data.mlx_ptr, &data.screen_width, &data.screen_height);
 	ft_printf("Screen: %dx%d\n", data.screen_width, data.screen_height);
 	data.img = NULL;
-	data.win_ptr = NULL;
 	data.win_ptr = mlx_new_window(data.mlx_ptr, map_array->width * 64, map_array->height * 64, "danjimen's game!");
 	if (!data.win_ptr)
 	{
@@ -165,7 +189,9 @@ void	initialize_game(t_map_chars *map_chars, t_map_array *map_array)
 
 	// Put the image to the window
 	put_images(&data, map_array);
-	//mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, img, 0, 0);
+
+	// Put text to the window
+	mlx_string_put(data.mlx_ptr, data.win_ptr, 10, 25, 0x000000, "Hola!");
 	
 	// Use when map_array is not necesary
 	free_array(map_array);
