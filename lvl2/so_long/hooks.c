3c12364118ab6689518a00f3e49cb15599febf79
BishopVK <danielgraindorge@gmail.com>
2024-06-12 21:14:02 +0200

    Commented printfs

--- lvl2/so_long/hooks.c
+++ lvl2/so_long/hooks.c
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 19:23:28 by danjimen          #+#    #+#             */
-/*   Updated: 2024/06/12 20:09:02 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/06/12 21:05:37 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,30 +26,30 @@ int	on_keypress(int keysym, t_data *data)
 {
 	if (keysym == UP_KEY || keysym == W_KEY)
 	{
-		ft_printf("Moved Up\n");
+		//ft_printf("Moved Up\n");
 		move_player(data, UP_KEY);
 	}
 	else if (keysym == DOWN_KEY || keysym == S_KEY)
 	{
-		ft_printf("Moved Down\n");
+		//ft_printf("Moved Down\n");
 		move_player(data, DOWN_KEY);
 	}
 	else if (keysym == RIGHT_KEY || keysym == D_KEY)
 	{
-		ft_printf("Moved Right\n");
+		//ft_printf("Moved Right\n");
 		move_player(data, RIGHT_KEY);
 	}
 	else if (keysym == LEFT_KEY || keysym == A_KEY)
 	{
-		ft_printf("Moved Left\n");
+		//ft_printf("Moved Left\n");
 		move_player(data, LEFT_KEY);
 	}
 	else if (keysym == ESC_KEY)
 	{
-		ft_printf("Pressed ESC key\n");
+		//ft_printf("Pressed ESC key\n");
 		on_destroy(data);
 	}
-	else
-		ft_printf("Pressed key: %d\n", keysym);
+	/* else
+		ft_printf("Pressed key: %d\n", keysym); */
 	return (0);
 }
