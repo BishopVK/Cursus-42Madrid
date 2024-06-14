54ce78acfc7b9c754935ddf880f7cc6536861c65
BishopVK <danielgraindorge@gmail.com>
2024-06-14 15:19:45 +0200

    Added win_game() and update_win_sprites() functions

--- lvl2/so_long/movements.c
+++ lvl2/so_long/movements.c
@@ -6,11 +6,45 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 13:42:36 by danjimen          #+#    #+#             */
-/*   Updated: 2024/06/14 14:15:33 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/06/14 15:18:16 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+static void	win_game(t_data *data, int new_x, int new_y)
+{
+	if (data->map_array->map[new_y][new_x] == 'E' &&
+		data->map_array->chars->collectible == 0)
+	{
+		ft_printf("You win!\n");
+		on_destroy(data);
+	}
+}
+
+static void	update_win_sprites(t_data *data, int x, int y)
+{
+	data->map_array->moves++;
+	if (y == data->map_array->exit_y && x == data->map_array->exit_x)
+	{
+		mlx_put_image_to_window(data->mlx_ptr, data->win_ptr,
+			data->img->exit, x * data->img->img_px, y * data->img->img_px);
+		data->map_array->map[y][x] = 'E';
+	}
+	else
+	{
+		mlx_put_image_to_window(data->mlx_ptr, data->win_ptr,
+			data->img->back, x * data->img->img_px, y * data->img->img_px);
+		data->map_array->map[y][x] = '0';
+	}
+	ft_printf("Counter: %i\n", data->map_array->moves);
+	if (data->map_array->chars->collectible == 0)
+	{
+		mlx_put_image_to_window(data->mlx_ptr, data->win_ptr,
+			data->img->exit_2, data->map_array->exit_x * data->img->img_px,
+			data->map_array->exit_y * data->img->img_px);
+	}
+}
 
 static void	move_resume(t_map_array *map_array, int *x, int *y, int dir)
 {
@@ -42,47 +76,16 @@ void	move_player(t_data *data, int dir)
 	new_x = x;
 	new_y = y;
 	move_resume(data->map_array, &new_x, &new_y, dir);
-
-	if (new_x >= 0 && new_x < data->map_array->width &&
-		new_y >= 0 && new_y < data->map_array->height &&
-		data->map_array->map[new_y][new_x] != '1')
+	if (new_x >= 0 && new_x < data->map_array->width
+		&& new_y >= 0 && new_y < data->map_array->height
+		&& data->map_array->map[new_y][new_x] != '1')
 	{
-		if (data->map_array->map[new_y][new_x] == 'E' &&
-			data->map_array->chars->collectible == 0)
-		{
-			ft_printf("Has Ganado!\n");
-			on_destroy(data);
-		}
-
-		data->map_array->moves++;
-		/* mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img->back,
-			x * data->img->img_px, y * data->img->img_px); */
-		if (y == data->map_array->exit_y && x == data->map_array->exit_x)
-		{
-			mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img->exit,
-				x * data->img->img_px, y * data->img->img_px);
-			data->map_array->map[y][x] = 'E';
-		}
-		else
-		{
-			mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img->back,
-				x * data->img->img_px, y * data->img->img_px);
-			data->map_array->map[y][x] = '0';
-		}
-
-		/* mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img->back,
-			new_x * data->img->img_px, new_y * data->img->img_px); */
-
-		// Coloca la imagen del jugador en la nueva posición >MANDATORY PART<
+		win_game(data, new_x, new_y);
+		update_win_sprites(data, x, y);
 		mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img->player,
-				new_x * data->img->img_px, new_y * data->img->img_px);
+			new_x * data->img->img_px, new_y * data->img->img_px);
 		data->map_array->map[new_y][new_x] = 'P';
-
 		data->map_array->start_x = new_x;
 		data->map_array->start_y = new_y;
-		ft_printf("Counter: %i\n", data->map_array->moves);
-
-		// Display Array for Debugging
-		display_array(data->map_array);
 	}
 }
