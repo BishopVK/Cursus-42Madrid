328a04e918e785c44dd7c1396e419198a066b374
BishopVK <danielgraindorge@gmail.com>
2024-06-14 12:33:37 +0200

    Norminette fixed

--- lvl2/so_long/flood_fill.c
+++ lvl2/so_long/flood_fill.c
@@ -6,13 +6,14 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 08:46:25 by danjimen          #+#    #+#             */
-/*   Updated: 2024/06/13 22:15:18 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/06/14 12:33:20 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	detect_player_and_exit(char *buffer, t_map_array *map_array, int map_lines)
+void	detect_player_and_exit(char *buffer, t_map_array *map_array,
+			int map_lines)
 {
 	int	i;
 
@@ -46,7 +47,7 @@ void	verify_flood_fill_result(t_map_chars *copy_chars,
 	if (copy_chars->player != 0 || copy_chars->exit != 0
 		|| copy_chars->collectible != 0)
 	{
-		dprintf(2, "Map imposible to solve\n");
+		dprintf(2, "Error\n> Map imposible to solve\n");
 		free_array(map_array);
 		exit (EXIT_FAILURE);
 	}
@@ -55,17 +56,9 @@ void	verify_flood_fill_result(t_map_chars *copy_chars,
 void	flood_fill(t_map_chars *copy_chars, t_map_array *copy_array,
 			int x, int y)
 {
-	// ft_printf("x ==> %i\n", x);
-	// ft_printf("y ==> %i\n", y);
-	// Base cases
-	//ft_printf("copy_array->map[x][y] ==> %c\n", copy_array->map[1][3]);
 	if (x < 1 || x >= copy_array->height || y < 1 || y >= copy_array->width
-			|| copy_array->map[x][y] == '1' || copy_array->map[x][y] == 'V')
-	{
+		|| copy_array->map[x][y] == '1' || copy_array->map[x][y] == 'V')
 		return ;
-	}
-
-	// Mark the cell as visited
 	if (copy_array->map[x][y] == 'P')
 		copy_chars->player--;
 	else if (copy_array->map[x][y] == 'E')
@@ -73,8 +66,6 @@ void	flood_fill(t_map_chars *copy_chars, t_map_array *copy_array,
 	else if (copy_array->map[x][y] == 'C')
 		copy_chars->collectible--;
 	copy_array->map[x][y] = 'V';
-
-	// Recursively apply flood fill to neighboring cells
 	flood_fill(copy_chars, copy_array, x + 1, y);
 	flood_fill(copy_chars, copy_array, x - 1, y);
 	flood_fill(copy_chars, copy_array, x, y + 1);
