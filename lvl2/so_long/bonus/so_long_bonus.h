43181452e7e89d25f3ef56c77c539ab726ea7391
BishopVK <danielgraindorge@gmail.com>
2024-06-14 14:13:06 +0200

    fixed paths to libft, ft_printf, get_nex_line and mlx

--- lvl2/so_long/bonus/so_long_bonus.h
+++ lvl2/so_long/bonus/so_long_bonus.h
@@ -1,29 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.h                                          :+:      :+:    :+:   */
+/*   so_long_bonus.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 13:33:23 by danjimen          #+#    #+#             */
-/*   Updated: 2024/06/14 13:11:25 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/06/14 14:06:01 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SO_LONG_H
-# define SO_LONG_H
+#ifndef SO_LONG_BONUS_H
+# define SO_LONG_BONUS_H
 
 //libft
-# include "libft/libft.h"
+# include "../libft/libft.h"
 
 //ft_printf, ft_dprintf
-# include "printf/ft_printf.h"
+# include "../printf/ft_printf.h"
 
 //get_next_line
-# include "get_next_line/get_next_line.h"
+# include "../get_next_line/get_next_line.h"
 
 //mlx
-# include "minilibx-linux/mlx.h"
+# include "../minilibx-linux/mlx.h"
 
 //X11
 # include <X11/X.h>
