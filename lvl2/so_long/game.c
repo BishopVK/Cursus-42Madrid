a912b15a8d45f401fb9351514281651bb36d1065
BishopVK <danielgraindorge@gmail.com>
2024-06-14 13:13:58 +0200

    Moved sprite functions to sprite.c file. And solved some norminette errors

--- lvl2/so_long/game.c
+++ lvl2/so_long/game.c
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 20:59:27 by danjimen          #+#    #+#             */
-/*   Updated: 2024/06/13 23:00:13 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/06/14 13:08:15 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,99 +56,12 @@ void	free_mlx_resources(t_data *data)
 	}
 }
 
-void	*new_file_img(char *path, t_data *data)
-{
-	void	*img;
-
-	img = mlx_xpm_file_to_image(data->mlx_ptr,
-		path, &data->img->img_px, &data->img->img_px);
-	if (img == 0)
-	{
-		ft_dprintf(2, "Error\n> Creating img whit %s\n", path);
-		//free_array(map_array);
-		on_destroy(data);
-		exit (EXIT_FAILURE);
-	}
-	return (img);
-}
-
-void	get_images_and_paths(t_data *data)
-{
-	data->img->player_path[0] = "./sprites/xpm/TinyRanch/Player/char_front_idle_1.xpm";
-	data->img->player_path[1] = "./sprites/xpm/TinyRanch/Player/char_back_idle_1.xpm";
-	data->img->player_path[2] = "./sprites/xpm/TinyRanch/Player/char_left_idle_1.xpm";
-	data->img->player_path[3] = "./sprites/xpm/TinyRanch/Player/char_right_idle_1.xpm";
-	data->img->collec_path = "./sprites/xpm/TinyRanch/Item/tomato.xpm";
-	data->img->wall_path = "./sprites/xpm/TinyRanch/Tileset/water_2.xpm";
-	data->img->wall_border_path = "./sprites/xpm/TinyRanch/Tileset/water_border_1.xpm";
-	data->img->back_path = "./sprites/xpm/TinyRanch/Tileset/background.xpm";
-	data->img->exit_path = "./sprites/xpm/TinyRanch/Exit/exit_4.xpm";
-	data->img->enemy_path = "./sprites/xpm/TinyRanch/Enemy/sheep_idle_1.xpm";
-	data->img->img_px = 64;
-
-	data->img->player = new_file_img(data->img->player_path[0], data);
-	data->img->collec = new_file_img(data->img->collec_path, data);
-	data->img->wall = new_file_img(data->img->wall_path, data);
-	data->img->wall_border = new_file_img(data->img->wall_border_path, data);
-	data->img->back = new_file_img(data->img->back_path, data);
-	data->img->exit = new_file_img(data->img->exit_path, data);
-	data->img->enemy = new_file_img(data->img->enemy_path, data);
-
-	/* data->img->player = mlx_xpm_file_to_image(data->mlx_ptr,
-		data->img->player_path, &data->img->img_px, &data->img->img_px);
-	data->img->collec = mlx_xpm_file_to_image(data->mlx_ptr,
-		data->img->collec_path, &data->img->img_px, &data->img->img_px);
-	data->img->wall = mlx_xpm_file_to_image(data->mlx_ptr,
-		data->img->wall_path, &data->img->img_px, &data->img->img_px);
-	data->img->back = mlx_xpm_file_to_image(data->mlx_ptr,
-		data->img->back_path, &data->img->img_px, &data->img->img_px);
-	data->img->exit = mlx_xpm_file_to_image(data->mlx_ptr,
-		data->img->exit_path, &data->img->img_px, &data->img->img_px);
-	data->img->enemy = mlx_xpm_file_to_image(data->mlx_ptr,
-		data->img->enemy_path, &data->img->img_px, &data->img->img_px); */
-}
-
-void	put_images(t_data *data, t_map_array *map_array)
-{
-	int	x;
-	int	y;
-
-	data->img = (t_img *)malloc(sizeof(t_img));
-	ft_memset(data->img, 0, sizeof(t_img));
-	get_images_and_paths(data);
-	y = 0;
-	while (map_array->map[y])
-	{
-		x = 0;
-		while (map_array->map[y][x])
-		{
-			if (y > 0 && map_array->map[y][x] == '1' && map_array->map[y - 1][x] != '1')
-				mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img->wall_border, (x * 64), (y * 64));
-			else if (map_array->map[y][x] == 'P')
-				mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img->player, (x * 64), (y * 64));
-			else if (map_array->map[y][x] == 'C')
-				mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img->collec, (x * 64), (y * 64));
-			else if (map_array->map[y][x] == '1')
-				mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img->wall, (x * 64), (y * 64));
-			else if (map_array->map[y][x] == '0')
-				mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img->back, (x * 64), (y * 64));
-			else if (map_array->map[y][x] == 'E')
-				mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img->exit, (x * 64), (y * 64));
-			else if (map_array->map[y][x] == 'K')
-				mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img->enemy, (x * 64), (y * 64));
-			x++;
-		}
-		y++;
-	}
-}
-
 void	verify_map_size(t_map_array *map_array, t_data *data)
 {
 	if (map_array->width * 64 > data->screen_width
 		|| map_array->height * 64 > data->screen_height)
 	{
 		ft_dprintf(2, "Error\n> The map is higger than screen resolution\n");
-		//free_array(map_array);
 		on_destroy(data);
 		exit (EXIT_FAILURE);
 	}
@@ -160,21 +73,10 @@ void	initialize_game(t_map_chars *map_chars, t_map_array *map_array)
 
 	map_array->chars = map_chars;
 	data.map_array = map_array;
-	ft_printf("<<<<<>>>>>\n");
-	ft_printf("collectibles = %i\n", map_array->chars->collectible);
-	ft_printf("player = %i\n", map_array->chars->player);
-	ft_printf("exit = %i\n", map_array->chars->exit);
-	ft_printf("wall = %i\n", map_array->chars->wall);
-	ft_printf("empty = %i\n", map_array->chars->empty);
-	ft_printf("<<<<<>>>>>\n");
-	ft_printf("map_array->width ==> %i\n", map_array->width);
-	ft_printf("map_array->height ==> %i\n", map_array->height);
 	data.mlx_ptr = mlx_init();
 	if (!data.mlx_ptr)
 		exit (1);
-	//img = mlx_xpm_file_to_image(data.mlx_ptr, relative_path, &img_width, &img_height);
 	mlx_get_screen_size(data.mlx_ptr, &data.screen_width, &data.screen_height);
-	ft_printf("Screen: %dx%d\n", data.screen_width, data.screen_height);
 	data.img = NULL;
 	data.win_ptr = mlx_new_window(data.mlx_ptr, map_array->width * 64, map_array->height * 64, "danjimen's game!");
 	if (!data.win_ptr)
@@ -182,29 +84,15 @@ void	initialize_game(t_map_chars *map_chars, t_map_array *map_array)
 		free(data.mlx_ptr);
 		exit (1);
 	}
-	
-	// Verify if map fits in the screen resolution
 	verify_map_size(map_array, &data);
-
-	// Put the image to the window
 	put_images(&data, map_array);
-
 	// Put text to the window on BONUS
-	//mlx_string_put(data.mlx_ptr, data.win_ptr, 10, 100, 0x000000, "Hola!");
-	char	*moves;
+	/* char	*moves;
 	moves = ft_itoa(data.map_array->moves);
 	mlx_string_put(data.mlx_ptr, data.win_ptr, 10, 25, 0x000000, moves);
-	free(moves);
-
-	// Register key release hook
+	free(moves); */
 	mlx_hook(data.win_ptr, KeyRelease, KeyReleaseMask, &on_keypress, &data);
-
-	// Register destroy hook
 	mlx_hook(data.win_ptr, DestroyNotify, StructureNotifyMask, &on_destroy, &data);
-
-	// Loop over the MLX pointer
 	mlx_loop(data.mlx_ptr);
-
-	// AT END: Clean all
 	free_mlx_resources(&data);
 }
