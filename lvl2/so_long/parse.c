3fe3a09d80917fda1a56ed21d79bda6a23728bc8
BishopVK <danielgraindorge@gmail.com>
2024-06-14 11:30:23 +0200

    Norminette fixed

--- lvl2/so_long/parse.c
+++ lvl2/so_long/parse.c
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 09:05:14 by danjimen          #+#    #+#             */
-/*   Updated: 2024/06/05 14:36:55 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/06/14 11:29:43 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,7 @@
 void	exit_map_error(char *buffer, char *message, int fd)
 {
 	if (buffer != NULL)
-	{
-		ft_dprintf(2, "Buffer before free: %s\n", buffer);
 		free(buffer);
-	}
 	get_next_line(fd, TRUE);
 	ft_dprintf(2, "Error\n> %s\n", message);
 	exit (EXIT_FAILURE);
@@ -30,31 +27,21 @@ int	count_buffer_len(char *buffer, int fd)
 
 	if (buffer_len == 0)
 	{
-		ft_printf("ft_strlen(buffer) => %i\n", ft_strlen(buffer));
-		//ft_dprintf(2, "THIS SHOULD ONLY COME ONE TIME\n");
 		if (buffer[ft_strlen(buffer) - 1] != '\n')
-			exit_map_error(buffer, "Wrong map size", fd); // 1 line map
+			exit_map_error(buffer, "Wrong map size", fd);
 		buffer_len = (ft_strlen(buffer) - 1);
 		if (buffer_len < 3)
-			exit_map_error(buffer, "The map must have at least 3 columns", fd); // At least 3 cols
-		ft_printf("buffer_len => %i\n", buffer_len);
+			exit_map_error(buffer, "The map must have at least 3 columns", fd);
 	}
 	else if (buffer[ft_strlen(buffer) - 1] == '\n')
 	{
-		//ft_dprintf(2, "IF there is a line break\n");
-		if ((ft_strlen(buffer) - 1) == buffer_len)
-			ft_dprintf(2, "Lines of the same length\n");
-		else
-			exit_map_error(buffer, "Wrong map size", fd); //Different line len
+		if ((ft_strlen(buffer) - 1) != buffer_len)
+			exit_map_error(buffer, "Wrong map size", fd);
 	}
 	else
 	{
-		//ft_dprintf(2, "NO line break\n");
-		ft_printf("LAST LINE: ft_strlen(buffer) => %i", ft_strlen(buffer));
-		if (ft_strlen(buffer) == buffer_len)
-			ft_dprintf(2, "\nLines of the same length\n");
-		else
-			exit_map_error(buffer, "Wrong map size", fd); //Different line len
+		if (ft_strlen(buffer) != buffer_len)
+			exit_map_error(buffer, "Wrong map size", fd);
 	}
 	return (buffer_len);
 }
@@ -70,20 +57,16 @@ int	read_map_lines(char *buffer, char *map)
 		exit_map_error(buffer, "Open error", fd);
 	buffer = get_next_line(fd, FALSE);
 	if (buffer == NULL || ft_strlen(buffer) == 0)
-		exit_map_error(buffer, "Void map file", fd); //Void map
+		exit_map_error(buffer, "Void map file", fd);
 	while (buffer != NULL)
 	{
-		// if (buffer == NULL) // Check if memory was allocated correctly
-		// 	exit_map_error(buffer, "Failed to allocate memory for buffer"); //Void map
 		if (buffer[0] == '\n')
-			exit_map_error(buffer, "Only new line detected", fd); // First line void
-		ft_printf("buffer[%i] = %s\n", map_lines, buffer); // Show the line read (DELETE)
-		count_buffer_len(buffer, fd); // Count buffer length
-		check_map_characters(buffer, "01CEP", fd); // Check characters
-		// if (buffer != NULL)
+			exit_map_error(buffer, "Only new line detected", fd);
+		count_buffer_len(buffer, fd);
+		check_map_characters(buffer, "01CEP", fd);
 		if (ft_strlen(buffer) > 256 || map_lines > 256)
 			exit_map_error(buffer, "The map is too big", fd);
-		free(buffer); // Release the memory allocated to the line
+		free(buffer);
 		buffer = get_next_line(fd, FALSE);
 		map_lines++;
 	}
@@ -97,12 +80,10 @@ void	read_map(char *map, t_map_chars *map_chars, t_map_array *map_array)
 
 	buffer = NULL;
 	map_array->height = read_map_lines(buffer, map);
-	ft_printf("map_lines => %i\n", map_array->height);
 	if (map_array->height < 3)
 		exit_map_error(buffer, "The map must have at least 3 lines", -1);
-	map_array->width = read_for_check_borders(buffer, map, map_array->height, map_chars); // Check borders
-	/* if (map_array->height > 256 || map_array->width > 256)
-		exit_map_error(NULL, "The map is too big", -1); */
+	map_array->width = read_for_check_borders(buffer, map,
+			map_array->height, map_chars);
 }
 
 void	check_arg_extension(char *map)
@@ -112,12 +93,10 @@ void	check_arg_extension(char *map)
 
 	map_len = ft_strlen(map);
 	map_name_len = map_len - 4;
-	if (ft_strncmp(map + map_name_len, ".ber", 4) == 0 && map_len > 4
-		&& map[map_len - 5] != '/')
-		ft_printf("Correct \"%s\" file extension\n", map + map_name_len);
-	else
+	if (!(ft_strncmp(map + map_name_len, ".ber", 4) == 0 && map_len > 4
+			&& map[map_len - 5] != '/'))
 	{
-		ft_dprintf(2, "Wrong file extension\n");
+		ft_dprintf(2, "Error\n> Wrong file extension\n");
 		exit (-1);
 	}
 }
