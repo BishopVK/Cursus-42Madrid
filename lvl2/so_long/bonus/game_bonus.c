dbc27718be92561d0de5cd952068680de1ae7742
BishopVK <danielgraindorge@gmail.com>
2024-06-14 18:38:27 +0200

    Moved destroy of player and exit from clean_up() to clean_up_img_arrays()

--- lvl2/so_long/bonus/game_bonus.c
+++ lvl2/so_long/bonus/game_bonus.c
@@ -6,18 +6,37 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 14:04:10 by danjimen          #+#    #+#             */
-/*   Updated: 2024/06/14 14:04:12 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/06/14 18:37:28 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long_bonus.h"
 
+static void	clean_up_img_arrays(t_data *data)
+{
+	int	i;
+
+	i	0;
+	while(i < 4)
+	{
+		if (data->img->player[i])
+			mlx_destroy_image(data->mlx_ptr, data->img->player[i]);
+		i++;
+	}
+	i = 0;
+	while(i < 5)
+	{
+		if (data->img->exit[i])
+			mlx_destroy_image(data->mlx_ptr, data->img->exit[i]);
+		i++;
+	}
+}
+
 void	clean_up(t_data *data)
 {
 	if (data->img)
 	{
-		if (data->img->player)
-			mlx_destroy_image(data->mlx_ptr, data->img->player);
+		clean_up_img_arrays(t_data *data);
 		if (data->img->collec)
 			mlx_destroy_image(data->mlx_ptr, data->img->collec);
 		if (data->img->wall)
@@ -26,8 +45,6 @@ void	clean_up(t_data *data)
 			mlx_destroy_image(data->mlx_ptr, data->img->wall_border);
 		if (data->img->back)
 			mlx_destroy_image(data->mlx_ptr, data->img->back);
-		if (data->img->exit)
-			mlx_destroy_image(data->mlx_ptr, data->img->exit);
 		if (data->img->enemy)
 			mlx_destroy_image(data->mlx_ptr, data->img->enemy);
 		free(data->img);
@@ -70,6 +87,7 @@ void	verify_map_size(t_map_array *map_array, t_data *data)
 void	initialize_game(t_map_chars *map_chars, t_map_array *map_array)
 {
 	t_data	data;
+	char	*moves;
 
 	map_array->chars = map_chars;
 	data.map_array = map_array;
@@ -86,11 +104,9 @@ void	initialize_game(t_map_chars *map_chars, t_map_array *map_array)
 	}
 	verify_map_size(map_array, &data);
 	put_images(&data, map_array);
-	// Put text to the window on BONUS
-	/* char	*moves;
 	moves = ft_itoa(data.map_array->moves);
 	mlx_string_put(data.mlx_ptr, data.win_ptr, 10, 25, 0x000000, moves);
-	free(moves); */
+	free(moves);
 	mlx_hook(data.win_ptr, KeyRelease, KeyReleaseMask, &on_keypress, &data);
 	mlx_hook(data.win_ptr, DestroyNotify, StructureNotifyMask, &on_destroy, &data);
 	mlx_loop(data.mlx_ptr);
