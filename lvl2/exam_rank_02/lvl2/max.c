cb6678d8b31e58ebdf1d714beda8c618241cf066
BishopVK <danielgraindorge@gmail.com>
2024-06-18 23:24:09 +0200

    Added lvl1 and lvl2 exercises of my exam rank 02

---
+++ lvl2/exam_rank_02/lvl2/max.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   max.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/18 23:08:32 by danjimen          #+#    #+#             */
+/*   Updated: 2024/06/18 23:22:49 by danjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		max(int* tab, unsigned int len)
+{
+	unsigned int	higger;
+	unsigned int	i;
+
+	if (len == 0)
+		return (0);
+	i = 0;
+	higger = tab[i];
+	while (i < len)
+	{
+		if (tab[i] > higger)
+			higger = tab[i];
+		i++;
+	}
+	return (higger);
+}
