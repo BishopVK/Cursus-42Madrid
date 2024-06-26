54d9464bf06e39ed40bd9ce57e2cecbf9c26cda2
Daniel Jimenez Graindorge <danjimen@c1r12s3.42madrid.com>
2024-06-26 08:05:13 +0200

    Created philo.h into includes folder

---
+++ lvl3/philo/includes/philo.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danjimen <danjimen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/26 07:52:36 by danjimen          #+#    #+#             */
+/*   Updated: 2024/06/26 08:01:19 by danjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// pthread_create, pthread_join, pthread_detach, pthread_mutex_init
+// pthread_mutex_destroy, pthread_mutex_lock, pthread_mutex_unlock
+#include <pthread.h>
+
+// printf
+#include <stdio.h>
+
+// memset
+#include <string.h>
+
+// malloc, free
+#include <stdlib.h>
+
+// write, usleep
+#include <unistd.h>
+
+// gettimeofday
+#include <sys/time.h>
