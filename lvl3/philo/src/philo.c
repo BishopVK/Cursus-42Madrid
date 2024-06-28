ad92dcb6ab89ebeaa671977530fea39f1c69ddeb
Daniel Jimenez Graindorge <danjimen@c1r12s3.42madrid.com>
2024-06-28 11:10:20 +0200

    Moved all parse functions to parse.c

--- lvl3/philo/src/philo.c
+++ lvl3/philo/src/philo.c
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 11:37:14 by danjimen          #+#    #+#             */
-/*   Updated: 2024/06/28 10:26:21 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/06/28 11:07:26 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,60 +34,17 @@
 	return (NULL);
 } */
 
-int	validate_args_set(int argc, char **argv)
-{
-	int	i;
-	int	j;
-
-	i = 1;
-	while (i < argc)
-	{
-		j = 0;
-		while (argv[i][j])
-		{
-			if (ft_isdigit(argv[i][j]) == 0)
-				return (1);
-			j++;
-		}
-		if (ft_atol(argv[i]) > INT_MAX || ft_atol(argv[i]) < INT_MIN)
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-t_table	parse_args(int argc, char **argv)
-{
-	t_table	table;
-
-	table.time_to_die = ft_atoi(argv[1]);
-	table.time_to_eat = ft_atoi(argv[2]);
-	table.time_to_sleep = ft_atoi(argv[3]);
-	if (argc == 5)
-		table.nbr_must_eat = ft_atoi(argv[4]);
-	else
-		table.nbr_must_eat = -1;
-	return (table);
-}
-
 int	main(int argc, char **argv)
 {
 	t_table	table;
 
-	if (argc >= 4 && argc <= 5)
-	{
-		if (validate_args_set(argc, argv) != 0)
-		{
-			printf("Invalid args type\n");
-			return (1);
-		}
-		table = parse_args(argc, argv);
-	}
-	else
-	{
-		printf("Correct use: ./philo time_to_die time_to_eat time_to_sleep");
-		printf(" [number_of_times_each_philosopher_must_eat]\n");
-	}
+	if (validate_args(argc, argv, &table) != 0)
+		return (1);
+	printf("time_to_die == %i\n", table.time_to_die);
+	printf("time_to_eat == %i\n", table.time_to_eat);
+	printf("time_to_sleep == %i\n", table.time_to_sleep);
+	printf("nbr_must_eat == %i\n", table.nbr_must_eat);
+
 	return (0);
 }
 
