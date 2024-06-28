b48cd38b17991bdd179b6e790b134bdf6796b1c1
Daniel Jimenez Graindorge <danjimen@c1r12s3.42madrid.com>
2024-06-28 09:23:55 +0200

    Added only int values parse

--- lvl3/philo/src/philo.c
+++ lvl3/philo/src/philo.c
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 11:37:14 by danjimen          #+#    #+#             */
-/*   Updated: 2024/06/27 15:40:58 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/06/28 09:18:09 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,17 +34,39 @@
 	return (NULL);
 } */
 
+int	validate_args_set(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			if (ft_isdigit(argv[i][j]) == 0)
+				return (1);
+			j++;
+		}
+		if (ft_atol(argv[i]) > INT_MAX || ft_atol(argv[i]) < INT_MIN)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
 t_table	parse_args(int argc, char **argv)
 {
 	t_table	table;
 
-	table.time_to_die = argv[1];
-	table.time_to_eat = argv[2];
-	table.time_to_sleep = argv[3];
-	if (argc == 6)
-		table.nbr_must_eat = argv[4];
+	if (argc == 5)
+		table.nbr_must_eat = ft_atoi(argv[4]);
 	else
 		table.nbr_must_eat = -1;
+	table.time_to_die = ft_atoi(argv[1]);
+	table.time_to_eat = ft_atoi(argv[2]);
+	table.time_to_sleep = ft_atoi(argv[3]);
 	return (table);
 }
 
@@ -52,13 +74,21 @@ int	main(int argc, char **argv)
 {
 	t_table	table;
 
-	if (argc >= 5 && argc <= 6)
+	if (argc >= 4 && argc <= 5)
+	{
+		if (validate_args_set(argc, argv) != 0)
+		{
+			printf("Invalid args type\n");
+			return (1);
+		}
 		table = parse_args(argc, argv);
+	}
 	else
 	{
 		printf("Correct use: ./philo time_to_die time_to_eat time_to_sleep");
-		printf("[number_of_times_each_philosopher_must_eat]\n");
+		printf(" [number_of_times_each_philosopher_must_eat]\n");
 	}
+	return (0);
 }
 
 /* int	main(void)
