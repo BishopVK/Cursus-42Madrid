9d9618cffee475d8165a43f31f5690cc5f9707ee
BishopVK <danielgraindorge@gmail.com>
2024-07-01 21:41:07 +0200

    Added end_of_routine() function and completed philo_routine()

--- lvl3/philo/src/routine.c
+++ lvl3/philo/src/routine.c
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 09:56:08 by danjimen          #+#    #+#             */
-/*   Updated: 2024/07/01 19:52:48 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/07/01 21:40:03 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,37 @@ void	print_action(int id, char *action)
 	printf("%li: %i %s\n", get_current_time(), id, action);
 }
 
+static int	end_of_routine(t_table *table)
+{
+	int	i;
+
+	i = 0;
+	while (i < table->nbr_philos)
+	{
+		if (get_current_time() - table->philos[i].last_meal_time > table->time_to_die)
+		{
+			print_action(table->philos[i].id, "died");
+			return (1);
+		}
+		if (table->nbr_must_eat != -1 && table->philos[i].meals_eaten < table->nbr_must_eat)
+			return (0);
+		i++;
+	}
+	return (0);
+}
+
 void	*philo_routine(void *arg)
 {
-	t_philosopher *philo;
+	t_philosopher	*philo;
 
 	philo = (t_philosopher *)arg;
-	think(philo);
+	while (end_of_routine(philo->table) == 0)
+	{
+		think(philo);
+		take_forks(philo);
+		eat(philo);
+		leave_forks(philo);
+		sleep_philosopher(philo);
+	}
 	return (NULL);
 }
