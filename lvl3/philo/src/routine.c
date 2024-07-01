b8079c70c064441c976ea76106b47f1a3c82b717
BishopVK <danielgraindorge@gmail.com>
2024-07-01 19:52:29 +0200

    Added call to think

--- lvl3/philo/src/routine.c
+++ lvl3/philo/src/routine.c
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 09:56:08 by danjimen          #+#    #+#             */
-/*   Updated: 2024/07/01 15:19:46 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/07/01 19:42:35 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,10 @@ void	print_action(int id, char *action)
 void	*philo_routine(void *arg)
 {
 	t_philosopher *philo;
-	t_table *table;
+	//t_table *table;
 
 	philo = (t_philosopher *)arg;
-	table = philo->table;
-	(void)arg;
-	printf("HOLA!\n");
+	//table = philo->table;
+	think(philo);
 	return (NULL);
 }
