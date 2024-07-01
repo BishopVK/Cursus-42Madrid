50f8a95d88d6d6bf23fd626fc302c5ae6bca8e5f
BishopVK <danielgraindorge@gmail.com>
2024-07-01 21:27:38 +0200

    Added take_forks() and leave_forks() functions

--- lvl3/philo/src/actions.c
+++ lvl3/philo/src/actions.c
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 15:05:13 by danjimen          #+#    #+#             */
-/*   Updated: 2024/07/01 19:55:18 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/07/01 21:25:48 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,4 +30,30 @@ void	sleep_philosopher(t_philosopher *philo)
 {
 	print_action(philo->id, "is sleeping");
 	usleep(philo->table->time_to_sleep * 1000);
+}
+
+void	take_forks(t_philosopher *philo)
+{
+	int	left_fork;
+	int	right_fork;
+
+	left_fork = philo->id - 1;
+	right_fork = philo->id % philo->table->nbr_philos;
+
+	pthread_mutex_lock(&philo->table->forks[left_fork]);
+	print_action(philo->id, "has taken a fork");
+	pthread_mutex_lock(&philo->table->forks[right_fork]);
+	print_action(philo->id, "has taken a fork");
+}
+
+void	leave_forks(t_philosopher *philo)
+{
+	int	left_fork;
+	int	right_fork;
+
+	left_fork = philo->id - 1;
+	right_fork = philo->id % philo->table->nbr_philos;
+
+	pthread_mutex_unlock(&philo->table->forks[left_fork]);
+	pthread_mutex_unlock(&philo->table->forks[right_fork]);
 }
