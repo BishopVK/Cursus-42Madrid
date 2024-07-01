9889b0d8f53e60edfcaa7f2d05d3fd17fd722eb0
BishopVK <danielgraindorge@gmail.com>
2024-07-01 19:51:21 +0200

    Added eat() function

--- lvl3/philo/src/actions.c
+++ lvl3/philo/src/actions.c
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 15:05:13 by danjimen          #+#    #+#             */
-/*   Updated: 2024/07/01 15:17:35 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/07/01 19:45:26 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,12 @@ void	think(t_philosopher *philo)
 {
 	print_action(philo->id, "is thinking");
 	usleep(1000);
+}
+
+void	eat(t_philosopher *philo)
+{
+	print_action(philo->id, "is eating");
+	usleep(philo->table->time_to_eat * 1000);
+	philo->meals_eaten++;
+	philo->last_meal_time = get_current_time();
 }
