8e52fb5a2b0da1675570708898766c6b77ae3197
BishopVK <danielgraindorge@gmail.com>
2024-07-01 11:05:23 +0200

    Added print_action() function

--- lvl3/philo/src/routine.c
+++ lvl3/philo/src/routine.c
@@ -6,11 +6,16 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 09:56:08 by danjimen          #+#    #+#             */
-/*   Updated: 2024/07/01 10:08:45 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/07/01 11:02:11 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
+
+void	print_action(int id, char *action)
+{
+	printf("%li: %i %s\n", get_current_time(), id, action);
+}
 
 void	*philo_routine(void *arg)
 {
