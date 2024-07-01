2fe9d65abec4082521e7a8de6436a2d75e17d619
BishopVK <danielgraindorge@gmail.com>
2024-07-01 19:56:23 +0200

    Added sleep_philosopher() function

--- lvl3/philo/src/actions.c
+++ lvl3/philo/src/actions.c
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 15:05:13 by danjimen          #+#    #+#             */
-/*   Updated: 2024/07/01 19:45:26 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/07/01 19:55:18 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,10 @@ void	eat(t_philosopher *philo)
 	usleep(philo->table->time_to_eat * 1000);
 	philo->meals_eaten++;
 	philo->last_meal_time = get_current_time();
+}
+
+void	sleep_philosopher(t_philosopher *philo)
+{
+	print_action(philo->id, "is sleeping");
+	usleep(philo->table->time_to_sleep * 1000);
 }
