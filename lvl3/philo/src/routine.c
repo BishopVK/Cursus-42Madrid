db8ff53eeff1947f340a3e9d0dff98f9a72e3a40
BishopVK <danielgraindorge@gmail.com>
2024-07-01 19:33:08 +0200

    Added initialization of philo and table variables

--- lvl3/philo/src/routine.c
+++ lvl3/philo/src/routine.c
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 09:56:08 by danjimen          #+#    #+#             */
-/*   Updated: 2024/07/01 11:02:11 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/07/01 15:19:46 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,11 @@ void	print_action(int id, char *action)
 
 void	*philo_routine(void *arg)
 {
+	t_philosopher *philo;
+	t_table *table;
+
+	philo = (t_philosopher *)arg;
+	table = philo->table;
 	(void)arg;
 	printf("HOLA!\n");
 	return (NULL);
