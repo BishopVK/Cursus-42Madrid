3220b530f8d1a64da8b945a1249c2f9e5f87e325
Daniel Jimenez Graindorge <danjimen@c1r12s3.42madrid.com>
2024-07-03 14:30:12 +0200

    Modified to comumicate philos whit referee and referee whit philo

--- lvl3/philo/src/actions.c
+++ lvl3/philo/src/actions.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   actions.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: danjimen <danjimen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 15:05:13 by danjimen          #+#    #+#             */
-/*   Updated: 2024/07/02 23:36:28 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/07/03 14:17:12 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,26 @@
 
 void	think(t_philosopher *philo)
 {
-	if (philo->table->loop_end == 0)
-		print_action(philo->id, "is thinking", philo->table->start_time);
+	pthread_mutex_lock(&philo->table->end_mutex);
+	philo->table->is_simulation_ended = philo->table->loop_end;
+	pthread_mutex_unlock(&philo->table->end_mutex);
+	if (philo->table->is_simulation_ended)
+		return ;
+	pthread_mutex_lock(&philo->table->global_mutex);	
+	print_action(philo->id, "is thinking", philo->table->start_time);
+	pthread_mutex_unlock(&philo->table->global_mutex);
 }
 
 void	eat(t_philosopher *philo)
 {
-	if (philo->table->loop_end == 0)
-		print_action(philo->id, "is eating", philo->table->start_time);
+	pthread_mutex_lock(&philo->table->end_mutex);
+	philo->table->is_simulation_ended = philo->table->loop_end;
+	pthread_mutex_unlock(&philo->table->end_mutex);
+	if (philo->table->is_simulation_ended)
+		return ;
+	pthread_mutex_lock(&philo->table->global_mutex);
+	print_action(philo->id, "is eating", philo->table->start_time);
+	pthread_mutex_unlock(&philo->table->global_mutex);
 	usleep(philo->table->time_to_eat * 1000);
 	philo->meals_eaten++;
 	philo->last_meal_time = get_current_time();
@@ -29,8 +41,14 @@ void	eat(t_philosopher *philo)
 
 void	sleep_philosopher(t_philosopher *philo)
 {
-	if (philo->table->loop_end == 0)
-		print_action(philo->id, "is sleeping", philo->table->start_time);
+	pthread_mutex_lock(&philo->table->end_mutex);
+	philo->table->is_simulation_ended = philo->table->loop_end;
+	pthread_mutex_unlock(&philo->table->end_mutex);
+	if (philo->table->is_simulation_ended)
+		return ;
+	pthread_mutex_lock(&philo->table->global_mutex);
+	print_action(philo->id, "is sleeping", philo->table->start_time);
+	pthread_mutex_unlock(&philo->table->global_mutex);
 	usleep(philo->table->time_to_sleep * 1000);
 }
 
@@ -120,6 +138,39 @@ void	sleep_philosopher(t_philosopher *philo)
 	}
 } */
 
+/* void	take_forks(t_philosopher *philo)
+{
+	int	left_fork;
+	int	right_fork;
+	int	is_simulation_ended;
+
+	left_fork = philo->id - 1;
+	right_fork = philo->id % philo->table->nbr_philos;
+
+	pthread_mutex_lock(&philo->table->end_mutex);
+	is_simulation_ended = philo->table->loop_end;
+	pthread_mutex_unlock(&philo->table->end_mutex);
+	if (is_simulation_ended)
+		return ;
+
+	pthread_mutex_lock(&philo->table->forks[left_fork]);
+	pthread_mutex_lock(&philo->table->global_mutex);
+	print_action(philo->id, "has taken a fork", philo->table->start_time);
+	pthread_mutex_unlock(&philo->table->global_mutex);
+	pthread_mutex_lock(&philo->table->end_mutex);
+	is_simulation_ended = philo->table->loop_end;
+	pthread_mutex_unlock(&philo->table->end_mutex);
+	if (is_simulation_ended)
+	{
+		pthread_mutex_unlock(&philo->table->forks[left_fork]);
+		return ;
+	}
+	pthread_mutex_lock(&philo->table->forks[right_fork]);
+	pthread_mutex_lock(&philo->table->global_mutex);
+	print_action(philo->id, "has taken a fork", philo->table->start_time);
+	pthread_mutex_unlock(&philo->table->global_mutex);
+} */
+
 void	take_forks(t_philosopher *philo)
 {
 	int	left_fork;
@@ -128,12 +179,34 @@ void	take_forks(t_philosopher *philo)
 	left_fork = philo->id - 1;
 	right_fork = philo->id % philo->table->nbr_philos;
 
-	pthread_mutex_lock(&philo->table->forks[left_fork]);
-	if (philo->table->loop_end == 0)
-		print_action(philo->id, "has taken a fork", philo->table->start_time);
-	pthread_mutex_lock(&philo->table->forks[right_fork]);
-	if (philo->table->loop_end == 0)
-		print_action(philo->id, "has taken a fork", philo->table->start_time);
+	pthread_mutex_lock(&philo->table->end_mutex);
+	philo->table->is_simulation_ended = philo->table->loop_end;
+	pthread_mutex_unlock(&philo->table->end_mutex);
+	if (philo->table->is_simulation_ended)
+		return ;
+	if (left_fork < right_fork)
+	{
+		pthread_mutex_lock(&philo->table->forks[left_fork]);
+		pthread_mutex_lock(&philo->table->forks[right_fork]);
+	}
+	else
+	{
+		pthread_mutex_lock(&philo->table->forks[right_fork]);
+		pthread_mutex_lock(&philo->table->forks[left_fork]);
+	}
+	pthread_mutex_lock(&philo->table->global_mutex);
+	print_action(philo->id, "has taken a fork", philo->table->start_time);
+	print_action(philo->id, "has taken a fork", philo->table->start_time);
+	pthread_mutex_unlock(&philo->table->global_mutex);
+	pthread_mutex_lock(&philo->table->end_mutex);
+	philo->table->is_simulation_ended = philo->table->loop_end;
+	pthread_mutex_unlock(&philo->table->end_mutex);
+	if (philo->table->is_simulation_ended)
+	{
+		pthread_mutex_unlock(&philo->table->forks[left_fork]);
+		pthread_mutex_unlock(&philo->table->forks[right_fork]);
+		return ;
+	}
 }
 
 void	leave_forks(t_philosopher *philo)
