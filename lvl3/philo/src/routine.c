f3dd9ec2e37247d899184319034f4e5263e1654b
Daniel Jimenez Graindorge <danjimen@c1r12s3.42madrid.com>
2024-07-05 09:50:50 +0200

    Modified print_action prototype and function

--- lvl3/philo/src/routine.c
+++ lvl3/philo/src/routine.c
@@ -3,21 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   routine.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: danjimen <danjimen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 09:56:08 by danjimen          #+#    #+#             */
-/*   Updated: 2024/07/02 23:36:22 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/07/05 09:40:32 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-void	print_action(int id, char *action, long start_time)
+void	print_action(t_philosopher *philo, char *action)
 {
-	printf("%ld: %d %s\n", get_current_time() - start_time, id, action);
+	long	actual_time;
+
+	actual_time = get_current_time() - philo->table->start_time;
+	if (philo->table->loop_end == 0)
+		printf("%ld: %d %s\n", actual_time, philo->id, action);
+
 }
 
-static int	end_of_routine(t_table *table)
+int	end_of_routine(t_table *table)
 {
 	int	i;
 
@@ -30,21 +35,22 @@ static int	end_of_routine(t_table *table)
 		if (get_current_time() - table->philos[i].last_meal_time > table->time_to_die)
 		{
 			table->loop_end = 1;
-			print_action(table->philos[i].id, "died", table->start_time);
-			return (pthread_mutex_unlock(&table->end_mutex), 1);
+			//print_action(table->philos, "died");
+			printf("%ld: %d %s\n", get_current_time() - table->start_time, table->philos[i].id, "died");
+			return (pthread_mutex_unlock(&table->end_mutex), true);
 			//return (1);
 		}
 		if (table->nbr_must_eat != -1 && table->philos[i].meals_eaten >= table->nbr_must_eat)
 		{
 			table->loop_end = 1;
 			printf("Done %i loops correctly\n", table->philos[i].meals_eaten);
-			return (pthread_mutex_unlock(&table->end_mutex), 1);
+			return (pthread_mutex_unlock(&table->end_mutex), true);
 			//return (1);
 		}
 		i++;
 	}
 	pthread_mutex_unlock(&table->end_mutex);
-	return (0);
+	return (false);
 }
 
 void	*philo_routine(void *arg)
