ea5d14a4a260fafd4b0a805daa14ecb431ad5067
BishopVK <danielgraindorge@gmail.com>
2024-07-28 07:26:14 +0200

    Created back-up in hollydays after 2 weeks whithout coding

---
+++ lvl3/philo/back-up/v5-starting-hollydays/src/actions.c
@@ -0,0 +1,178 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   actions.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/01 15:05:13 by danjimen          #+#    #+#             */
+/*   Updated: 2024/07/09 14:50:28 by danjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo.h"
+
+void	think(t_philosopher *philo)
+{
+	//if (philo->table->loop_end == 0)
+		print_action(philo, "is thinking");
+}
+
+void	eat(t_philosopher *philo)
+{
+	// long	current;
+
+	// current = get_current_time() - philo->table->start_time;
+	//if (philo->table->loop_end == 0)
+		print_action(philo, "is eating");
+	//usleep(philo->table->time_to_eat * 1000);
+	//if (current + (philo->table->time_to_die * 1000) - (philo->table->time_to_eat * 1000) > current + (philo->table->time_to_eat * 1000))
+	/* if (philo->last_meal_time + (philo->table->time_to_die * 1000) - (philo->table->time_to_eat * 1000) > current + (philo->table->time_to_eat * 1000))
+	{
+		usleep(philo->table->time_to_eat * 1000);
+		//custom_sleep(philo->table->time_to_eat * 1000, philo);
+		philo->meals_eaten++;
+		philo->last_meal_time = get_current_time();
+	} */
+	// printf("philo->table->time_to_eat = %i\n", philo->table->time_to_eat);
+	// printf("current = %li\n", current);
+	// printf("philo->table->time_to_die = %i\n", philo->table->time_to_die);
+	// printf("philo->last_meal_time = %li\n", philo->last_meal_time - philo->table->start_time);
+	/* if (philo->table->time_to_eat > (philo->table->time_to_die - current - philo->table->start_time))
+	{
+		printf("philo->table->time_to_die - current = %li\n", philo->table->time_to_die - current);
+		usleep(philo->table->time_to_die - current);
+	} */
+		usleep(philo->table->time_to_eat * 1000);
+		//custom_sleep(philo->table->time_to_eat * 1000, philo);
+		philo->meals_eaten++;
+		philo->last_meal_time = get_current_time();
+		printf("philo->last_meal_time == %li\n", philo->last_meal_time);
+}
+
+void	sleep_philosopher(t_philosopher *philo)
+{
+	//long	current;
+
+	//current = get_current_time();
+	//if (philo->table->loop_end == 0)
+		print_action(philo, "is sleeping");
+	//if (current + philo->table->time_to_die > current + philo->table->time_to_sleep)
+	//if (philo->last_meal_time + (philo->table->time_to_die * 1000) - (philo->table->time_to_sleep * 1000) > current + (philo->table->time_to_sleep * 1000))
+		usleep(philo->table->time_to_sleep * 1000);
+	//custom_sleep(philo->table->time_to_sleep * 1000, philo);
+}
+
+/* void	take_forks(t_philosopher *philo)
+{
+	int	left_fork;
+	int	right_fork;
+
+	left_fork = philo->id - 1;
+	right_fork = philo->id % philo->table->nbr_philos;
+
+	if (philo->id % 2 == 0)
+	{
+		while (1)
+		{
+			pthread_mutex_lock(&philo->table->global_mutex); // Lock the global mutex to check forks availability
+			pthread_mutex_lock(&philo->table->forks[left_fork]);
+			if (pthread_mutex_lock(&philo->table->forks[right_fork]) == 0)
+			{
+				pthread_mutex_unlock(&philo->table->global_mutex); // Unlock the global mutex as we have taken both forks
+				if (philo->table->loop_end == 0)
+					print_action(philo->id, "has taken a fork", philo->table->start_time);
+				if (philo->table->loop_end == 0)
+					print_action(philo->id, "has taken a fork", philo->table->start_time);
+				return ; // Both forks taken, exit the function
+			}
+			else
+			{
+				pthread_mutex_unlock(&philo->table->forks[left_fork]); // Release the left fork if right fork is not available
+				pthread_mutex_unlock(&philo->table->forks[right_fork]); // Release the left fork if right fork is not available
+			}
+			pthread_mutex_unlock(&philo->table->global_mutex); // Unlock the global mutex if we couldn't take both forks
+			usleep(100);
+		}
+	}
+	else
+	{
+		while (1)
+		{
+			pthread_mutex_lock(&philo->table->global_mutex); // Lock the global mutex to check forks availability
+			pthread_mutex_lock(&philo->table->forks[right_fork]);
+			if (pthread_mutex_lock(&philo->table->forks[left_fork]) == 0)
+			{
+				pthread_mutex_unlock(&philo->table->global_mutex); // Unlock the global mutex as we have taken both forks
+				if (philo->table->loop_end == 0)
+					print_action(philo->id, "has taken a fork", philo->table->start_time);
+				if (philo->table->loop_end == 0)
+					print_action(philo->id, "has taken a fork", philo->table->start_time);
+				return ; // Both forks taken, exit the function
+			}
+			else
+			{
+				pthread_mutex_unlock(&philo->table->forks[right_fork]); // Release the left fork if right fork is not available
+				pthread_mutex_unlock(&philo->table->forks[left_fork]); // Release the left fork if right fork is not available
+			}
+			pthread_mutex_unlock(&philo->table->global_mutex); // Unlock the global mutex if we couldn't take both forks
+			usleep(100);
+		}
+	}
+} */
+
+/* void	take_forks(t_philosopher *philo)
+{
+	int	left_fork;
+	int	right_fork;
+
+	left_fork = philo->id - 1;
+	right_fork = philo->id % philo->table->nbr_philos;
+
+	if (philo->id % 2 == 0)
+	{
+		pthread_mutex_lock(&philo->table->forks[left_fork]);
+		if (philo->table->loop_end == 0)
+			print_action(philo->id, "has taken a fork", philo->table->start_time);
+		pthread_mutex_lock(&philo->table->forks[right_fork]);
+		if (philo->table->loop_end == 0)
+			print_action(philo->id, "has taken a fork", philo->table->start_time);
+	}
+	else
+	{
+		pthread_mutex_lock(&philo->table->forks[right_fork]);
+		if (philo->table->loop_end == 0)
+			print_action(philo->id, "has taken a fork", philo->table->start_time);
+		pthread_mutex_lock(&philo->table->forks[left_fork]);
+		if (philo->table->loop_end == 0)
+			print_action(philo->id, "has taken a fork", philo->table->start_time);
+	}
+} */
+
+void	take_forks(t_philosopher *philo)
+{
+	int	left_fork;
+	int	right_fork;
+
+	left_fork = philo->id - 1;
+	right_fork = philo->id % philo->table->nbr_philos;
+
+	pthread_mutex_lock(&philo->table->forks[left_fork]);
+	//if (philo->table->loop_end == 0)
+		print_action(philo, "has taken a fork");
+	pthread_mutex_lock(&philo->table->forks[right_fork]);
+	//if (philo->table->loop_end == 0)
+		print_action(philo, "has taken a fork");
+}
+
+void	leave_forks(t_philosopher *philo)
+{
+	int	left_fork;
+	int	right_fork;
+
+	left_fork = philo->id - 1;
+	right_fork = philo->id % philo->table->nbr_philos;
+
+	pthread_mutex_unlock(&philo->table->forks[left_fork]);
+	pthread_mutex_unlock(&philo->table->forks[right_fork]);
+}
---
+++ lvl3/philo/back-up/v5-starting-hollydays/src/routine.c
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   routine.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/01 09:56:08 by danjimen          #+#    #+#             */
+/*   Updated: 2024/07/09 14:49:39 by danjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo.h"
+
+/* void	custom_sleep(int microseconds, t_philosopher *philo)
+{
+	int	interval;
+	int	slept;
+
+	//interval = philo->table->even_delay * 1000;
+	interval = 100 * 1000;
+	slept = 0;
+	while (slept < microseconds)
+	{
+		if (microseconds - slept > interval)
+			usleep(interval);
+		else
+			usleep(microseconds - slept);
+		slept += interval;
+
+		// Verificar si hemos sido interrumpidos
+		pthread_mutex_lock(&philo->table->global_mutex);
+		if (end_of_routine(philo->table) == true)
+		{
+			pthread_mutex_unlock(&philo->table->global_mutex);
+			break ;
+		}
+		pthread_mutex_unlock(&philo->table->global_mutex);
+	}
+} */
+
+void	print_action(t_philosopher *philo, char *action)
+{
+	long	actual_time;
+
+	actual_time = get_current_time() - philo->table->start_time;
+	pthread_mutex_lock(&philo->table->end_mutex);
+	if (philo->table->loop_end == 0)
+		printf("%ld: %d %s\n", actual_time, philo->id, action);
+	pthread_mutex_unlock(&philo->table->end_mutex);
+}
+
+int	end_of_routine(t_table *table)
+{
+	int	i;
+	// long	current;
+
+	// current = get_current_time() - table->start_time;
+	pthread_mutex_lock(&table->end_mutex);
+	if (table->loop_end)
+		return (pthread_mutex_unlock(&table->end_mutex), 1);
+	i = 0;
+	while (i < table->nbr_philos)
+	{
+		if (get_current_time() - table->philos[i].last_meal_time > table->time_to_die)
+		{
+			table->loop_end = 1;
+			//print_action(table->philos, "died");
+			printf("%ld: %d %s\n", get_current_time() - table->start_time, table->philos[i].id, "died");
+			return (pthread_mutex_unlock(&table->end_mutex), true);
+			//return (1);
+		}
+		if (table->nbr_must_eat != -1 && table->philos[i].meals_eaten >= table->nbr_must_eat)
+		{
+			table->loop_end = 1;
+			printf("Done %i loops correctly\n", table->philos[i].meals_eaten);
+			return (pthread_mutex_unlock(&table->end_mutex), true);
+			//return (1);
+		}
+		i++;
+	}
+	pthread_mutex_unlock(&table->end_mutex);
+	return (false);
+}
+
+void	*philo_routine(void *arg)
+{
+	t_philosopher	*philo;
+
+	philo = (t_philosopher *)arg;
+	if (philo->table->nbr_philos == 3 && philo->id == 3)
+		usleep(philo->table->even_delay);
+	if (philo->id % 2 == 0)
+		usleep(philo->table->even_delay);
+	while (end_of_routine(philo->table) == false)
+	{
+		//think(philo);
+		if (end_of_routine(philo->table) == true)
+			break ;
+		take_forks(philo);
+		if (end_of_routine(philo->table) == true)
+		{
+			leave_forks(philo);
+			break ;
+		}
+		eat(philo);
+		leave_forks(philo);
+		if (end_of_routine(philo->table) == true)
+			break ;
+		sleep_philosopher(philo);
+		think(philo);
+	}
+	return (NULL);
+}
