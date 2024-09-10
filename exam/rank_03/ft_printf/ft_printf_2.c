7e1920c875e22d1fe50e6c2b624d13075272845e
BishopVK <danielgraindorge@gmail.com>
2024-09-10 22:58:35 +0200

    Added another retry

--- exam/rank_03/ft_printf/ft_printf_2.c
+++ exam/rank_03/ft_printf/ft_printf_2.c
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 15:43:25 by danjimen          #+#    #+#             */
-/*   Updated: 2024/09/10 22:58:03 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/09/10 22:44:50 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@
 int	putstr(char	*str, int i)
 {
 	if (str == NULL)
-		putstr("(null)", i);
-	if (str[i] == '\n')
-		return i;
+		return (putstr("(null)", i));
+	if (str[i] == '\0')
+		return (i);
 	write(1, &str[i], 1);
 	return (putstr(str, i + 1));
 }
 
-int	putnbr(long nbr, int base)
+int putnbr(long nbr, int base)
 {
 	char	*b = "0123456789abcdef";
 	if (nbr < 0)
@@ -33,25 +33,27 @@ int	putnbr(long nbr, int base)
 	return (putnbr(nbr / base, base) + putnbr(nbr % base, base));
 }
 
-int ptf(char *s, va_list args, int i, int n)
+int	ptf(char *s, va_list args, int i, int n)
 {
 	if (s[i] == '\0')
-		return (n);
+		return n;
 	if (s[i] == '%' && s[i + 1] == 's')
-		return (ptf(s, args, i + 2, n + putstr(va_arg(args, char *), 0)));
+		return (ptf(s,args, i + 2, n + putstr(va_arg(args, char *), 0)));
 	if (s[i] == '%' && s[i + 1] == 'd')
 		return (ptf(s, args, i + 2, n + putnbr(va_arg(args, int), 10)));
 	if (s[i] == '%' && s[i + 1] == 'x')
 		return (ptf(s, args, i + 2, n + putnbr(va_arg(args, int), 16)));
-	write (1, &s[i], 1);
+	write(1, &s[i], 1);
 	return (ptf(s, args, i + 1, n + 1));
 }
 
-int	ft_printf(char *s, ...)
+int ft_printf(char *s, ... )
 {
 	va_list args;
 	return (va_start(args, s), va_end(args), ptf(s, args, 0, 0));
 }
+
+//#include <stdio.h>
 
 int	main(void)
 {
