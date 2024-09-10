c45b9207eda37e111e3289fedb7d3a8d5806a53f
Daniel Jimenez Graindorge <danjimen@c1r12s3.42madrid.com>
2024-09-10 15:56:53 +0200

    Added putstr() function and main() for testing

--- exam/rank_03/ft_printf/ft_printf.c
+++ exam/rank_03/ft_printf/ft_printf.c
@@ -6,9 +6,27 @@
 /*   By: danjimen <danjimen@student.42madrid.e      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 15:43:25 by danjimen          #+#    #+#             */
-/*   Updated: 2024/09/10 15:44:07 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/09/10 15:52:50 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdarg.h>
+
+int	putstr(char *str, int i)
+{
+	if (str == NULL)
+		return (putstr("(null)", i));
+	if (str[i] == '\0')
+		return (i);
+	write(1, &str[i], 1);
+	return (putstr(str, i + 1));
+}
+
+int	main(void)
+{
+	char	*str = "Hola!";
+
+	return (putstr(str, 0));
+}
+
