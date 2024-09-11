5e7178a47e525533c6383550c0059b9acea5f2ab
Daniel Jimenez Graindorge <danjimen@c1r12s3.42madrid.com>
2024-09-11 14:09:22 +0200

    Bonus ended

---
+++ lvl2/push_swap/bonus/get_next_line/get_next_line.c
@@ -0,0 +1,212 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/27 08:11:52 by danjimen          #+#    #+#             */
+/*   Updated: 2024/02/27 08:11:52 by danjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static char	*get_line_and_update_result(char **result, char *line)
+{
+	char	*tmp;
+
+	ft_strlcpy(line, *result, (ft_strchr(*result, '\n') - *result) + 2);
+	tmp = *result;
+	*result = ft_strdup(ft_strchr(*result, '\n') + 1);
+	free(tmp);
+	return (line);
+}
+
+static char	*process_result(char **result)
+{
+	char	*line;
+
+	if (ft_strchr(*result, '\n'))
+	{
+		line = (char *)malloc(ft_strchr(*result, '\n') - *result + 2);
+		if (line == NULL)
+			return (NULL);
+		line = get_line_and_update_result(result, line);
+	}
+	else
+	{
+		line = (char *)malloc(ft_strlen(*result) + 1);
+		if (line == NULL)
+			return (NULL);
+		if (line)
+		{
+			ft_strlcpy(line, *result, ft_strlen(*result) + 1);
+			free(*result);
+			*result = (NULL);
+		}
+	}
+	return (line);
+}
+
+int	read_file(int fd, char **result, char *buffer)
+{
+	int		bytes_read;
+	char	*tmp;
+
+	bytes_read = 1;
+	while (bytes_read != 0 && ft_strchr(*result, '\n') == NULL)
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+		{
+			free(*result);
+			*result = ft_strdup("");
+			return (-1);
+		}
+		buffer[bytes_read] = '\0';
+		tmp = *result;
+		*result = ft_strjoin(*result, buffer);
+		free(tmp);
+	}
+	return (bytes_read);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*buffer;
+	static char	*result;
+	char		*line;
+	int			bytes_read;
+
+	if (BUFFER_SIZE <= 0 || fd < 0)
+		return (NULL);
+	if (!result)
+		result = ft_strdup("");
+	buffer = (char *)malloc(BUFFER_SIZE * sizeof(char) + 1);
+	if (buffer == NULL)
+		return (NULL);
+	bytes_read = read_file(fd, &result, buffer);
+	free(buffer);
+	if (bytes_read <= 0 && ft_strlen(result) == 0)
+	{
+		free(result);
+		result = NULL;
+		return (NULL);
+	}
+	line = process_result(&result);
+	return (line);
+}
+
+// cc -Wall -Wextra -Werror -D BUFFER_SIZE=100
+// get_next_line.c get_next_line_utils.c -fsanitize=address -static-libsan
+// ./a.out txt/texto.txt
+
+//LECTURA DE FICHERO O DE STDIN
+/* int	main(int argc, char *argv[])
+{
+	char *buffer;
+
+	// Leer desde archivo si se proporciona un argumento en la línea de comandos
+	if (argc == 2)
+	{
+		// Abrir el archivo para lectura
+		int fd = open(argv[1], O_RDONLY);
+		if (fd == -1)
+		{
+			perror("open");
+			exit(EXIT_FAILURE);
+		}
+
+		// Llamar a la función para leer el contenido del archivo
+		while ((buffer = get_next_line(fd)) != NULL)
+		{
+			// Verificar si se asignó memoria correctamente
+			if (buffer == NULL)
+			{
+				fprintf(stderr, "Fallo al asignar memoria para el buffer\n");
+				break;
+			}
+
+			// Procesar la línea leída
+			printf("RECIBIDO: %s\n", buffer);
+
+			// Liberar la memoria asignada a la línea
+			free(buffer);
+		}
+
+		// Cerrar el descriptor de archivo
+		if (close(fd) == -1)
+		{
+			perror("close");
+			exit(EXIT_FAILURE);
+		}
+	}
+	// Leer desde stdin si no se proporcionan argumentos en la línea de comandos
+	else if (argc == 1)
+	{
+		printf("Ingrese texto desde stdin:\n");
+		while ((buffer = get_next_line(STDIN_FILENO)) != NULL)
+		{
+			// Verificar si se asignó memoria correctamente
+			if (buffer == NULL)
+			{
+				fprintf(stderr, "Fallo al asignar memoria para el buffer\n");
+				break;
+			}
+
+			// Procesar la línea leída
+			printf("RECIBIDO: %s\n", buffer);
+
+			// Liberar la memoria asignada a la línea
+			free(buffer);
+		}
+	}
+	else
+	{
+		fprintf(stderr, "Uso: %s [nombre_del_archivo]\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	return 0;
+} */
+
+//PRUEBA DE LECTURA DE UN FD DEL CUAL NO ES POSIBLE LA LECTURA
+/* int main(int argc, char *argv[])
+{
+	char *buffer;
+
+	if (argc != 2) {
+		fprintf(stderr, "Uso: %s <nombre_del_archivo>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	// Abrir el archivo para lectura
+	int fd = open(argv[1], O_RDONLY);
+	if (fd == -1) {
+		perror("open");
+		exit(EXIT_FAILURE);
+	}
+
+	// Intentar leer de un descriptor de archivo arbitrario
+	int invalid_fd = 42;
+	printf("Intentando leer de un fd arbitrario (%d):\n", invalid_fd);
+	buffer = get_next_line(invalid_fd);
+
+	if (buffer == NULL) {
+		printf("gnl devolvió NULL para el fd arbitrario %d, como esperaba\n",
+			invalid_fd);
+	} else {
+		printf("gnl devolvió algo inesperado para el fd arbitrario %d.\n",
+			invalid_fd);
+		free(buffer);
+	}
+
+	// Cerrar el descriptor de archivo
+	if (close(fd) == -1) {
+		perror("close");
+		exit(EXIT_FAILURE);
+	}
+
+	return 0;
+} */
---
+++ lvl2/push_swap/bonus/get_next_line/get_next_line_utils_bonus.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/27 08:13:10 by danjimen          #+#    #+#             */
+/*   Updated: 2024/02/27 09:11:46 by danjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+//Busca la primera aparición de un carácter en una cadena.
+char	*ft_strchr(const char *s, int c)
+{
+	c = (unsigned char)c;
+	if (s == NULL)
+		return (NULL);
+	while (*s)
+	{
+		if (*s == c)
+			return ((char *)s);
+		s++;
+	}
+	if (c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
+
+//Calcula la longitud de una cadena de caracteres
+//(sin incluir el carácter nulo).
+size_t	ft_strlen(const char *str)
+{
+	size_t	count;
+
+	if (!str)
+		return (0);
+	if (str == NULL)
+		return (0);
+	count = 0;
+	while (str[count])
+		count++;
+	return (count);
+}
+
+//Duplica una cadena de caracteres en una nueva ubicación de memoria,
+//asignando dinámicamente espacio suficiente
+//y copiando el contenido de la cadena original.
+char	*ft_strdup(const char *s)
+{
+	char	*dest;
+	char	*principio;
+
+	if (s == NULL)
+		return (NULL);
+	dest = (char *)malloc(ft_strlen(s) + 1);
+	if (dest == NULL)
+		return (NULL);
+	principio = dest;
+	while (*s != '\0')
+		*dest++ = *s++;
+	*dest = '\0';
+	return (principio);
+}
+
+//Reserva (con malloc(3)) y devuelve una nueva string,
+//formada por la concatenación de ’s1’ y ’s2’.
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*result;
+	int		i;
+
+	i = 0;
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+	result = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (result == NULL)
+		return (NULL);
+	while (*s1)
+		result[i++] = *s1++;
+	while (*s2)
+		result[i++] = *s2++;
+	result[i] = '\0';
+	return (result);
+}
+
+//Copia una cadena a un buffer con un límite máximo,
+//evitando desbordamientos de buffer.
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	src_len;
+	size_t	i;
+
+	if (src == NULL)
+		return (0);
+	src_len = ft_strlen(src);
+	if (size == 0)
+		return (src_len);
+	i = 0;
+	while (i < size - 1 && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (src_len);
+}
