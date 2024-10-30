caba74dc15b3e3756e341bb41ad1c5055b4e437a
BishopVK <danielgraindorge@gmail.com>
2024-10-30 21:27:54 +0100

    Renamed files

--- exam/rank_03/get_next_line/get_next_line_6.h
+++ exam/rank_03/get_next_line/get_next_line_6.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_6.h                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 20:11:58 by danjimen          #+#    #+#             */
-/*   Updated: 2024/09/10 20:18:36 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/10/30 21:27:25 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_6_H
+# define GET_NEXT_LINE_6_H
 
 #include <unistd.h>
 #include <stdlib.h>
