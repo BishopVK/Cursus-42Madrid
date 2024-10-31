a09772aae1a93836b8b4ab1bef8857106ca38e83
Daniel Jimenez Graindorge <danjimen@c1r12s3.42madrid.com>
2024-10-31 09:06:17 +0100

    Added new retry

---
+++ exam/rank_03/get_next_line/get_next_line.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/31 08:55:42 by danjimen          #+#    #+#             */
+/*   Updated: 2024/10/31 09:05:07 by danjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strdup(char *str)
+{
+	int		i;
+	char	*dst;
+
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (str[i])
+		i++;
+	dst = malloc(i + 1);
+	if (!dst)
+		return (NULL);
+	i = 0;
+	while (str[i])
+	{
+		dst[i] = str[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	buffer[BUFFER_SIZE];
+	char		line[70000];
+	static int	buffer_readed;
+	static int	buffer_pos;
+	int			i;
+
+	i = 0;
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	while (1)
+	{
+		if (buffer_pos >= buffer_readed)
+		{
+			buffer_readed = read(fd, buffer, BUFFER_SIZE);
+			buffer_pos = 0;
+			if (buffer_readed <= 0)
+				break ;
+		}
+		line[i++] = buffer[buffer_pos++];
+		if (line[i - 1] == '\n')
+			break ;
+	}
+	line[i] = '\0';
+	if (i == 0)
+		return (NULL);
+	return (ft_strdup(line));
+}
+
+int	main(void)
+{
+	char	*line;
+	int		fd;
+	int		i;
+
+	fd = open("txt.txt", O_RDONLY);
+	i = 1;
+	line = get_next_line(fd);
+	while (line)
+	{
+		printf("Line %i: %s", i++, line);
+		free(line);
+		line = get_next_line(fd);
+	}
+	close (fd);
+	return (0);
+}
---
+++ exam/rank_03/get_next_line/get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/31 08:52:56 by danjimen          #+#    #+#             */
+/*   Updated: 2024/10/31 09:05:00 by danjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+
+#endif
