58fa1141bd05dad81450fccd9f8106b982ea2031
BishopVK <danielgraindorge@gmail.com>
2024-11-18 19:34:29 +0100

    Replaced ft_usleep (t_think * 0.1) by 1 milisecond

--- lvl3/philo/src/actions/think.c
+++ lvl3/philo/src/actions/think.c
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 10:49:36 by danjimen          #+#    #+#             */
-/*   Updated: 2024/11/18 10:49:56 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/11/18 16:05:44 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 void	think(t_philosopher *philo)
 {
-	long	t_eat;
+	/* long	t_eat;
 	long	t_sleep;
-	long	t_think;
+	long	t_think; */
 
 	pthread_mutex_lock(&philo->table->end_mutex);
 	if (philo->table->loop_end == false)
@@ -31,10 +31,11 @@ void	think(t_philosopher *philo)
 	}
 	if (philo->table->nbr_philos % 2 == 0)
 		return ;
-	t_eat = philo->table->time_to_eat;
+	/* t_eat = philo->table->time_to_eat;
 	t_sleep = philo->table->time_to_sleep;
 	t_think = (t_eat * 2) - t_sleep;
 	if (t_think < 0)
 		t_think = 0;
-	ft_usleep(t_think * 0.1);
+	ft_usleep(t_think * 0.1); */
+	ft_usleep(1);
 }
