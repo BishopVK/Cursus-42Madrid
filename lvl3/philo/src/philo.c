2612932a2bd390cd2f030bb44ba058f80483c98a
Daniel Jimenez Graindorge <danjimen@c1r12s3.42madrid.com>
2024-11-18 10:35:57 +0100

    Norminette fixed

--- lvl3/philo/src/philo.c
+++ lvl3/philo/src/philo.c
@@ -6,47 +6,11 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 11:37:14 by danjimen          #+#    #+#             */
-/*   Updated: 2024/11/18 10:33:04 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/11/18 10:35:40 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
-
-/* long long	timestamp(void)
-{
-	struct timeval	tv;
-
-	gettimeofday(&tv, NULL);
-	return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
-} */
-
-/* void	ft_usleep(int ms)
-{
-	long int	time;
-
-	time = timestamp();
-	while (timestamp() - time < ms)
-		usleep(ms / 10);
-} */
-
-/* long long	ft_calc_time(void)
-{
-	struct timeval	tv;
-	long long	time_now;
-
-	gettimeofday(&tv, NULL);
-	time_now = (tv.tv_sec * 1000 + tv.tv_usec / 1000);
-	return (time_now);
-} */
-
-/* void	ft_usleep(int time)
-{
-	long long	old_time;
-
-	old_time = ft_calc_time();
-	while ((ft_calc_time() - old_time) < time)
-		usleep(400);
-} */
 
 long	get_current_time(void)
 {
@@ -64,14 +28,7 @@ void	ft_usleep(size_t time)
 
 	start = get_current_time();
 	while ((get_current_time() - start) < time)
-	{
-		/* printf("DB: Entré\n");
-		printf("DB: get_instant() = %zu\n", get_instant());
-		printf("DB: start = %zu\n", start);
-		printf("DB: get_instant() - start = %zu\n", get_instant() - start);
-		printf("DB: time = %zu\n", time / 1000); */
 		usleep(200);
-	}
 }
 
 static void	cleanup(t_table *table)
@@ -79,7 +36,7 @@ static void	cleanup(t_table *table)
 	int	i;
 
 	i = 0;
-	while(i < table->nbr_philos)
+	while (i < table->nbr_philos)
 	{
 		pthread_mutex_destroy(&table->forks[i]);
 		i++;
@@ -93,7 +50,7 @@ static void	cleanup(t_table *table)
 
 static void	initialize_structs(t_table *table)
 {
-	int	i;
+	int				i;
 	struct timeval	time;
 
 	table->forks = malloc(sizeof(pthread_mutex_t) * table->nbr_philos);
@@ -120,17 +77,6 @@ static void	initialize_structs(t_table *table)
 	}
 }
 
-/*
-1 800 200 200 (muere)
-5 800 200 200 (viven)
-5 800 200 200 7 (viven y cada uno come 7 veces)
-4 410 200 200 (viven)
-4 310 200 100 (mueren)
-3 310 103 103 (tienen que vivir)
-3 310 104 104 (tienen que morir)
-181 400 200 200 (mueren)
-*/
-
 int	main(int argc, char **argv)
 {
 	t_table	table;
@@ -143,10 +89,11 @@ int	main(int argc, char **argv)
 	i = 0;
 	while (i < table.nbr_philos)
 	{
-		pthread_create(&table.philos[i].thread, NULL, philo_routine, &table.philos[i]);
+		pthread_create(&table.philos[i].thread, NULL,
+			philo_routine, &table.philos[i]);
 		i++;
 	}
-	pthread_join(table.referee, NULL); // Comment??
+	pthread_join(table.referee, NULL);
 	i = 0;
 	while (i < table.nbr_philos)
 	{
