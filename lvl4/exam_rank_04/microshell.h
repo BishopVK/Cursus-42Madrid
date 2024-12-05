0a7120e00ca1ab43a3b1a98b332bc471f5dc4337
Daniel Jimenez Graindorge <danjimen@c1r12s3.42madrid.com>
2024-12-05 12:29:35 +0100

    Added file whit all system headers includes

---
+++ lvl4/exam_rank_04/microshell.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   microshell.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/05 12:05:49 by danjimen          #+#    #+#             */
+/*   Updated: 2024/12/05 12:27:54 by danjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MICROSHELL_H
+# define MICROSHELL_H
+
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+#endif
