5cb532d5517e09b8c5f606af9c1bc36c95b4ca46
BishopVK <danielgraindorge@gmail.com>
2024-12-06 12:29:48 +0100

    Completed ex00

--- lvl4/CPP_Module/CPP_Module_00/ex00/megaphone.cpp
+++ lvl4/CPP_Module/CPP_Module_00/ex00/megaphone.cpp
@@ -6,19 +6,23 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/06 11:22:23 by danjimen          #+#    #+#             */
-/*   Updated: 2024/12/06 12:13:07 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/12/06 12:28:50 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <string>
 
 int	main(int argc, char **argv)
 {
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	for (int i = 0; i < argc; i++)
-		std::cout << argv[i] << std::endl;
-
+	for (int i = 1; i < argc; i++)
+	{
+		std::string arg = argv[i];
+		for (int j = 0; j < arg.length(); j++)
+			arg[j] = std::toupper(arg[j]);
+		std::cout << arg;
+	}
+	std::cout << std::endl;
 	return (0);
 }
