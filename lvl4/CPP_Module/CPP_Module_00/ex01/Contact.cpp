659a07647f8c012382437a13b68c570251683e24
BishopVK <danielgraindorge@gmail.com>
2024-12-09 21:53:22 +0100

    Removed prints

--- lvl4/CPP_Module/CPP_Module_00/ex01/Contact.cpp
+++ lvl4/CPP_Module/CPP_Module_00/ex01/Contact.cpp
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 21:35:26 by danjimen          #+#    #+#             */
-/*   Updated: 2024/12/09 21:46:04 by danjimen         ###   ########.fr       */
+/*   Updated: 2024/12/09 21:53:09 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,10 @@
 
 Contact::Contact(void)
 {
-	std::cout << "Se ha creado un contacto" << std::endl;
 	return ;
 }
 
 Contact::~Contact(void)
 {
-	std::cout << "Se ha destruido un contacto" << std::endl;
 	return ;
 }
