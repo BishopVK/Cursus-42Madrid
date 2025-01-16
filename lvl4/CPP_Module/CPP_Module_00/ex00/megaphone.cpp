813a5c2912a7944d560578011f141a76e98622dd
BishopVK <danielgraindorge@gmail.com>
2025-01-16 14:12:27 +0100

    Added a version to expand 2 bytes chars

--- lvl4/CPP_Module/CPP_Module_00/ex00/megaphone.cpp
+++ lvl4/CPP_Module/CPP_Module_00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/06 11:22:23 by danjimen          #+#    #+#             */
-/*   Updated: 2024/12/06 12:28:50 by danjimen         ###   ########.fr       */
+/*   Updated: 2025/01/16 14:12:09 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,3 +26,52 @@ int	main(int argc, char **argv)
 	std::cout << std::endl;
 	return (0);
 }
+
+/* #include <iostream>
+#include <string>
+#include <map>
+
+std::string to_upper_extended(const std::string& s) {
+	static const std::map<std::string, std::string> extended_upper = {
+		{"á", "Á"}, {"é", "É"}, {"í", "Í"},
+		{"ó", "Ó"}, {"ú", "Ú"}, {"ñ", "Ñ"},
+		{"ä", "Ä"}, {"ë", "Ë"}, {"ï", "Ï"},
+		{"ö", "Ö"}, {"ü", "Ü"}
+	};
+
+	// Si la cadena está en el mapa, retornamos la versión en mayúscula
+	if (extended_upper.count(s)) {
+		return extended_upper.at(s);
+	}
+
+	return s; // Retornamos la misma cadena si no está en el mapa
+}
+
+std::string process_argument(const std::string& arg) {
+	std::string result;
+	for (size_t i = 0; i < arg.size(); i++) {
+		// Detectar caracteres multibyte (usando UTF-8)
+		if ((arg[i] & 0x80) != 0) { // Carácter extendido
+			std::string multibyte_char = arg.substr(i, 2); // Tomamos 2 bytes
+			result += to_upper_extended(multibyte_char);
+			i++; // Saltamos el segundo byte
+		} else {
+			// Carácter ASCII
+			result += std::toupper(static_cast<unsigned char>(arg[i]));
+		}
+	}
+	return result;
+}
+
+int main(int argc, char **argv) {
+	if (argc == 1) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	} else {
+		for (int i = 1; i < argc; i++) {
+			std::cout << process_argument(argv[i]);
+		}
+	}
+	std::cout << std::endl;
+	return 0;
+}
+*/
