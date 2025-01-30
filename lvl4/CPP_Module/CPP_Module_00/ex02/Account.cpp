3216ca7f54348aed4893de0796d3677efdb4ef1c
BishopVK <danielgraindorge@gmail.com>
2025-01-30 17:27:52 +0100

    Added downloaded files in 'files' folder. Added Account.cpp to begining the exercise

---
+++ lvl4/CPP_Module/CPP_Module_00/ex02/Account.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/30 16:51:20 by danjimen          #+#    #+#             */
+/*   Updated: 2025/01/30 17:08:21 by danjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+Account::Account( int initial_deposit )
+{
+	/* Phonebook::current = 0;
+	Phonebook::nbContacts = 0; */
+	return ;
+}
+
+Account::~Account(void)
+{
+	return ;
+}
