23fc6e784fadadbe7fc650191dde89424b42010f
BishopVK <danielgraindorge@gmail.com>
2025-03-21 18:41:19 +0100

    Modified bsp to work whit Fixed vars

--- lvl4/CPP_Module/CPP_Module_02/ex03/bsp.cpp
+++ lvl4/CPP_Module/CPP_Module_02/ex03/bsp.cpp
@@ -6,44 +6,44 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 17:00:22 by danjimen          #+#    #+#             */
-/*   Updated: 2025/03/21 18:30:50 by danjimen         ###   ########.fr       */
+/*   Updated: 2025/03/21 18:37:55 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
 // Returns absolute value
-float	absolute(const float &nb)
+Fixed	absolute(const Fixed &nb)
 {
 	if (nb < 0)
-		return (-nb);
+		return (Fixed(nb) * -1);
 	return (nb);
 }
 
-float	calculateArea(const Point &a, const Point &b, const Point &c)
+Fixed	calculateArea(const Point &a, const Point &b, const Point &c, const std::string &name)
 {
-	float	area;
+	Fixed	area;
 
-	area = 0.5f * absolute((a.getX().toFloat() * (b.getY().toFloat() - c.getY().toFloat())) +
+	area = Fixed(0.5f) * absolute((a.getX().toFloat() * (b.getY().toFloat() - c.getY().toFloat())) +
 							(b.getX().toFloat() * (c.getY().toFloat() - a.getY().toFloat())) +
 							(c.getX().toFloat() * (a.getY().toFloat() - b.getY().toFloat())));
 
-	std::cout << "Area = " << area << std::endl;
+	std::cout << "Area " << name << " = " << area << std::endl;
 	
 	return (area);
 }
 
 bool	bsp(const Point &a, const Point &b, const Point &c, const Point &target)
 {
-	float abc;
-	float abtarget;
-	float actarget;
-	float bctarget;
+	Fixed abc;
+	Fixed abtarget;
+	Fixed actarget;
+	Fixed bctarget;
 
-	abc = calculateArea(a, b, c);
-	abtarget = calculateArea(a, b, target);
-	actarget = calculateArea(a, c, target);
-	bctarget = calculateArea(b, c, target);
+	abc = calculateArea(a, b, c, "abc");
+	abtarget = calculateArea(a, b, target, "abtarget");
+	actarget = calculateArea(a, c, target, "actarget");
+	bctarget = calculateArea(b, c, target, "bctarget");
 	if (abc == (abtarget + actarget + bctarget))
 		return (true);
 	return (false);
--- lvl4/CPP_Module/CPP_Module_02/ex03/main.cpp
+++ lvl4/CPP_Module/CPP_Module_02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 22:44:38 by danjimen          #+#    #+#             */
-/*   Updated: 2025/03/21 18:30:11 by danjimen         ###   ########.fr       */
+/*   Updated: 2025/03/21 18:40:33 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int main(void)
 	Point a(2,2);
 	Point b(4,7);
 	Point c(9,3);
-	Point target(4,2);
+	Point target(3,3);
 
 	if (bsp(a, b, c, target))
 		std::cout << GREEN << "Target point (" << target.getX() << "," << target.getY() << ") its inside the triangle" << RESET << std::endl;
