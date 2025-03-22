643882d09154e2124a5724405dcea9bdb67d979b
BishopVK <danielgraindorge@gmail.com>
2025-03-22 13:30:06 +0100

    Create ex01 folder whit a ex00 folder copy

---
+++ lvl4/CPP_Module/CPP_Module_03/ex01/ClapTrap.hpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/21 23:18:58 by danjimen          #+#    #+#             */
+/*   Updated: 2025/03/22 01:30:32 by danjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+#include <iostream>
+#include <string>
+
+#define BOLD "\033[1m"
+#define BOLD_OFF "\033[22m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN "\033[36m"
+#define RESET "\033[0m"
+#define INVERT "\033[7m"
+
+class ClapTrap
+{
+	private:
+		std::string _name;
+		int			_hitPoints = 10;
+		int			_energy = 10;
+		int			_attack = 10;
+
+	public:
+		ClapTrap();
+		ClapTrap(const std::string &name);
+		ClapTrap(const ClapTrap &other);
+		ClapTrap &operator=(const ClapTrap &other);
+		~ClapTrap();
+
+		std::string		getName() const;
+		unsigned int	getHitPoints() const;
+		unsigned int	getEnergy() const;
+		unsigned int	getAttack() const;
+		void			setName(const std::string &name);
+		void			setHitPoints(const unsigned int &amount);
+		void			setEnergy(const unsigned int &amount);
+		void			setAttack(const unsigned int &amount);
+		void			printStats() const;
+
+		// REQUIRED
+		void	attack(const std::string &target);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
+};
+
+#endif
