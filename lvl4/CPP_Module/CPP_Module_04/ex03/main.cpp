4dbc5ddac57060da51127cd3ad4b87bfe8d16312
BishopVK <danielgraindorge@gmail.com>
2025-03-25 16:35:45 +0100

    Added new tests

--- lvl4/CPP_Module/CPP_Module_04/ex03/main.cpp
+++ lvl4/CPP_Module/CPP_Module_04/ex03/main.cpp
@@ -6,16 +6,19 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 02:08:45 by danjimen          #+#    #+#             */
-/*   Updated: 2025/03/24 22:43:17 by danjimen         ###   ########.fr       */
+/*   Updated: 2025/03/25 16:23:31 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "AMateria.hpp"
 #include "Ice.hpp"
 #include "Cure.hpp"
+#include "ICharacter.hpp"
+#include "Character.hpp"
 
 int main()
 {
-	{
+	/* {
 		const AMateria *ice = new Ice();
 		const AMateria *cure = new Cure();
 		Ice		ice2;
@@ -28,29 +31,39 @@ int main()
 
 		delete cure;
 		delete ice;
-	}
+	} */
 
 	{
 		/* IMateriaSource* src = new MateriaSource();
 		src->learnMateria(new Ice());
-		src->learnMateria(new Cure());
+		src->learnMateria(new Cure()); */
+
+		AMateria *ice = new Ice();
+		AMateria *cure = new Cure();
 
 		ICharacter* me = new Character("me");
+		me->printStats();
 
-		AMateria* tmp;
+
+		/* AMateria* tmp;
 		tmp = src->createMateria("ice");
 		me->equip(tmp);
-		tmp = src->createMateria("cure");
-		me->equip(tmp);
-
+		tmp = src->createMateria("cure"); */
+		me->equip(ice);
+		me->equip(cure);
+		
 		ICharacter* bob = new Character("bob");
-
+		
 		me->use(0, *bob);
 		me->use(1, *bob);
-
+		me->unequip(0);
+		me->use(0, *bob);
+		me->use(1, *bob);
+		
+		me->printStats();
 		delete bob;
 		delete me;
-		delete src; */
+		//delete src;
 	}
 
 	return 0;
