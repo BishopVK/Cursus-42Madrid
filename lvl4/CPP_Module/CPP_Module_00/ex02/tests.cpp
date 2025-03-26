37dfc4d4112e7927f665fd2b3dd845c82f0a19a7
BishopVK <danielgraindorge@gmail.com>
2025-03-26 14:24:47 +0100

    Commented part of code for testing

--- lvl4/CPP_Module/CPP_Module_00/ex02/tests.cpp
+++ lvl4/CPP_Module/CPP_Module_00/ex02/tests.cpp
@@ -38,7 +38,7 @@ int		main( void ) {
 	ints_t::iterator	wit_end		= withdrawals.end();
 
 	Account::displayAccountsInfos();
-	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
+	/* std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
 
 	for ( acc_int_t it( acc_begin, dep_begin );
 		  it.first != acc_end && it.second != dep_end;
@@ -58,7 +58,7 @@ int		main( void ) {
 	}
 
 	Account::displayAccountsInfos();
-	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
+	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) ); */
 
 	return 0;
 }
