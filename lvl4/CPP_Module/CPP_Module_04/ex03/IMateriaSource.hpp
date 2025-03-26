e5591eb724e814dc1a1daf009ec72b53bbaf447b
BishopVK <danielgraindorge@gmail.com>
2025-03-26 02:31:40 +0100

    Added printMateriaLearned()

--- lvl4/CPP_Module/CPP_Module_04/ex03/IMateriaSource.hpp
+++ lvl4/CPP_Module/CPP_Module_04/ex03/IMateriaSource.hpp
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 22:31:23 by danjimen          #+#    #+#             */
-/*   Updated: 2025/03/25 22:32:51 by danjimen         ###   ########.fr       */
+/*   Updated: 2025/03/26 00:46:00 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,14 @@
 class IMateriaSource
 {
 	public:
+		/* IMateriaSource() {};
+		IMateriaSource(const IMateriaSource &other);
+		IMateriaSource &operator=(const IMateriaSource &other); */
 		virtual ~IMateriaSource() {}
 		virtual void learnMateria(AMateria*) = 0;
 		virtual AMateria* createMateria(std::string const & type) = 0;
+
+		virtual void	printMateriaLearned() = 0;
 };
 
 #endif
