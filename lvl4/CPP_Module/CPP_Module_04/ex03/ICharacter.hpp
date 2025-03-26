97ebe54f0626be627dcb2cc61dbb7a8c1dfbb233
BishopVK <danielgraindorge@gmail.com>
2025-03-26 02:30:56 +0100

    Added setName()

--- lvl4/CPP_Module/CPP_Module_04/ex03/ICharacter.hpp
+++ lvl4/CPP_Module/CPP_Module_04/ex03/ICharacter.hpp
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 22:12:19 by danjimen          #+#    #+#             */
-/*   Updated: 2025/03/25 15:56:08 by danjimen         ###   ########.fr       */
+/*   Updated: 2025/03/26 02:13:13 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ class ICharacter
 		virtual void use(int idx, ICharacter& target) = 0; // Pure virtual method
 
 		virtual void	printStats() = 0;
+		virtual void	setName(const std::string &name) = 0;
 };
 
 #endif
