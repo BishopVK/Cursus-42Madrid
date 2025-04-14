f107f9914f9db9d4c2e6d2da5da9754673a0a19c
BishopVK <danielgraindorge@gmail.com>
2025-04-14 12:16:07 +0200

    Added CPP06

---
+++ lvl5/CPP_Module/CPP_Module_06/ex00/ScalarConverter.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/14 09:06:13 by danjimen          #+#    #+#             */
+/*   Updated: 2025/04/14 09:07:40 by danjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+# define SCALARCONVERTER_HPP
+
+#include <iostream>
+#include <string>
+
+class ScalarConverter
+{
+	public:
+
+	private:
+};
+
+#endif
