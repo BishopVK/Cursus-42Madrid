88289ff4e2934495248ae25d3c95aa2154d99d45
Daniel Jimenez Graindorge <danjimen@c2r11s2.42madrid.com>
2025-04-21 08:24:03 +0200

    Updated Name of zombies in a Horde

--- lvl4/CPP_Module/CPP_Module_01/ex01/zombieHorde.cpp
+++ lvl4/CPP_Module/CPP_Module_01/ex01/zombieHorde.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   zombieHorde.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: danjimen <danjimen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 19:47:16 by danjimen          #+#    #+#             */
-/*   Updated: 2025/02/02 12:14:00 by danjimen         ###   ########.fr       */
+/*   Updated: 2025/04/21 08:23:32 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@ Zombie*	zombieHorde( int N, std::string name )
 {
 	if (N <= 0)
 	{
-		std::cout << RED BOLD << "Error: " << RESET << "number of zombies must be more than 0." << std::endl; 
+		std::cerr << RED BOLD << "Error: " << RESET << "number of zombies must be more than 0." << std::endl; 
 		return (NULL);
 	}
 
 	Zombie *zombie = new Zombie[N];
 
 	for (int i = 0; i < N; i++)
-		zombie[i].set_name(name);
+		zombie[i].set_name(name + "_" + std::to_string(i + 1));
 
 	return (zombie);
 }
