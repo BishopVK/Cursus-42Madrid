e22529923efdab0eb213739829d1e0ca5d6f5cd8
BishopVK <danielgraindorge@gmail.com>
2025-04-23 09:20:55 +0200

    Created basic structure of the project

---
+++ lvl5/CPP_Module/CPP_Module_06/ex02/Base.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Base.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/23 09:11:55 by danjimen          #+#    #+#             */
+/*   Updated: 2025/04/23 09:18:54 by danjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BASE_HPP
+# define BASE_HPP
+
+#include <iostream>
+#include <string>
+
+class Base
+{
+	public:
+		virtual ~Base();
+};
+
+#endif
