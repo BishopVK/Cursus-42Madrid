eb1ab06a836aefbc8bab3769a39a4e67d511b440
BishopVK <danielgraindorge@gmail.com>
2025-04-23 22:47:28 +0200

    Added more tests and added delete to allocated pointers

--- lvl5/CPP_Module/CPP_Module_06/ex02/main.cpp
+++ lvl5/CPP_Module/CPP_Module_06/ex02/main.cpp
@@ -6,20 +6,47 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 09:09:56 by danjimen          #+#    #+#             */
-/*   Updated: 2025/04/23 22:36:15 by danjimen         ###   ########.fr       */
+/*   Updated: 2025/04/23 22:47:02 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
 
 int main(void)
 {
-	Base	*base;
+	{
+		std::cout << std::endl << YELLOW << "-- Test 1 --" << RESET << std::endl;
+		A	*a = new A();
+		B	*b = new B();
+		C	*c = new C();
 
-	base = generate();
+		identify(a);
+		identify(b);
+		identify(c);
 
-	identify(base);
-	identify(*base);
+		identify(*a);
+		identify(*b);
+		identify(*c);
+
+		delete a;
+		delete b;
+		delete c;
+	}
+
+	{
+		std::cout << std::endl << YELLOW << "-- Test 2 --" << RESET << std::endl;
+		Base	*base;
+		
+		base = generate();
+		
+		identify(base);
+		identify(*base);
+
+		delete base;
+	}
 
 	return 0;
 }
