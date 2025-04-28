5f98a7c1b82252a7117eb9b6b848ff0eae118741
BishopVK <danielgraindorge@gmail.com>
2025-04-29 01:31:32 +0200

    Added new test

--- lvl4/CPP_Module/testing/array.cpp
+++ lvl4/CPP_Module/testing/array.cpp
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 23:34:05 by danjimen          #+#    #+#             */
-/*   Updated: 2025/04/28 23:44:03 by danjimen         ###   ########.fr       */
+/*   Updated: 2025/04/29 01:31:05 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,13 @@ int main()
 {
 	int	array[15];
 	Test	array2[9];
+	int	array3[] = {10, 17, 2, -5, 44};
 
 	int	size1 = sizeof(array) / sizeof(array[0]);
 	std::cout << "int array len: " << size1 << std::endl;
 	int	size2 = sizeof(array2) / sizeof(array2[0]);
 	std::cout << "int array len: " << size2 << std::endl;
+	int	size3 = sizeof(array3) / sizeof(array3[0]);
+	std::cout << "int array len: " << size3 << std::endl;
 	return 0;
 }
