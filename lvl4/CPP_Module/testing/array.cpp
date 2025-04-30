dcc1409daf495af355395ccd8e7e5d38c1c96990
BishopVK <danielgraindorge@gmail.com>
2025-04-30 18:29:41 +0200

    Added new tests

--- lvl4/CPP_Module/testing/array.cpp
+++ lvl4/CPP_Module/testing/array.cpp
@@ -6,12 +6,14 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 23:34:05 by danjimen          #+#    #+#             */
-/*   Updated: 2025/04/29 01:31:05 by danjimen         ###   ########.fr       */
+/*   Updated: 2025/04/30 18:29:33 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
+
+#define SIZEOF(x) (sizeof(x) / sizeof(x[0]))
 
 class Test
 {
@@ -25,15 +27,30 @@ class Test
 
 int main()
 {
-	int	array[15];
-	Test	array2[9];
-	int	array3[] = {10, 17, 2, -5, 44};
+	{
+		std::cout << std::endl << "-- TEST 1 --" << std::endl;
+		int	array[15];
+		Test	array2[9];
+		int	array3[] = {10, 17, 2, -5, 44};
 
-	int	size1 = sizeof(array) / sizeof(array[0]);
-	std::cout << "int array len: " << size1 << std::endl;
-	int	size2 = sizeof(array2) / sizeof(array2[0]);
-	std::cout << "int array len: " << size2 << std::endl;
-	int	size3 = sizeof(array3) / sizeof(array3[0]);
-	std::cout << "int array len: " << size3 << std::endl;
+		int	size1 = sizeof(array) / sizeof(array[0]);
+		std::cout << "int array len: " << size1 << std::endl;
+		int	size2 = sizeof(array2) / sizeof(array2[0]);
+		std::cout << "int array len: " << size2 << std::endl;
+		int	size3 = sizeof(array3) / sizeof(array3[0]);
+		std::cout << "int array len: " << size3 << std::endl;
+	}
+
+	{
+		std::cout << std::endl << "-- TEST 2 --" << std::endl;
+		int	array[15];
+		Test	array2[9];
+		int	array3[] = {10, 17, 2, -5, 44};
+
+		std::cout << "int array len: " << SIZEOF(array) << std::endl;
+		std::cout << "int array len: " << SIZEOF(array2) << std::endl;
+		std::cout << "int array len: " << SIZEOF(array3) << std::endl;
+	}
+
 	return 0;
 }
