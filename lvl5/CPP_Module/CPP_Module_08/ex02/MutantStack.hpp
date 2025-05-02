132c4fea2ea21df94c1ed5a2bc55b27b542764f4
BishopVK <danielgraindorge@gmail.com>
2025-05-02 23:11:21 +0200

    Fixed code

--- lvl5/CPP_Module/CPP_Module_08/ex02/MutantStack.hpp
+++ lvl5/CPP_Module/CPP_Module_08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 16:07:11 by danjimen          #+#    #+#             */
-/*   Updated: 2025/05/02 16:42:18 by danjimen         ###   ########.fr       */
+/*   Updated: 2025/05/02 23:11:12 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,26 +32,5 @@ class MutantStack : public std::stack<T>
 		iterator begin() {return this->c.begin();}
 		iterator end() {return this->c.end();}
 };
-
-/* template <typename T>
-class MutantStack
-{
-	private:
-		std::stack<int>	_data;
-
-	public:
-		MutantStack();
-		MutantStack(const MutantStack &other);
-		MutantStack &operator=(const MutantStack &other);
-		~MutantStack();
-
-		void	push(const int &n);
-		void	pop();
-		int		top() const;
-		bool	empty() const;
-		int		size() const;
-
-
-}; */
 
 #endif
