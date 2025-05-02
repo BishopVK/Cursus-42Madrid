63ae0028d3b892062a39f36b83cdd600b78bfb8c
BishopVK <danielgraindorge@gmail.com>
2025-05-02 23:30:48 +0200

    Added more tests

--- lvl5/CPP_Module/CPP_Module_08/ex02/main.cpp
+++ lvl5/CPP_Module/CPP_Module_08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 16:05:47 by danjimen          #+#    #+#             */
-/*   Updated: 2025/05/02 23:21:37 by danjimen         ###   ########.fr       */
+/*   Updated: 2025/05/02 23:29:49 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 int main()
 {
 	{
+		std::cout << std::endl << YELLOW "-- TEST 1 --" RESET << std::endl;
 		MutantStack<int> mstack;
 
 		mstack.push(5);
@@ -53,6 +54,27 @@ int main()
 		{
 			std::cout << "Value at " << s.size() - i << " position: " << s.top() << std::endl;
 			s.pop();
+		}
+	}
+
+	{
+		std::cout << std::endl << YELLOW "-- TEST 2 --" RESET << std::endl;
+		MutantStack<int> mstack;
+
+		srand(time(NULL));
+		for (int i = 0; i < 100; ++i)
+			mstack.push(rand() % 100);
+
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+
+		int i = 0;
+		std::cout << std::endl << "mstack values:" << std::endl;
+		while (it != ite)
+		{
+			std::cout << "Value at " << i << " position: " << *it << std::endl;
+			i++;
+			++it;
 		}
 	}
 	return 0;
