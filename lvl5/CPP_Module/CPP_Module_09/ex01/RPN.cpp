768c86be50cc6d4b3e85c92cf46850bb6d4e77cf
BishopVK <danielgraindorge@gmail.com>
2025-05-10 00:06:58 +0200

    Added print of map to debug

--- lvl5/CPP_Module/CPP_Module_09/ex01/RPN.cpp
+++ lvl5/CPP_Module/CPP_Module_09/ex01/RPN.cpp
@@ -6,11 +6,21 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 23:21:45 by danjimen          #+#    #+#             */
-/*   Updated: 2025/05/09 23:57:56 by danjimen         ###   ########.fr       */
+/*   Updated: 2025/05/10 00:06:13 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
+
+static bool	isdigit_string(const std::string &str)
+{
+	for (size_t i = 0; i < str.length(); ++i)
+	{
+		if (!std::isdigit(str[i]))
+			return false;
+	}
+	return true;
+}
 
 std::stack<std::string> split(const std::string &input, char delimiter)
 {
@@ -22,16 +32,6 @@ std::stack<std::string> split(const std::string &input, char delimiter)
 		tokens.push(item);
 	
 	return tokens;
-}
-
-static bool	isdigit_string(const std::string &str)
-{
-	for (size_t i = 0; i < str.length(); ++i)
-	{
-		if (!std::isdigit(str[i]))
-			return false;
-	}
-	return true;
 }
 
 int	check_input(std::stack<std::string> tokens)
@@ -66,5 +66,11 @@ int	RPN(const std::string &input)
 	std::stack<std::string> tokens = split(input, ' ');
 	if (check_input(tokens) == EXIT_FAILURE)
 		return EXIT_FAILURE;
+	int	i = 1;
+	while (!tokens.empty())
+	{
+		std::cout << "Token " << i << " => " << tokens.top() << std::endl;
+		tokens.pop();
+	}
 	return EXIT_SUCCESS;
 }
