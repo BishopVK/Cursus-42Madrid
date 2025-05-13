6f9b6e00fb9e83ef881c5eef9b9b560551cb4204
Daniel Jimenez Graindorge <danjimen@c2r11s2.42madrid.com>
2025-05-13 12:07:22 +0200

    Added new test and list method explanations

--- lvl5/CPP_Module/testing/stl/list.cpp
+++ lvl5/CPP_Module/testing/stl/list.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   list.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: danjimen <danjimen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 21:05:24 by danjimen          #+#    #+#             */
-/*   Updated: 2025/05/01 21:20:08 by danjimen         ###   ########.fr       */
+/*   Updated: 2025/05/13 09:40:06 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 // - No permite acceso por índice directo.
 // - Más memoria por punteros extra.
 
-int	main(void)
+/* int	main(void)
 {
 	std::list<int> lst;
 	lst.push_back(1);
@@ -28,4 +28,21 @@ int	main(void)
 		std::cout << *it << std::endl;
 	}
 	return 0;
+} */
+
+int main()
+{
+	std::list<int> l1;
+	l1.push_back(1);
+	l1.push_back(3);
+	l1.push_back(9);
+	l1.push_back(7);
+	l1.push_back(5);
+
+	l1.sort();
+	for (std::list<int>::iterator it = l1.begin(); it != l1.end(); ++it) {
+		std::cout << *it << std::endl;
+	}
+	return 0;
 }
+
