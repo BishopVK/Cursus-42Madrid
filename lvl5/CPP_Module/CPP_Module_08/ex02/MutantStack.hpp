e6dba490ccd5cc32f0a8ac282fae942f5c5533b8
BishopVK <danielgraindorge@gmail.com>
2025-06-05 01:17:31 +0200

    Added constructors and destructor. Added const, reverse and const_reverse iterators

--- lvl5/CPP_Module/CPP_Module_08/ex02/MutantStack.hpp
+++ lvl5/CPP_Module/CPP_Module_08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 16:07:11 by danjimen          #+#    #+#             */
-/*   Updated: 2025/05/02 23:25:59 by danjimen         ###   ########.fr       */
+/*   Updated: 2025/06/05 01:15:28 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,44 @@
 # include <iostream>
 # include <string>
 # include <stack>
-# include <stdlib.h> //srand()
+# include <cstdlib> //srand()
+# include <ctime> // time()
 
 #define RED "\033[31m"
 #define GREEN "\033[32m"
 #define YELLOW "\033[33m"
+#define BLUE "\033[34m"
 #define RESET "\033[0m"
 
 template <typename T>
 class MutantStack : public std::stack<T>
 {
 	private:
-	public:
-		typedef typename std::stack<T>::container_type::iterator iterator;
 
-		iterator begin() {return this->c.begin();}
-		iterator end() {return this->c.end();}
+	public:
+		MutantStack();
+		MutantStack(const MutantStack &other);
+		MutantStack<T> &operator=(const MutantStack<T> &other);
+		~MutantStack();
+
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+		typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+
+		iterator begin() {return this->c.begin(); }
+		iterator end() {return this->c.end(); }
+
+		const_iterator begin() const { return this->c.begin(); }
+		const_iterator end() const { return this->c.end(); }
+		
+		reverse_iterator rbegin() { return this->c.rbegin(); }
+		reverse_iterator rend() { return this->c.rend(); }
+		
+		const_reverse_iterator rbegin() const { return this->c.rbegin(); }
+		const_reverse_iterator rend() const { return this->c.rend(); }
 };
+
+#include "MutantStack.tpp"
 
 #endif
