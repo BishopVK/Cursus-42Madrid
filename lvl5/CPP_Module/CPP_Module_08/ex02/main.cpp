eb9149e70733c47581cd1bdc2048be58d585e07b
BishopVK <danielgraindorge@gmail.com>
2025-06-05 01:16:39 +0200

    Added new test

--- lvl5/CPP_Module/CPP_Module_08/ex02/main.cpp
+++ lvl5/CPP_Module/CPP_Module_08/ex02/main.cpp
@@ -6,11 +6,12 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 16:05:47 by danjimen          #+#    #+#             */
-/*   Updated: 2025/05/02 23:29:49 by danjimen         ###   ########.fr       */
+/*   Updated: 2025/06/05 01:16:20 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
+#include <list>
 
 int main()
 {
@@ -42,8 +43,7 @@ int main()
 		std::cout << std::endl << "mstack values:" << std::endl;
 		while (it != ite)
 		{
-			std::cout << "Value at " << i << " position: " << *it << std::endl;
-			i++;
+			std::cout << "Value at " << i++ << " position: " << *it << std::endl;
 			++it;
 		}
 		
@@ -59,6 +59,63 @@ int main()
 
 	{
 		std::cout << std::endl << YELLOW "-- TEST 2 --" RESET << std::endl;
+		MutantStack<int>	mstack;
+		std::list<int>		lst;
+
+		mstack.push(5);
+		mstack.push(17);
+		lst.push_back(5);
+		lst.push_back(17);
+
+		std::cout << "Top value in mstack: " << mstack.top() << std::endl;
+		std::cout << "Top value in list: " << lst.back() << std::endl;
+
+		mstack.pop();
+		lst.pop_back();
+
+		std::cout << "Size of mstack: " << mstack.size() << std::endl;
+		std::cout << "Size of list: " << lst.size() << std::endl;
+
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		mstack.push(0);
+		lst.push_back(3);
+		lst.push_back(5);
+		lst.push_back(737);
+		lst.push_back(0);
+
+		std::cout << std::endl << BLUE "CREATE mstack ITERATORS AND PRINT VALUES" RESET << std::endl;
+		MutantStack<int>::iterator mstack_it = mstack.begin();
+		MutantStack<int>::iterator mstack_ite = mstack.end();
+
+		++mstack_it;
+		--mstack_it;
+		int i = 0;
+		std::cout << std::endl << "mstack values:" << std::endl;
+		while (mstack_it != mstack_ite)
+		{
+			std::cout << "mstack value at " << i++ << " position: " << *mstack_it << std::endl;
+			++mstack_it;
+		}
+		
+		std::cout << std::endl << BLUE "CREATE lst ITERATORS AND PRINT VALUES" RESET << std::endl;
+		std::list<int>::iterator lst_it = lst.begin();
+		std::list<int>::iterator lst_ite = lst.end();
+
+		++lst_it;
+		--lst_it;
+		i = 0;
+		std::cout << std::endl << "lst values:" << std::endl;
+		while (lst_it != lst_ite)
+		{
+			std::cout << "lst value at " << i++ << " position: " << *lst_it << std::endl;
+			++lst_it;
+		}
+	}
+
+	{
+		std::cout << std::endl << YELLOW "-- TEST 3 --" RESET << std::endl;
 		MutantStack<int> mstack;
 
 		srand(time(NULL));
@@ -72,8 +129,7 @@ int main()
 		std::cout << std::endl << "mstack values:" << std::endl;
 		while (it != ite)
 		{
-			std::cout << "Value at " << i << " position: " << *it << std::endl;
-			i++;
+			std::cout << "Value at " << i++ << " position: " << *it << std::endl;
 			++it;
 		}
 	}
