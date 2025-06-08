7deb017bda51e88aec22f355e0cceb99f412da51
BishopVK <danielgraindorge@gmail.com>
2025-06-08 03:24:54 +0200

    Created RPN class

--- lvl5/CPP_Module/CPP_Module_09/ex01/RPN.hpp
+++ lvl5/CPP_Module/CPP_Module_09/ex01/RPN.hpp
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 23:17:02 by danjimen          #+#    #+#             */
-/*   Updated: 2025/05/10 22:43:05 by danjimen         ###   ########.fr       */
+/*   Updated: 2025/06/08 03:16:13 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,36 @@
 
 #define RED "\033[31m"
 #define GREEN "\033[32m"
+#define YELLOW "\033[33m"
 #define RESET "\033[0m"
 
-int	RPN(const std::string &input);
+class RPN
+{
+	private:
+		std::string	_input;
+
+	public:
+		RPN();
+		RPN(const std::string &input);
+		RPN(const RPN &other);
+		RPN &operator=(const RPN &other);
+		~RPN();
+
+		void	RPNCalculator();
+
+		std::string	getInput() const;
+		void		setInput(const std::string &input);
+
+		class ErrorException : public std::exception
+		{
+			private:
+				std::string _message;
+			public:
+				ErrorException() : _message(RED "Error" RESET) {}
+				ErrorException(const std::string &message) : _message(RED "Error: " + message + RESET) {}
+				virtual ~ErrorException() throw() {}
+				virtual const char* what() const throw();
+		};
+};
 
 #endif
