fda976b6dfe1233f4a4a935b5892dd686a80193b
BishopVK <danielgraindorge@gmail.com>
2025-06-09 01:21:24 +0200

    Added class PmergeMe

--- lvl5/CPP_Module/CPP_Module_09/ex02/PmergeMe.hpp
+++ lvl5/CPP_Module/CPP_Module_09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 09:45:57 by danjimen          #+#    #+#             */
-/*   Updated: 2025/05/15 23:15:01 by danjimen         ###   ########.fr       */
+/*   Updated: 2025/06/09 01:19:02 by danjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 #include <iostream>
 #include <string>
+#include <cstring> // strlen
 #include <list>
 #include <vector>
 #include <set>
@@ -23,12 +24,29 @@
 #include <climits> // INT_MAX
 #include <time.h> // clock()
 
-
 #define RED "\033[31m"
 #define GREEN "\033[32m"
 #define YELLOW "\033[33m"
+#define BLUE "\033[34m"
 #define RESET "\033[0m"
 
-void	PmergeMe(char const *argv[]);
+class PmergeMe
+{
+	private:
+		int		_argc;
+		char**	_argv;
+		PmergeMe() {};
+
+	public:
+		PmergeMe(int argc, const char* argv[]);
+		PmergeMe(const PmergeMe &other);
+		PmergeMe &operator=(const PmergeMe &other);
+		~PmergeMe();
+
+		void	sortWithList();
+		void	sortWithVector();
+};
+
+//void	PmergeMe(char const *argv[]);
 
 #endif
