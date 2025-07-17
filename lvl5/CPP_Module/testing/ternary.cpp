b7c87c7a1b6c37b37df0c550442b0f69f97c0a2c
BishopVK <danielgraindorge@gmail.com>
2025-07-17 02:33:43 +0200

    Added more test and added namespace use for testing

--- lvl5/CPP_Module/testing/ternary.cpp
+++ lvl5/CPP_Module/testing/ternary.cpp
@@ -6,24 +6,25 @@
 /*   By: danjimen,isainz-r,serferna <webserv@stu    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/26 23:07:32 by danjimen,is       #+#    #+#             */
-/*   Updated: 2025/06/26 23:13:47 by danjimen,is      ###   ########.fr       */
+/*   Updated: 2025/07/17 02:28:52 by danjimen,is      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
+using namespace std;
 
 int main()
 {
 	int			a = 42;
-	std::string	str = "TXT";
+	string		str = "TXT";
 	bool		trueBool = true;
 	bool		falseBool = false;
 
-	std::cout << "int a = " << a << std::endl
+	cout << "int a = " << a << endl
 	<< "string str = " << str << std::endl
-	<< "bool trueBool = " << (trueBool ? std::string("true") : std::string("false")) << std::endl
-	<< "bool falseBool = " << (falseBool ? std::string("true") : std::string("false")) << std::endl;
+	<< "bool trueBool = " << (trueBool ? std::string("true") : std::string("false")) << endl
+	<< "bool falseBool = " << (falseBool ? std::string("true") : std::string("false")) << endl;
 
 	return 0;
 }
