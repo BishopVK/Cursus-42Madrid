165c33e4cdf1858aee6f57ea60accfab5a499b46
Daniel Jimenez Graindorge <danjimen@c1r9s2.42madrid.com>
2025-07-28 09:20:25 +0200

    Updated objects names and default values

--- lvl4/CPP_Module/CPP_Module_04/ex00/main.cpp
+++ lvl4/CPP_Module/CPP_Module_04/ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: danjimen <danjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: danjimen,isainz-r,serferna <webserv@stu    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/22 22:25:26 by danjimen          #+#    #+#             */
-/*   Updated: 2025/03/23 01:44:21 by danjimen         ###   ########.fr       */
+/*   Updated: 2025/07/28 09:14:14 by danjimen,is      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,34 +20,34 @@ int main()
 {
 	{
 		std::cout << YELLOW << std::endl << "-- ANIMALS --" << RESET << std::endl;
-		const Animal* meta = new Animal();
-		const Animal* j = new Dog();
-		const Animal* i = new Cat();
-		std::cout << j->getType() << " " << std::endl;
-		std::cout << i->getType() << " " << std::endl;
-		i->makeSound(); //will output the cat sound!
-		j->makeSound(); //will output the dog sound!
-		meta->makeSound(); //will output the default sound!
+		const Animal* animal = new Animal();
+		const Animal* dog = new Dog();
+		const Animal* cat = new Cat();
+		std::cout << dog->getType() << " " << std::endl;
+		std::cout << cat->getType() << " " << std::endl;
+		cat->makeSound(); //will output the cat sound!
+		dog->makeSound(); //will output the dog sound!
+		animal->makeSound(); //will output the default sound!
 
-		delete i;
-		delete j;
-		delete meta;
+		delete cat;
+		delete dog;
+		delete animal;
 	}
 
 	{
 		std::cout << YELLOW << std::endl << "-- WRONG ANIMALS --" << RESET << std::endl;
-		const WrongAnimal* meta = new WrongAnimal();
-		const WrongAnimal* j = new WrongDog();
-		const WrongAnimal* i = new WrongCat();
-		std::cout << j->getType() << " " << std::endl;
-		std::cout << i->getType() << " " << std::endl;
-		i->makeSound(); //will output the default sound!
-		j->makeSound(); //will output the default sound!
-		meta->makeSound(); //will output the default sound!
+		const WrongAnimal* wrongAnimal = new WrongAnimal();
+		const WrongAnimal* wrongDog = new WrongDog();
+		const WrongAnimal* wrongCat = new WrongCat();
+		std::cout << wrongDog->getType() << " " << std::endl;
+		std::cout << wrongCat->getType() << " " << std::endl;
+		wrongCat->makeSound(); //will output the default sound!
+		wrongDog->makeSound(); //will output the default sound!
+		wrongAnimal->makeSound(); //will output the default sound!
 
-		delete i;
-		delete j;
-		delete meta;
+		delete wrongCat;
+		delete wrongDog;
+		delete wrongAnimal;
 	}
 
 	return 0;
